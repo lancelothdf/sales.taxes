793f2d4fdfddd957662a93b1f6bf8f1eb3e68282
John Bonney <31965225+johnnybonney@users.noreply.github.com>
2018-11-15 11:57:32 -0600

    Work on identifying exemptions

--- R/identify_exemptions_in_progress.R
+++ R/identify_exemptions_in_progress.R
@@ -3,91 +3,39 @@
 #' @description \code{identify_exemptions} identifies tax-exempt goods and marks
 #'     their tax rates as zero.
 #'
-#'
+#' requires tidyr
 
-identify_exemptions <- function(sales_data){
+identify_exemptions <- function(sales_data,
+                                module_exemptions_path = "/project2/igaarder/Data/Nielsen/List_modules_sample_with_description.xlsx"){
   # Step 1: import the module exemptions file (wide or long)
-  module_exemptions <- read.dta13("/project2/igaarder/Data/Nielsen/Module_exemptions_by_state.dta")
-  # From there, if wide, convert to long
-  # Match based on state and product code
-  sales_data[, tax_status := ]
-  gen tax_status = taxable /* 0 is always exempt; 1 is always taxable; 2 different
-  but always constant rate; 3 status change; 4 different and changing */
-    /** Will be used in event studies **/
-    replace sales_tax = 0.02 if taxable == 2 &amp; fips_state == 5 &amp; year &lt;= 2010
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 5 &amp; year &lt;= 2010
-  replace sales_tax = 0.015 if taxable == 2 &amp; fips_state == 5 &amp; year &gt;= 2011
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 5 &amp; year &gt;= 2011
-  replace tax_status = 4 if taxable == 2 &amp; fips_state == 5
-  replace taxable = 0 if taxable == 2 &amp; fips_state == 8 &amp; (year &lt;= 2009 | (year ==
-                                                                                        2010 &amp; quarter == 1))
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 8 &amp; (year &gt;= 2011 | (year ==
-                                                                                        2010 &amp; quarter &gt;= 2))
-  replace tax_status = 3 if taxable == 2 &amp; fips_state == 8
-  replace sales_tax = 0.01 if taxable == 2 &amp; fips_state == 17
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 17
-  replace tax_status = 2 if taxable == 2 &amp; fips_state == 17
-  replace sales_tax = 0.01 if taxable == 3 &amp; fips_state == 17 &amp; (year == 2008 | (year
-                                                                                         == 2009 &amp; quarter &lt; 4))
-  replace taxable = 1 if taxable == 3 &amp; fips_state == 17 &amp; (year == 2008 | (year ==
-                                                                                      2009 &amp; quarter &lt; 4))
-  replace taxable = 1 if taxable == 3 &amp; fips_state == 17 &amp; (year &gt;= 2010 | (year ==
-                                                                                         2009 &amp; quarter == 4))
-  replace tax_status = 4 if taxable == 3 &amp; fips_state == 17
-  replace taxable = 0 if taxable == 2 &amp; fips_state == 23 &amp; (year &lt;= 2012 | (year ==
-                                                                                         2013 &amp; quarter &lt;= 3))
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 23 &amp; (year &gt;= 2014 | (year ==
-                                                                                         2013 &amp; quarter == 4))
-  replace tax_status = 3 if taxable == 2 &amp; fips_state == 23
-  replace sales_tax = 0.01225 if taxable == 2 &amp; fips_state == 29
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 29
-  replace tax_status = 4 if taxable == 2 &amp; fips_state == 29
-  replace sales_tax = 0.02 if taxable == 2 &amp; fips_state == 37
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 37
-  replace tax_status = 4 if taxable == 2 &amp; fips_state == 37
-  replace taxable = 0 if taxable == 2 &amp; fips_state == 44 &amp; (year &lt;= 2010 | (year ==
-                                                                                         2011 &amp; quarter &lt;= 3))
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 44 &amp; (year &gt;= 2012 | (year ==
-                                                                                         2011 &amp; quarter == 4))
-  replace tax_status = 3 if taxable == 2 &amp; fips_state == 44
+  module_exemptions <- read.xlsx(module_exemptions_path, sheetIndex = 1)
+  module_exemptions <- gather(module_exemptions, AL:WY, key = "state", value = "taxable")
+  module_exemptions <- as.data.table(module_exemptions)[, .(Module, state, taxable)]
+  setnames(module_exemptions, old = "Module", new = "product_module_code")
+  module_exemptions[, taxable := as.integer(taxable)]
 
-  replace sales_tax = 0.0525 if taxable == 2 &amp; fips_state == 47 &amp; (year &lt;= 2012 |
-                                                                             (year == 2013 &amp; quarter &lt;= 2))
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 47 &amp; (year &lt;= 2012 | (year ==
-                                                                                         2013 &amp; quarter &lt;= 2))
-  replace sales_tax = 0.05 if taxable == 2 &amp; fips_state == 47 &amp; (year &gt;= 2014 | (year
-                                                                                            == 2013 &amp; quarter &gt;= 3))
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 47 &amp; (year &gt;= 2014 | (year ==
-                                                                                         2013 &amp; quarter &gt;= 3))
-  replace tax_status = 4 if taxable == 2 &amp; fips_state == 47
-  replace sales_tax = 0.03 if taxable == 2 &amp; fips_state == 49
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 49
-  replace tax_status = 4 if taxable == 2 &amp; fips_state == 49
-  replace sales_tax = 0.015 if taxable == 2 &amp; fips_state == 51
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 51
-  replace tax_status = 4 if taxable == 2 &amp; fips_state == 51
-  replace taxable = 0 if taxable == 2 &amp; fips_state == 53 &amp; (year &lt;= 2009 | (year ==
-                                                                                         2010 &amp; quarter &lt;= 2) | year &gt;= 2011)
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 53 &amp; (year == 2010 &amp; quarter
-                                                                    &gt;= 3)
-  replace tax_status = 3 if taxable == 2 &amp; fips_state == 53
-  replace sales_tax = 0.04 if taxable == 2 &amp; fips_state == 54 &amp; (year == 2008 &amp;
-                                                                         quarter &lt;= 2)
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 54 &amp; (year == 2008 &amp; quarter
-                                                                    &lt;= 2)
-  replace sales_tax = 0.03 if taxable == 2 &amp; fips_state == 54 &amp; ((year == 2008 &amp;
-                                                                          quarter &gt;= 3) | (year &gt;= 2009 &amp; year &lt;= 2011))
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 54 &amp; ((year == 2008 &amp; quarter
-                                                                     &gt;= 3) | (year &gt;= 2009 &amp; year &lt;= 2011))
-  replace sales_tax = 0.02 if taxable == 2 &amp; fips_state == 54 &amp; (year == 2012 &amp;
-                                                                         quarter &lt;= 2)
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 54 &amp; (year == 2012 &amp; quarter
-                                                                    &lt;= 2)
-  replace sales_tax = 0.01 if taxable == 2 &amp; fips_state == 54 &amp; ((year == 2012 &amp;
-                                                                          quarter &gt;= 3) | (year == 2013 &amp; quarter &lt;= 2))
-  replace taxable = 1 if taxable == 2 &amp; fips_state == 54 &amp; ((year == 2012 &amp; quarter
-                                                                     &gt;= 3) | (year == 2013 &amp; quarter &lt;= 2))
-  replace taxable = 0 if taxable == 2 &amp; fips_state == 54 &amp; ((year == 2013 &amp; quarter
-                                                                     &gt;= 3) | year &gt;= 2014)
-  replace tax_status = 4 if taxable == 2 &amp; fips_state == 54
+  fips_codes <- structure(list(state = c("AL", "AK", "AZ", "AR", "CA", "CO",
+                                         "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
+                                         "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE",
+                                         "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA",
+                                         "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI",
+                                         "WY", "AS", "GU", "MP", "PR", "UM", "VI"),
+                               fips_state = c(1L, 2L, 4L, 5L, 6L, 8L, 9L, 10L, 11L, 12L, 13L, 15L, 16L, 17L, 18L,
+                                                 19L, 20L, 21L, 22L, 23L, 24L, 25L, 26L, 27L, 28L, 29L, 30L, 31L,
+                                                 32L, 33L, 34L, 35L, 36L, 37L, 38L, 39L, 40L, 41L, 42L, 44L, 45L,
+                                                 46L, 47L, 48L, 49L, 50L, 51L, 53L, 54L, 55L, 56L, 60L, 66L, 69L,
+                                                 72L, 74L, 78L)), row.names = c(NA, -57L), class = "data.frame")
+  module_exemptions <- merge(module_exemptions, as.data.table(fips_codes),
+                             by = c("state"))
+  # 0 is always exempt, 1 is always taxable, 2 is different but always constant rate,
+  # 3 is status change, 4 is different and changing.
+
+
+  #TODO: why are some NA?
+
+  # Lance will send me 1) panel of county-level sales tax rates and 2) list of
+  # exemptions/special tax rates for various products.
+  # At that point, I will connect the county-level sales tax rates to the products,
+  # except for where exemptions apply, in which case I will apply the exemption
+  # or the reduced rate.
 }
