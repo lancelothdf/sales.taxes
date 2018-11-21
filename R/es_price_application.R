7cedac787576786bd9b85bf59765b94b9994c549
John Bonney <31965225+johnnybonney@users.noreply.github.com>
2018-11-21 12:40:23 -0600

    Working on event study graphs

---
+++ R/es_price_application.R
@@ -0,0 +1,94 @@
+#' Make event study graphs for prices (temporary file)
+
+es_price_application <- function(price_data,
+                                 treatment_data_path,
+                                 county_pop_data = NULL,
+                                 weighting_var,
+                                 price_var,
+                                 w_tax,
+                                 fig_outfile = NULL){
+  assertDataTable(price_data)
+  assertCharacter(treatment_data_path)
+  assertCharacter(time)
+  assertLogical(w_tax)
+
+
+  # Before normalizing, need to identify event times...
+  price_data <- merge_treatment(original_data = price_data,
+                                treatment_data_path = treatment_data_path,
+                                time = "event",
+                                merge_by = c("fips_county", "fips_state"))
+
+  price_data[, tt_event := as.integer(12 * year + month - (12 * ref_year + ref_month))]
+  price_data <- price_data[tt_event >= -24 & tt_event <= 24]
+
+  price_data <- normalize_price(price_data = price_data,
+                                      time_type = "event",
+                                      base_time = -1,
+                                      price_var = "price",
+                                      new_price_var = "normalized_price")
+
+
+  price_data <- normalize_price(price_data = price_data,
+                                      time_type = "event",
+                                      base_time = -1,
+                                      price_var = "price_w_tax",
+                                      new_price_var = "normalized_price_w_tax")
+
+
+  # Aggregate to county x product level
+  product_by_county_prices <- price_data[, list(mld_price = mean(normalized_price),
+                                                mld_price_w_tax = mean(normalized_price_w_tax),
+                                                mld_price.wtd = weighted.mean(x = normalized_price,
+                                                                              w = sales.weight),
+                                                mld_price_w_tax.wtd = weighted.mean(x = normalized_price_w_tax,
+                                                                                    w = sales.weight),
+                                                total_sales = sum(sales),
+                                                n_stores = .N),
+                                               by = c("fips_state", "fips_county",
+                                                      "product_module_code",
+                                                      "tt_event")]
+
+  if (!is.null(county_pop_data)){
+    product_by_county_prices <- merge(product_by_county_prices,
+                                      county_pop,
+                                      by = c("fips_state", "fips_county"))
+  }
+
+  price_data[, weights := get(weighting_var)]
+  price_data[, price_var := get(price_var)]
+
+  if (w_tax){
+    y_label <- "Mean normalized ln(price) (post-tax)"
+  } else {
+    y_label <- "Mean normalized ln(price) (pre-tax)"
+  }
+
+
+  price_panel_not_na <- price_panel[!is.na(price_var)]
+  # price_panel[, tt_event := as.integer(12 * year + month - (12 * ref_year + ref_month))]
+  # price_panel <- price_panel[tt_event >= -24 & tt_event <= 24]
+  es_price_collapsed <- price_panel_not_na[, list(mean_ln_price = weighted.mean(x = pretax_price_var,
+                                                                                w = weights),
+                                                  n_counties = uniqueN(1000 * fips_state + fips_county),
+                                                  n_stores = sum(n_stores)),
+                                           by = c("tr_group", "tt_event")]
+  # add number of counties
+  es_price_collapsed <- add_tr_count(collapsed_data = es_price_collapsed,
+                                     tr_group_name = "tr_group",
+                                     count_col_name = "n_counties")
+
+  log_prices <- ggplot(data = es_price_collapsed, mapping = aes(x = tt_event,
+                                                                y = mean_ln_price,
+                                                                color = tr_count)) +
+    labs(x = "Month", y = y_label, color = "Sales tax change",
+         caption = "Note: Price is normalized by subtracting the log of the price in event time t-1 from the log price")
+  geom_line() +
+    theme_bw()
+  if (!is.null(fig_outfile)){
+    ggsave(fig_outfile, plot = log_prices)
+  }
+
+
+  return(log_prices)
+}
--- R/graph_prices_event_time.R
+++ R/graph_prices_event_time.R
@@ -2,7 +2,7 @@
 #' Last modified: 11/21/2018
 #'
 #' Graphs:
-#' Plot log of normalized prices by calendar time
+#' Plot log of normalized prices by event time
 #'
 
 rm(list=ls())
@@ -24,50 +24,17 @@ all_nielsen_data <- balance_panel_data(all_nielsen_data,
                                        n_periods = 84)
 all_nielsen_data[, price := sales / quantity]
 
-# Merge on the tax rates (including exemptions)
+## incorporate tax rates
 all_nielsen_data <- merge_tax_rates(sales_data = all_nielsen_data,
                                     county_monthly_tax_data = county_monthly_tax)
 all_nielsen_data[, price_w_tax := (1 + applicable_tax) * price]
-all_nielsen_data <- normalize_price(price_data = all_nielsen_data,
-                    time_type = "calendar",
-                    base_time = c(2008, 1),
-                    price_var = "price",
-                    new_price_var = "normalized_price")
-
-
-all_nielsen_data <- normalize_price(price_data = all_nielsen_data,
-                                    time_type = "calendar",
-                                    base_time = c(2008, 1),
-                                    price_var = "price_w_tax",
-                                    new_price_var = "normalized_price_w_tax")
 # creates a weighted sales variable called sales.weight
 all_nielsen_data <- make_fixed_weights(all_nielsen_data,
                                        weight_time = list(year = 2008, month = 1),
                                        weight_var = "sales",
                                        panel_unit_vars = c("fips_state", "fips_county", "store_code_uc", "product_module_code"))
 
-# Aggregate to county x product level
-product_by_county_prices <- all_nielsen_data[, list(mld_price = mean(normalized_price),
-                                                    mld_price_w_tax = mean(normalized_price_w_tax),
-                                                    mld_price.wtd = weighted.mean(x = normalized_price,
-                                                                                  w = sales.weight),
-                                                    mld_price_w_tax.wtd = weighted.mean(x = normalized_price_w_tax,
-                                                                                        w = sales.weight),
-                                                    total_sales = sum(sales),
-                                                    n_stores = .N),
-                                       by = c("fips_state", "fips_county",
-                                              "product_module_code",
-                                              "month", "year")]
-product_by_county_prices[, cty_base_sales := max(total_sales * as.integer(year == 2008 & month == 1)),
-                         by = c("fips_state", "fips_county", "product_module_code")]
-fwrite(product_by_county_prices, "Data/Nielsen/product_by_county_prices.csv")
-
-# product_by_county_prices <- fread("Data/Nielsen/product_by_county_prices.csv")
 county_pop <- fread("Data/county_population.csv")
-product_by_county_prices <- merge(product_by_county_prices,
-                                  county_pop,
-                                  by = c("fips_state", "fips_county"))
-
 
 ### At this point, the data is ready (for unweighted, calendar plots)
 ### COMPREHENSIVE DEFINITION ###
@@ -111,7 +78,7 @@ price_application(product_by_county_prices,
 price_application(product_by_county_prices,
                   treatment_data_path = "Data/tr_groups_comprehensive.csv",
                   time = "calendar",
-                  weighting_var = "cty_base_sales",
+                  weighting_var = "total_sales",
                   pretax_var = "mld_price.wtd",
                   posttax_var = "mld_price_w_tax.wtd",
                   w_tax = F,
@@ -119,7 +86,7 @@ price_application(product_by_county_prices,
 price_application(product_by_county_prices,
                   treatment_data_path = "Data/tr_groups_comprehensive.csv",
                   time = "calendar",
-                  weighting_var = "cty_base_sales",
+                  weighting_var = "total_sales",
                   pretax_var = "mld_price.wtd",
                   posttax_var = "mld_price_w_tax.wtd",
                   w_tax = T,
@@ -129,7 +96,7 @@ price_application(product_by_county_prices,
 price_application(product_by_county_prices,
                   treatment_data_path = "Data/tr_groups_restrictive.csv",
                   time = "calendar",
-                  weighting_var = "cty_base_sales",
+                  weighting_var = "total_sales",
                   pretax_var = "mld_price.wtd",
                   posttax_var = "mld_price_w_tax.wtd",
                   w_tax = F,
@@ -137,7 +104,7 @@ price_application(product_by_county_prices,
 price_application(product_by_county_prices,
                   treatment_data_path = "Data/tr_groups_restrictive.csv",
                   time = "calendar",
-                  weighting_var = "cty_base_sales",
+                  weighting_var = "total_sales",
                   pretax_var = "mld_price.wtd",
                   posttax_var = "mld_price_w_tax.wtd",
                   w_tax = T,
