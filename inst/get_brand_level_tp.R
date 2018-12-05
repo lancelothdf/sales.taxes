e810bc7be32e32817e3a2b5438b7ae2dfcd8d77a
John Bonney <31965225+johnnybonney@users.noreply.github.com>
2018-12-05 11:34:30 -0600

    Add product details

--- inst/get_brand_level_tp.R
+++ inst/get_brand_level_tp.R
@@ -8,21 +8,25 @@ library(sales.taxes)
 library(readstata13)
 library(data.table)
 
-tp_sales_panel <- data.table(NULL)
-for (year in 2006:2016){
-  filename <- paste0("/project2/igaarder/Data/Nielsen/", year, "_monthly_toilet_tissue.dta")
-  annual_tp <- as.data.table(read.dta13(filename))
-  store_id_file <- paste0("/project2/igaarder/Data/Nielsen/stores_", year, ".dta")
-  store_id <- as.data.table(read.dta13(store_id_file))
-  setnames(store_id, old = "fips_state_code", new = "fips_state")
-  setnames(store_id, old = "fips_county_code", new = "fips_county")
-  annual_tp <- merge(annual_tp, store_id, by = "store_code_uc", all.x = T)
-  annual_tp <- annual_tp[channel_code %in% c("M", "F", "D")]
+# tp_sales_panel <- data.table(NULL)
+# for (year in 2006:2016){
+#   filename <- paste0("/project2/igaarder/Data/Nielsen/", year, "_monthly_toilet_tissue.dta")
+#   annual_tp <- as.data.table(read.dta13(filename))
+#   store_id_file <- paste0("/project2/igaarder/Data/Nielsen/stores_", year, ".dta")
+#   store_id <- as.data.table(read.dta13(store_id_file))
+#   setnames(store_id, old = "fips_state_code", new = "fips_state")
+#   setnames(store_id, old = "fips_county_code", new = "fips_county")
+#   annual_tp <- merge(annual_tp, store_id, by = "store_code_uc", all.x = T)
+#   annual_tp <- annual_tp[channel_code %in% c("M", "F", "D")]
+#
+#   annual_tp[, year := year]
+#   tp_sales_panel <- rbind(tp_sales_panel, annual_tp)
+# }
 
-  annual_tp[, year := year]
-  tp_sales_panel <- rbind(tp_sales_panel, annual_tp)
-}
-
+#fwrite(tp_sales_panel, file = "Data/Nielsen/allyears_toilet_tissue.csv")
+tp_sales_panel <- fread("Data/Nielsen/allyears_toilet_tissue.csv")
+master_products <- as.data.table(read.dta13("Data/Nielsen/master_products.dta"))
+tp_sales_panel <- merge(tp_sales_panel, master_products, all.x = T)
 fwrite(tp_sales_panel, file = "Data/Nielsen/allyears_toilet_tissue.csv")
 
 # limit data to Maricopa County, AZ
