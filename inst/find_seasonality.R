2ba1986907db2d659ff6d0c3d8d483af442d9c5f
John Bonney <31965225+johnnybonney@users.noreply.github.com>
2018-12-14 10:41:47 -0600

    Take out linear time trend before calculating seasonality

--- inst/find_seasonality.R
+++ inst/find_seasonality.R
@@ -18,10 +18,19 @@ sales_panel <- make_fixed_weights(panel_data = sales_panel,
                                   weight_var = "sales",
                                   panel_unit_vars = c("fips_state", "fips_county", "store_code_uc", "product_module_code"))
 sales_panel[, ln_total_sales := log(sales / sales.weight)]
+sales_panel[, linear_quarter := (year - 2008) * 4 + quarter]
 
-sales_panel[, season_effect := mean(ln_total_sales),
+## remove linear time trend
+sales_panel <- residualize_outcome(input_data = sales_panel,
+                                   outcome_var = ln_total_sales,
+                                   discrete_vars = NULL,
+                                   continuous_vars = linear_quarter,
+                                   weight_var = NULL)
+
+sales_panel[, season_effect := mean(ln_total_sales_residual),
             by = .(product_module_code, quarter)]
 
+## calculate season-product effects
 sales_collapsed <- sales_panel[, list(max_season = max(season_effect),
                                       min_season = min(season_effect)),
                                by = product_module_code]
