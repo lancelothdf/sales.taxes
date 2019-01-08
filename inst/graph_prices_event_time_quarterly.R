e38fe1c1e640d13cdfbdf3d84067ba53ed6b8c6c
John Bonney <31965225+johnnybonney@users.noreply.github.com>
2019-01-08 10:30:06 -0600

    Initial commit of quarterly price files

---
+++ inst/graph_prices_event_time_quarterly.R
@@ -0,0 +1,131 @@
+#' Maintained by: John Bonney
+#' Last modified: 11/21/2018
+#'
+#' Graphs:
+#' Plot log of normalized prices by event time
+#'
+
+rm(list=ls())
+wd <- "/project2/igaarder"
+setwd(wd)
+
+library(sales.taxes)
+library(readstata13)
+library(data.table)
+library(zoo)
+library(ggplot2)
+
+county_monthly_tax <- fread("Data/county_monthly_tax_rates.csv")
+county_monthly_tax <- county_monthly_tax[, .(fips_state, fips_county, year, month, sales_tax)]
+
+all_nielsen_data <- fread("Data/Nielsen/allyears_module_store_level.csv")
+all_nielsen_data[, price := sales / quantity]
+
+## incorporate tax rates
+all_nielsen_data <- merge_tax_rates(sales_data = all_nielsen_data,
+                                    keep_taxable_only = T,
+                                    county_monthly_tax_data = county_monthly_tax)
+all_nielsen_data <- balance_panel_data(all_nielsen_data,
+                                       panel_unit = "store_code_uc",
+                                       n_periods = 84)
+
+all_nielsen_data[, price_w_tax := (1 + applicable_tax) * price]
+all_nielsen_data[, quarter := ceiling(month / 3)]
+all_nielsen_data <- all_nielsen_data[, list(price_w_tax = mean(price_w_tax),
+                                            price = mean(price),
+                                            sales = sum(sales)),
+                                     by = c("fips_state", "fips_county", "product_group_code",
+                                            "store_code_uc", "product_module_code", "quarter", "year")]
+
+# creates a weighted sales variable called sales.weight
+all_nielsen_data <- make_fixed_weights(all_nielsen_data,
+                                       weight_time = list(year = 2008, quarter = 1),
+                                       weight_var = "sales",
+                                       panel_unit_vars = c("fips_state", "fips_county", "store_code_uc", "product_module_code"))
+
+county_pop <- fread("Data/county_population.csv")
+
+## at this point, we have
+## all_nielsen_data -- contains price and price_w_tax on module-store-month level
+
+county_module_weights <- all_nielsen_data[year == 2008 & quarter == 1,
+                                          list(cty_base_sales = sum(sales)),
+                                          by = c("fips_state", "fips_county", "product_module_code")]
+
+### COMPREHENSIVE DEF ###
+es_price_application(all_nielsen_data,
+                     treatment_data_path = "Data/event_study_tr_groups_comprehensive.csv",
+                     county_pop_data = county_pop,
+                     county_sales_weights = NULL,
+                     weighting_var = "population",
+                     price_var = "mld_price",
+                     w_tax = F,
+                     fig_outfile = "Graphs/log_price_trends_compr_pretax_es.png")
+
+es_price_application(all_nielsen_data,
+                     treatment_data_path = "Data/event_study_tr_groups_comprehensive.csv",
+                     county_pop_data = county_pop,
+                     county_sales_weights = NULL,
+                     weighting_var = "population",
+                     price_var = "mld_price_w_tax",
+                     w_tax = T,
+                     fig_outfile = "Graphs/log_price_trends_compr_posttax_es.png")
+
+### RESTRICTIVE DEFINITION ###
+# es_price_application(all_nielsen_data,
+#                      treatment_data_path = "Data/event_study_tr_groups_restrictive.csv",
+#                      county_pop_data = county_pop,
+#                      county_sales_weights = NULL,
+#                      weighting_var = "population",
+#                      price_var = "mld_price",
+#                      w_tax = F,
+#                      fig_outfile = "Graphs/log_price_trends_restr_pretax_es.png")
+#
+# es_price_application(all_nielsen_data,
+#                      treatment_data_path = "Data/event_study_tr_groups_restrictive.csv",
+#                      county_pop_data = county_pop,
+#                      county_sales_weights = NULL,
+#                      weighting_var = "population",
+#                      price_var = "mld_price_w_tax",
+#                      w_tax = T,
+#                      fig_outfile = "Graphs/log_price_trends_restr_posttax_es.png")
+
+## Weighted plots
+### COMPREHENSIVE DEFINITION ###
+es_price_application(all_nielsen_data,
+                     treatment_data_path = "Data/event_study_tr_groups_comprehensive.csv",
+                     county_pop_data = NULL,
+                     county_sales_weights = county_module_weights,
+                     weighting_var = "cty_base_sales",
+                     price_var = "mld_price",
+                     w_tax = F,
+                     fig_outfile = "Graphs/log_price_trends_compr_pretax_wtd_es.png")
+
+es_price_application(all_nielsen_data,
+                     treatment_data_path = "Data/event_study_tr_groups_comprehensive.csv",
+                     county_pop_data = NULL,
+                     county_sales_weights = county_module_weights,
+                     weighting_var = "cty_base_sales",
+                     price_var = "mld_price_w_tax",
+                     w_tax = T,
+                     fig_outfile = "Graphs/log_price_trends_compr_posttax_wtd_es.png")
+
+### RESTRICTIVE DEFINITION ###
+# es_price_application(all_nielsen_data,
+#                      treatment_data_path = "Data/event_study_tr_groups_restrictive.csv",
+#                      county_pop_data = NULL,
+#                      county_sales_weights = county_module_weights,
+#                      weighting_var = "cty_base_sales",
+#                      price_var = "mld_price",
+#                      w_tax = F,
+#                      fig_outfile = "Graphs/log_price_trends_restr_pretax_wtd_es.png")
+#
+# es_price_application(all_nielsen_data,
+#                      treatment_data_path = "Data/event_study_tr_groups_restrictive.csv",
+#                      county_pop_data = NULL,
+#                      county_sales_weights = county_module_weights,
+#                      weighting_var = "cty_base_sales",
+#                      price_var = "mld_price_w_tax",
+#                      w_tax = T,
+#                      fig_outfile = "Graphs/log_price_trends_restr_posttax_wtd_es.png")
+
---
+++ inst/graph_prices_residualized_quarterly.R
@@ -0,0 +1,211 @@
+#' Maintained by: John Bonney
+#'
+#' Graphs:
+#' Plot residualized log of normalized prices by calendar time
+#'
+
+rm(list=ls())
+wd <- "/project2/igaarder"
+setwd(wd)
+
+library(sales.taxes)
+library(readstata13)
+library(data.table)
+library(zoo)
+library(ggplot2)
+
+county_monthly_tax <- fread("Data/county_monthly_tax_rates.csv")
+county_monthly_tax <- county_monthly_tax[, .(fips_state, fips_county, year, month, sales_tax)]
+
+all_nielsen_data <- fread("Data/Nielsen/allyears_module_store_level.csv")
+all_nielsen_data[, price := sales / quantity]
+
+# Merge on the tax rates (including exemptions)
+all_nielsen_data <- merge_tax_rates(sales_data = all_nielsen_data,
+                                    keep_taxable_only = T,
+                                    county_monthly_tax_data = county_monthly_tax)
+all_nielsen_data <- balance_panel_data(all_nielsen_data,
+                                       panel_unit = "store_code_uc",
+                                       n_periods = 84)
+all_nielsen_data[, price_w_tax := (1 + applicable_tax) * price]
+
+## transform data to quarterly
+all_nielsen_data[, quarter := ceiling(month / 3)]
+all_nielsen_data <- all_nielsen_data[, list(price_w_tax = mean(price_w_tax),
+                                            price = mean(price),
+                                            sales = sum(sales)),
+                                     by = c("fips_state", "fips_county", "product_group_code",
+                                            "store_code_uc", "product_module_code", "quarter", "year")]
+all_nielsen_data[, month := quarter] # a bit of a fishy workaround (temporary) for normalizing price on quarterly level
+
+all_nielsen_data <- normalize_price(price_data = all_nielsen_data,
+                                    time_type = "calendar",
+                                    base_time = c(2008, 1),
+                                    price_var = "price",
+                                    new_price_var = "normalized_price")
+
+
+all_nielsen_data <- normalize_price(price_data = all_nielsen_data,
+                                    time_type = "calendar",
+                                    base_time = c(2008, 1),
+                                    price_var = "price_w_tax",
+                                    new_price_var = "normalized_price_w_tax")
+# creates a weighted sales variable called sales.weight
+all_nielsen_data <- make_fixed_weights(all_nielsen_data,
+                                       weight_time = list(year = 2008, month = 1),
+                                       weight_var = "sales",
+                                       panel_unit_vars = c("fips_state", "fips_county", "store_code_uc", "product_module_code"))
+
+# Aggregate to county x product level
+# product_by_county_prices <- all_nielsen_data[, list(mld_price = mean(normalized_price),
+#                                                     mld_price_w_tax = mean(normalized_price_w_tax),
+#                                                     mld_price.wtd = weighted.mean(x = normalized_price,
+#                                                                                   w = sales.weight),
+#                                                     mld_price_w_tax.wtd = weighted.mean(x = normalized_price_w_tax,
+#                                                                                         w = sales.weight),
+#                                                     total_sales = sum(sales),
+#                                                     n_stores = .N),
+#                                              by = c("fips_state", "fips_county",
+#                                                     "product_module_code", "product_group_code",
+#                                                     "month", "year")]
+#
+# product_by_county_prices[, cty_base_sales := max(total_sales * as.integer(year == 2008 & month == 1)),
+#                          by = c("fips_state", "fips_county", "product_module_code")]
+# fwrite(product_by_county_prices, "Data/Nielsen/product_by_county_prices_.csv")
+#
+# product_by_county_prices <- fread("Data/Nielsen/product_by_county_prices.csv")
+
+# county_pop <- fread("Data/county_population.csv")
+# product_by_county_prices <- merge(product_by_county_prices,
+#                                   county_pop,
+#                                   by = c("fips_state", "fips_county"))
+################################
+### basic linear time trend ###
+###############################
+
+### COMPREHENSIVE DEFINITION ###
+price_application(all_nielsen_data,
+                  treatment_data_path = "Data/tr_groups_comprehensive.csv",
+                  time = "calendar",
+                  weighting_var = "sales.weight",
+                  pretax_var = "price",
+                  posttax_var = "price_w_tax",
+                  resid_type = "A",
+                  w_tax = T,
+                  fig_outfile = "Graphs/log_price_trends_compr_posttax_qly_A.png")
+
+### RESTRICTIVE DEFINITION ###
+# price_application(product_by_county_prices,
+#                   treatment_data_path = "Data/tr_groups_restrictive.csv",
+#                   time = "calendar",
+#                   weighting_var = "cty_base_sales",
+#                   pretax_var = "mld_price.wtd",
+#                   posttax_var = "mld_price_w_tax.wtd",
+#                   resid_type = "A",
+#                   w_tax = T,
+#                   fig_outfile = "Graphs/log_price_trends_restr_posttax_A.png")
+
+#####################################
+### group-level linear time trend ###
+####################################
+
+### COMPREHENSIVE DEFINITION ###
+price_application(all_nielsen_data,
+                  treatment_data_path = "Data/tr_groups_comprehensive.csv",
+                  time = "calendar",
+                  weighting_var = "sales.weight",
+                  pretax_var = "price",
+                  posttax_var = "price_w_tax",
+                  resid_type = "B",
+                  w_tax = T,
+                  fig_outfile = "Graphs/log_price_trends_compr_posttax_qly_B.png")
+
+### RESTRICTIVE DEFINITION ###
+# price_application(product_by_county_prices,
+#                   treatment_data_path = "Data/tr_groups_restrictive.csv",
+#                   time = "calendar",
+#                   weighting_var = "cty_base_sales",
+#                   pretax_var = "mld_price.wtd",
+#                   posttax_var = "mld_price_w_tax.wtd",
+#                   resid_type = "B",
+#                   w_tax = T,
+#                   fig_outfile = "Graphs/log_price_trends_restr_posttax_B.png")
+
+###########################################
+### linear time trend and month effects ###
+###########################################
+
+### COMPREHENSIVE DEFINITION ###
+price_application(all_nielsen_data,
+                  treatment_data_path = "Data/tr_groups_comprehensive.csv",
+                  time = "calendar",
+                  weighting_var = "sales.weight",
+                  pretax_var = "price",
+                  posttax_var = "price_w_tax",
+                  resid_type = "C",
+                  w_tax = T,
+                  fig_outfile = "Graphs/log_price_trends_compr_posttax_qly_C.png")
+
+### RESTRICTIVE DEFINITION ###
+# price_application(product_by_county_prices,
+#                   treatment_data_path = "Data/tr_groups_restrictive.csv",
+#                   time = "calendar",
+#                   weighting_var = "cty_base_sales",
+#                   pretax_var = "mld_price.wtd",
+#                   posttax_var = "mld_price_w_tax.wtd",
+#                   resid_type = "C",
+#                   w_tax = T,
+#                   fig_outfile = "Graphs/log_price_trends_restr_posttax_C.png")
+
+##################################################################
+### linear time trend and month effects on product-group level ###
+##################################################################
+
+### COMPREHENSIVE DEFINITION ###
+price_application(all_nielsen_data,
+                  treatment_data_path = "Data/tr_groups_comprehensive.csv",
+                  time = "calendar",
+                  weighting_var = "sales.weight",
+                  pretax_var = "price",
+                  posttax_var = "price_w_tax",
+                  resid_type = "D",
+                  w_tax = T,
+                  fig_outfile = "Graphs/log_price_trends_compr_posttax_qly_D.png")
+
+### RESTRICTIVE DEFINITION ###
+# price_application(product_by_county_prices,
+#                   treatment_data_path = "Data/tr_groups_restrictive.csv",
+#                   time = "calendar",
+#                   weighting_var = "cty_base_sales",
+#                   pretax_var = "mld_price.wtd",
+#                   posttax_var = "mld_price_w_tax.wtd",
+#                   resid_type = "D",
+#                   w_tax = T,
+#                   fig_outfile = "Graphs/log_price_trends_restr_posttax_D.png")
+
+
+#############################
+### calendar time effects ###
+#############################
+
+### COMPREHENSIVE DEFINITION ###
+price_application(all_nielsen_data,
+                  treatment_data_path = "Data/tr_groups_comprehensive.csv",
+                  time = "calendar",
+                  weighting_var = "sales.weight",
+                  pretax_var = "price",
+                  posttax_var = "price_w_tax",
+                  resid_type = "E",
+                  w_tax = T,
+                  fig_outfile = "Graphs/log_price_trends_compr_posttax_qly_E.png")
+#
+# ### RESTRICTIVE DEFINITION ###
+# price_application(product_by_county_prices,
+#                   treatment_data_path = "Data/tr_groups_restrictive.csv",
+#                   time = "calendar",
+#                   weighting_var = "cty_base_sales",
+#                   pretax_var = "mld_price.wtd",
+#                   posttax_var = "mld_price_w_tax.wtd",
+#                   resid_type = "E",
+#                   w_tax = T,
+#                   fig_outfile = "Graphs/log_price_trends_restr_posttax_E.png")
