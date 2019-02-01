3f210facc2422ba5f40092d336d84a10350ff3d6
John Bonney <31965225+johnnybonney@users.noreply.github.com>
2019-02-01 15:11:39 -0600

    Balance the sales-weighted indices

--- inst/price_indices/plot_national_index2.R
+++ inst/price_indices/plot_national_index2.R
@@ -56,6 +56,8 @@ cpi_data <- base::rbind.data.frame(ungroup(cpi_data),
                                    pi_county_avg,
                                    pi_national)
 
+pi_first_set <- cpi_data
+
 ggplot(mapping = aes(x = t, y = chained_food_cpi, linetype = index, color = index)) +
   geom_line(data = cpi_data, aes(group = index), size = 1) +
   scale_x_yearqtr(format = "%Y Q%q", expand = c(0.01, 0.01), n = 6) +
@@ -85,5 +87,107 @@ ggplot(mapping = aes(x = t, y = chained_food_cpi, linetype = index, color = inde
 
 ggsave(
   "C:/Users/John Bonney/Desktop/Magne_projects/sales_taxes/reports/figs/comparing2.png",
+  height = 120, width = 180, units = "mm"
+)
+
+##########################################
+##### simple sales-weighted average ######
+##########################################
+
+### Import sales-weighted avg
+sw_avg <- read.csv("C:/Users/John Bonney/Desktop/Magne_projects/sales_taxes/output/server/simple_sales_weighted_pi.csv")
+sw_avg$t <- with(sw_avg, as.yearqtr(paste0(year, " Q", quarter)))
+
+sw_avg <- sw_avg %>%
+  select(year, quarter, chained_food_cpi = natl_avg, t) %>%
+  mutate(index = "sales_avg")
+
+base_index <- sw_avg %>%
+  filter(year == 2006, quarter == 4) %>%
+  select(chained_food_cpi) %>% pull()
+sw_avg <- sw_avg %>% mutate(chained_food_cpi = chained_food_cpi / base_index)
+
+cpi_data <- read.csv("CPI/national_monthly_food_beverage_chained_cpi.csv") %>%
+  as.tibble() %>% filter(between(year, 2006, 2016)) %>%
+  mutate(quarter = as.integer(ceiling(month / 3))) %>%
+  group_by(year, quarter) %>%
+  summarize(chained_food_cpi = mean(chained_food_cpi))
+cpi_data$t <- with(cpi_data, as.yearqtr(paste0(year, " Q", quarter)))
+
+base_cpi <- cpi_data %>% ungroup() %>%
+  filter(year == 2006, quarter == 4) %>%
+  select(chained_food_cpi) %>% pull()
+cpi_data <- cpi_data %>%
+  filter(year > 2006 | quarter == 4) %>%
+  mutate(chained_food_cpi = chained_food_cpi / base_cpi,
+         index = "cpi")
+
+cpi_data <- base::rbind.data.frame(ungroup(cpi_data), sw_avg)
+
+ggplot(mapping = aes(x = t, y = chained_food_cpi, linetype = index, color = index)) +
+  geom_line(data = cpi_data, aes(group = index), size = 1) +
+  scale_x_yearqtr(format = "%Y Q%q", expand = c(0.01, 0.01), n = 6) +
+  scale_y_continuous(breaks = seq(0, 1.4, .1), expand = c(0.005, 0.005)) +
+  theme_bw() +
+  labs(y = expression(bold("Price Index (Normalized 2006 Q4 = 1.00)")), x = expression(bold("Year-Quarter"))) +
+  scale_linetype_manual(name = NULL, breaks = c("cpi", "sales_avg"),
+                        labels = c("Food & Beverage Chained CPI", "Sales-weighted Average Index"),
+                        values = c("3344", "solid")) +
+  scale_color_manual(name = NULL, breaks = c("cpi", "sales_avg"),
+                     labels = c("Food & Beverage Chained CPI", "Sales-weighted Average Index"),
+                     values = c("#F8766D", "black")) +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(size = 0.1, colour = 'grey'),
+    panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+    strip.background = element_rect(colour="white", fill="white"),
+    plot.title = element_text(hjust = 0.5, size = 11), panel.spacing = unit(2, "lines"),
+    legend.position = c(0.7, 0.15), axis.ticks.length=unit(-0.15, "cm"),
+    legend.margin = margin(t=-.2, r=0, b=-.2, l=0, unit="cm"),
+    axis.text.x = element_text(margin=unit(rep(0.3, 4), "cm")),
+    axis.text.y = element_text(margin=unit(rep(0.3, 4), "cm")),
+    axis.text.y.right = element_text(margin=unit(rep(0.3, 4), "cm"))
+  )
+
+ggsave(
+  "C:/Users/John Bonney/Desktop/Magne_projects/sales_taxes/reports/figs/comparing3.png",
+  height = 120, width = 180, units = "mm"
+)
+
+##########################################
+########## combining the two #############
+##########################################
+
+pi_combined <- rbind(pi_first_set, sw_avg)
+
+ggplot(mapping = aes(x = t, y = chained_food_cpi, linetype = index, color = index)) +
+  geom_line(data = pi_combined, aes(group = index), size = 1) +
+  scale_x_yearqtr(format = "%Y Q%q", expand = c(0.01, 0.01), n = 6) +
+  scale_y_continuous(breaks = seq(0, 1.4, .1), expand = c(0.005, 0.005)) +
+  theme_bw() +
+  labs(y = expression(bold("Price Index (Normalized 2006 Q4 = 1.00)")), x = expression(bold("Year-Quarter"))) +
+  scale_linetype_manual(name = NULL, breaks = c("cpi", "county_avg", "national_index", "sales_avg"),
+                        labels = c("Food & Beverage Chained CPI", "Retail Scanner Index (county average)",
+                                   "Retail Scanner Index (national index)", "Sales-weighted Average Index"),
+                        values = c("3344", "solid", "4433", "11")) +
+  scale_color_manual(name = NULL, breaks = c("cpi", "county_avg", "national_index", "sales_avg"),
+                     labels = c("Food & Beverage Chained CPI", "Retail Scanner Index (county average)",
+                                "Retail Scanner Index (national index)", "Sales-weighted Average Index"),
+                     values = c("#F8766D", "black", "#00BA38", "#619CFF")) +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(size = 0.1, colour = 'grey'),
+    panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+    strip.background = element_rect(colour="white", fill="white"),
+    plot.title = element_text(hjust = 0.5, size = 11), panel.spacing = unit(2, "lines"),
+    legend.position = c(0.7, 0.25), axis.ticks.length=unit(-0.15, "cm"),
+    legend.margin = margin(t=-.2, r=0, b=-.2, l=0, unit="cm"),
+    axis.text.x = element_text(margin=unit(rep(0.3, 4), "cm")),
+    axis.text.y = element_text(margin=unit(rep(0.3, 4), "cm")),
+    axis.text.y.right = element_text(margin=unit(rep(0.3, 4), "cm"))
+  )
+
+ggsave(
+  "C:/Users/John Bonney/Desktop/Magne_projects/sales_taxes/reports/figs/comparing_all.png",
   height = 120, width = 180, units = "mm"
 )
--- inst/price_indices/simple_sales_weighted_pi.R
+++ inst/price_indices/simple_sales_weighted_pi.R
@@ -5,6 +5,10 @@ library(data.table)
 
 setwd("/project2/igaarder")
 
+###########################################
+####### balancing on store-level ##########
+###########################################
+
 ## load sales data
 sales_data <- fread("Data/sales_quarterly_2006-2016.csv")
 ## keep only 2006 Q4 sales
@@ -15,6 +19,12 @@ sales_data <- sales_data[, .(store_code_uc, product_module_code, sales)]
 pi_data <- fread("Data/Nielsen/price_quantity_indices_food.csv")
 pi_data <- pi_data[year >= 2007 | (year == 2006 & quarter == 4)]
 
+## balance on store-level
+stores <- unique(pi_data, by = c("year", "quarter", "store_code_uc"))
+stores <- stores[, list(n = .N), by = store_code_uc]
+keep_stores <- stores[n == (2016 - 2006) * 4 + 1][["store_code_uc"]]
+pi_data <- pi_data[store_code_uc %in% keep_stores]
+
 ## merge on sales data
 pi_data <- merge(pi_data, sales_data, by = c("product_module_code", "store_code_uc"))
 
@@ -22,3 +32,21 @@ pi_data <- merge(pi_data, sales_data, by = c("product_module_code", "store_code_
 national_avg <- pi_data[, list(natl_avg = weighted.mean(x = cpricei, w = sales)),
                         by = .(year, quarter)]
 fwrite(national_avg, "Data/simple_sales_weighted_pi.csv")
+
+##################################################
+####### balancing on store-module-level ##########
+##################################################
+
+## balance on module X store level
+store_modules <- pi_data[, list(n = .N), by = .(store_code_uc, product_module_code)]
+keep_store_modules <- store_modules[n == (2016 - 2006) * 4 + 1]
+keep_store_modules[, balanced := TRUE]
+
+pi_data <- merge(pi_data, keep_store_modules,
+                 by = c("store_code_uc", "product_module_code"))
+pi_data[, balanced := ifelse(is.na(balanced), FALSE, balanced)]
+pi_data <- pi_data[balanced == TRUE]
+
+national_avg2 <- pi_data[, list(natl_avg = weighted.mean(x = cpricei, w = sales)),
+                        by = .(year, quarter)]
+fwrite(national_avg2, "Data/simple_sales_weighted_pi_modbalanced.csv")
