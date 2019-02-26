0fbd64820e2d51afa0e36d04e17e7759ebfb33e5
John Bonney <31965225+johnnybonney@users.noreply.github.com>
2019-02-26 14:43:23 -0600

    Incorporate 2014 reforms + bug fix

--- inst/price_indices/cohorts/cohort_specific_2009Q1.R
+++ inst/price_indices/cohorts/cohort_specific_2009Q1.R
@@ -2,23 +2,131 @@
 #'
 #' This file groups tax-change cohorts by quarters, and focuses on 2009 Q1 cohort.
 
+library(tidyverse)
 library(data.table)
 library(readstata13)
 library(sales.taxes)
 library(zoo)
-library(ggplot2)
 
 setwd("/project2/igaarder")
 
 ## useful filepaths ------------------------------------------------------------
-eventstudy_tr_path <- "Data/event_study_tr_groups_comprehensive.csv"
-tr_groups_path <- "Data/tr_groups_comprehensive.csv"
+eventstudy_tr_path <- "Data/event_study_tr_groups_comprehensive_w2014.csv"
+tr_groups_path <- "Data/tr_groups_comprehensive_w2014.csv"
 sales_data_path <- "Data/sales_quarterly_2006-2016.csv"
 tax_rates_path <- "Data/county_monthly_tax_rates.csv"
 quarterly_tax_path <- "Data/quarterly_tax_rates.csv"
 module_exemptions_path <- "Data/modules_exemptions_long.csv"
 all_goods_pi_path <- "Data/Nielsen/price_quantity_indices_allitems.csv"
 taxable_pi_path <- "Data/Nielsen/price_quantity_indices_taxableitems.csv"
+expanded.reforms.path <- "Data/tax_reforms_all_incl2014.csv"
+
+## prepare tax reforms ---------------------------------------------------------
+
+tax_panel <- read.csv(tax_rates_path)
+tax_panel$year_month <- as.yearmon(paste(tax_panel$year,
+                                         tax_panel$month), "%Y %m")
+
+tax_reforms <- read.csv(expanded.reforms.path)
+
+tax_reforms <- tax_reforms %>% group_by(fips_state, fips_county) %>%
+  summarize(ever_increase = as.integer(max(dtax) > 0),
+            ever_decrease = as.integer(min(dtax) < 0))
+
+tr_groups <- data.frame(ever_increase = c(0, 0, 1, 1),
+                        ever_decrease = c(0, 1, 0, 1),
+                        tr_group = c("No change",
+                                     "Decrease only",
+                                     "Increase only",
+                                     "Increase & decrease"))
+tax_reforms <- merge(tax_reforms, tr_groups,
+                     by = c("ever_increase", "ever_decrease"))
+
+raw_table <- table(tax_reforms$tr_group)
+
+tax_increase <- tax_reforms %>%
+  filter(tr_group %in% c("Increase & decrease", "Increase only")) %>%
+  mutate(tr_group = "Ever increase")
+
+tax_decrease <- tax_reforms %>%
+  filter(tr_group %in% c("Increase & decrease", "Decrease only")) %>%
+  mutate(tr_group = "Ever decrease")
+
+tax_treatment <- rbind(tax_reforms %>% filter(tr_group == "Increase only"),
+                       tax_decrease,
+                       tax_increase) %>%
+  select(fips_state, fips_county, tr_group)
+
+duplicates_table <- table(tax_treatment$tr_group)
+# Check that we have duplicated correctly
+all(duplicates_table["Increase only"] == raw_table["Increase only"],
+    duplicates_table["Ever decrease"] == raw_table["Decrease only"] +
+      raw_table["Increase & decrease"],
+    duplicates_table["Ever increase"] == raw_table["Increase only"] +
+      raw_table["Increase & decrease"])
+
+## create new datasets
+tax_reforms_es <- read.csv(tax_reforms_file)
+
+tax_reforms_es <- tax_reforms_es %>%
+  mutate(change = ifelse(dtax < 0, "Decrease", "Increase"),
+         time_of_event = 12 * ref_year + ref_month) %>%
+  select(fips_state, fips_county, ref_year, ref_month, dtax, change,
+         time_of_event, event_id)
+
+event_df <- merge(tax_panel %>% select(fips_state, fips_county,
+                                       year, month, sales_tax),
+                  tax_reforms_es,
+                  by = c("fips_state", "fips_county"), all.y = T) %>%
+  mutate(time_since_event = (12 * year + month) - time_of_event) %>%
+  filter(between(time_since_event, -12, 12)) %>%  # this was adjusted on 1/3/2019 to accommodate more reforms
+  select(fips_state, fips_county, year, month, time_since_event, sales_tax, dtax,
+         ref_year, ref_month, change, event_id) %>%
+  arrange(event_id, time_since_event)
+
+event_df <- event_df %>% group_by(event_id) %>%
+  mutate(n = n()) %>%
+  filter(n == 25, between(ref_year, 2009, 2014)) # this was adjusted on 2/26/2019 to accommodate more reforms
+
+write.csv(distinct(event_df %>% select(event_id, fips_state, fips_county,
+                                       ref_year, ref_month, change)),
+          "output/tr_events_w2014.csv")
+
+event_df <- merge(event_df, tax_treatment,
+                  by = c("fips_state", "fips_county"), all = T) %>%
+  filter(change == "Increase" & tr_group %in% c("Ever increase", "Increase only") |
+           change == "Decrease" & tr_group == "Ever decrease")
+
+write.csv(distinct(event_df %>% select(fips_state, fips_county, ref_year,
+                                       ref_month, tr_group)), eventstudy_tr_path)
+
+counties_to_keep <- event_df %>% ungroup() %>%
+  select(fips_state, fips_county, tr_group) %>%
+  distinct() %>%
+  mutate(balanced = TRUE)
+
+event_ts_N1 <- event_df %>%
+  group_by(time_since_event, tr_group) %>%
+  summarize(mean_tax = weighted.mean(x = sales_tax,
+                                     w = population),
+            count = n())
+
+event_ts_N1 <- event_ts_N1 %>% group_by(tr_group) %>%
+  mutate(tr_count = paste0(tr_group, " (n = ", mean(count), ")"))
+
+tax_panel_N1 <- merge(tax_panel, tax_treatment,
+                      by = c("fips_county", "fips_state"), all = T)
+tax_panel_N1$tr_group <- tax_panel_N1$tr_group %>% replace_na("No change")
+tax_panel_N1 <- merge(tax_panel_N1, county_weights,
+                      by = c("fips_county", "fips_state"), all.x = T)
+tax_panel_N1 <- merge(tax_panel_N1, counties_to_keep,
+                      by = c("fips_county", "fips_state", "tr_group"), all.x = T)
+save_counties <- tax_panel_N1 %>%
+  select(fips_county, fips_state, tr_group, balanced) %>%
+  distinct() %>%
+  mutate(balanced = ifelse(tr_group == "No change", TRUE, balanced))
+
+write.csv(save_counties, tr_groups_path)
 
 ## load in and clean data ------------------------------------------------------
 all_pi <- fread(all_goods_pi_path)
@@ -44,7 +152,8 @@ setkey(all_pi, fips_county, fips_state)
 tr.events <- fread(eventstudy_tr_path)
 tr.events <- tr.events[between(ref_year, 2009, 2014) & ref_month %in% 1:3 & tr_group == "Ever increase"]
 tr.events[, n_events := .N, by = .(fips_state, fips_county)]
-tr.events <- tr.events[, .(fips_county, fips_state, ref_year, n_events)]
+tr.events[, cohort_size := sum(1 / n_events), by = .(ref_year)]
+tr.events <- tr.events[, .(fips_county, fips_state, ref_year, n_events, cohort_size)]
 setnames(tr.events, "ref_year", "treatment_year")
 setkey(tr.events, fips_county, fips_state)
 
@@ -99,7 +208,7 @@ taxable_pi.09Q1.collapsed <- taxable_pi.09Q1[, list(
         `2011` = weighted.mean(`2011`, w = base.sales * event.weight),
         `2012` = weighted.mean(`2012`, w = base.sales * event.weight),
         `2013` = weighted.mean(`2013`, w = base.sales * event.weight),
-#        `2014` = weighted.mean(`2014`, w = base.sales * event.weight),
+        `2014` = weighted.mean(`2014`, w = base.sales * event.weight),
    `No change` = weighted.mean(`No change`, w = base.sales * event.weight)
   ), by = .(year, quarter)]
 
@@ -107,17 +216,15 @@ setnames(taxable_pi.09Q1.collapsed, "mean.cpricei", "2009")
 taxable_pi.09Q1.collapsed <- tidyr::gather(taxable_pi.09Q1.collapsed,
                                            key = cohort, value = cpricei,
                                            c(`2009`, `2010`, `2011`,
-                                             `2012`, `2013`, `No change`)) # deleted 2014 from here as well
+                                             `2012`, `2013`, `2014`, `No change`))
 
 ## attach cohort sizes
 n_counties.nochange <- uniqueN(tr.groups[tr_group == "No change"], by = c("fips_state", "fips_county"))
-n_counties.other <- unique(tr.events[, .(fips_state, fips_county, n_events)],
-                           by = c("fips_state", "fips_county", "n_events"))
-n_counties.other[, ones := 1]
-n_counties.other <- n_counties.other[, list(n_counties = sum(ones / n_events)), by = treatment_year]
+n_counties.other <- unique(tr.events[, .(treatment_year, cohort_size)],
+                           by = c("treatment_year", "cohort_size"))
 n_counties.other[, treatment_year := as.character(treatment_year)]
 n_counties <- rbind(n_counties.other,
-                    data.table(treatment_year = "No change", n_counties = n_counties.nochange))
+                    data.table(treatment_year = "No change", cohort_size = n_counties.nochange))
 setnames(n_counties, "treatment_year", "cohort")
 taxable_pi.09Q1.collapsed <- merge(taxable_pi.09Q1.collapsed, n_counties,
                                    by = "cohort")
