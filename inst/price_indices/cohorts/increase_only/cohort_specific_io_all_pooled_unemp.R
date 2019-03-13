f7de60ba26efb21eaf138bd13af3aec91b9c5dae
John Bonney <31965225+johnnybonney@users.noreply.github.com>
2019-03-13 10:05:50 -0500

    Reorganization of pooled cohort code, with addition of ever decrease and increase only

---
+++ inst/price_indices/cohorts/increase_only/cohort_specific_io_all_pooled_unemp.R
@@ -0,0 +1,197 @@
+#' Author: John Bonney
+#'
+#' Now we should "pool" across cohorts for both a diff-in-diff and a pure event
+#' study design and see which one looks better.  Those would be similar graphs
+#' to the ones you have been doing recently.  We revert back to defining cohorts
+#' by quarters.
+
+#' 3b)  Plot mean price index (tax inclusive) for all counties that increased
+#' their taxes at some point (ever increase group).  Then build two alternative
+#' control groups: A) same as we have been doing - use an average of counties
+#' that never change their taxes "matching" calendar time and product.  B) for
+#' each "treated cohort" build the control group by taking all counties from the
+#' ever increase group that have not yet increased their taxes (but will in the
+#' future) "matching" on calendar time and product - average over the different
+#' control groups for each treated cohortXproduct.
+
+library(tidyverse)
+library(data.table)
+library(readstata13)
+library(sales.taxes)
+library(zoo)
+
+setwd("/project2/igaarder")
+prep.new.data <- F
+
+# check function
+g <- function(dt) {
+  print(head(dt))
+}
+
+change_of_interest <- "Increase only"
+
+
+## useful filepaths ------------------------------------------------------------
+eventstudy_tr_path <- "Data/event_study_tr_groups_comprehensive_w2014.csv"
+tr_groups_path <- "Data/tr_groups_comprehensive_w2014.csv"
+sales_data_path <- "Data/sales_monthly_2006-2016.csv"
+tax_rates_path <- "Data/county_monthly_tax_rates.csv"
+quarterly_tax_path <- "Data/quarterly_tax_rates.csv"
+module_exemptions_path <- "Data/modules_exemptions_long.csv"
+all_goods_pi_path <- "Data/Nielsen/price_quantity_indices_allitems_2006-2016_notaxinfo.csv"
+taxable_pi_path <- "Data/Nielsen/price_quantity_indices_taxableitems.csv"
+expanded.reforms.path <- "Data/tax_reforms_all_incl2014.csv"
+
+unemp_path <- "Data/covariates/county_monthly_unemp_clean.csv"
+
+county_weights <- fread("Data/county_population.csv")
+unemp_dt <- fread(unemp_path)
+unemp_dt <- unemp_dt[between(year, 2006, 2014)]
+
+###  attach sales --------------------------------------------------------------
+sales_data <- fread(sales_data_path)
+sales_data <- sales_data[, .(store_code_uc, product_module_code, fips_county,
+                             fips_state, month, year, sales)]
+sales_data <- sales_data[year <= 2014]
+sales_data <- sales_data[, list(sales = sum(sales)),
+                         by = .(fips_county, fips_state, month, year)]
+
+unemp_dt <- merge(unemp_dt, sales_data,
+                   by = c("fips_county", "fips_state", "month", "year"))
+rm(sales_data)
+gc()
+
+## prep the data ---------------------------------------------------------------
+
+## normalize
+unemp_dt[, normalized.rate := rate] # just for consistency in notation -- we don't actually normalized unemp
+unemp_dt[, base.sales := sales[year == 2008 & month == 1],
+       by = .(fips_county, fips_state)]
+unemp_dt <- unemp_dt[!is.na(normalized.rate) & !is.na(base.sales)]
+
+## balance on county level
+keep_counties <- unemp_dt[, list(n = .N), by = .(fips_state, fips_county)]
+keep_counties <- keep_counties[n == (2014 - 2005) * 12]
+
+setkey(unemp_dt, fips_county, fips_state)
+setkey(keep_counties, fips_county, fips_state)
+
+unemp_dt <- unemp_dt[keep_counties]
+
+## prep treatment events -------------------------------------------------------
+tr.events <- fread(eventstudy_tr_path)
+tr.events <- tr.events[tr_group == change_of_interest]
+tr.events[, treatment_month := 12 * ref_year + ref_month]
+## we are only interested in the first event
+tr.events[, min.event := (treatment_month == min(treatment_month)),
+          by = .(fips_state, fips_county)]
+tr.events <- tr.events[min.event == T]
+tr.events[, n_events := .N, by = .(fips_state, fips_county)]
+tr.events <- tr.events[, .(fips_county, fips_state, ref_year, ref_month, n_events)]
+# exclude 2012 Q4, 2013 Q1, 2013 Q2 reforms
+tr.events <- tr.events[!(ref_year == 2012 & ref_month >= 10) & !(ref_year == 2013 & ref_month %in% 1:6)]
+g(tr.events)
+
+setnames(tr.events, "ref_year", "treatment_year")
+setkey(tr.events, fips_county, fips_state)
+
+cohort_sizes <- tr.events[, list(cohort_size = .N), by = .(treatment_year, ref_month)]
+
+# for later use
+tr.groups <- fread(tr_groups_path)
+never.treated.master <- tr.groups[tr_group == "No change"]
+never.treated.master <- never.treated.master[, .(fips_state, fips_county)]
+setkey(never.treated.master, fips_state, fips_county)
+
+## iterate over all quarters and years -----------------------------------------
+master_res <- data.table(NULL)
+
+for (ref.year in 2009:2013) {
+  for (ref.month in 1:12) {
+    if ((ref.year == 2012 & ref.month %in% 10:12) | (ref.year == 2013 & ref.month %in% 1:6)) {
+      print(paste("Skipping", ref.year, "m", ref.month))
+      next
+    }
+    print(paste("Year:", ref.year, "; Month:", ref.month))
+    ## identify treated cohort -------------------------------------------------
+    tr.events.09m1 <- tr.events[treatment_year == ref.year & ref_month == ref.month]
+    setkey(tr.events.09m1, fips_county, fips_state)
+
+    unemp.09m1 <- unemp_dt[tr.events.09m1]
+    g(unemp.09m1)
+
+    ## prepare control data --------------------------------------------------------
+
+    ## identify never treated counties
+    never.treated <- unemp_dt[never.treated.master]
+    never.treated[, group := "No change"]
+    g(never.treated)
+
+    ## identify not-yet-treated (but future treated) counties
+    unemp_ss <- unemp_dt[tr.events[treatment_year > ref.year |
+                                 (treatment_year == ref.year &
+                                    ref_month > ref.month)]]
+    unemp_ss[, treatment_month := 12 * treatment_year + ref_month]
+    unemp_ss[, calendar_month := 12 * year + month]
+    unemp_ss[, not_yet_treated := min(treatment_month) > calendar_month,
+           by = c("fips_state", "fips_county")]
+
+    unemp_ss <- unemp_ss[not_yet_treated == TRUE]
+    unemp_ss[, not_yet_treated := NULL]
+    unemp_ss[, group := "Future"]
+    g(unemp_ss)
+
+    ## combine never treated + later cohorts
+    unemp_ss <- rbind(unemp_ss, never.treated, fill = T)
+    g(unemp_ss)
+    rm(never.treated)
+
+    # ss_pi[, event.weight := ifelse(is.na(n_events), 1, 1 / n_events)]
+
+    ## collapse to group x time level ------------------------------------------
+    unemp_ss.collapsed <- unemp_ss[, list(
+      control.rate = weighted.mean(normalized.rate, w = base.sales)
+    ), by = .(year, month, group)]
+    rm(unemp_ss)
+    g(unemp_ss.collapsed)
+
+    ## rearrange for simple merging of groups onto 2009 m1 cohort
+    unemp_ss.collapsed <- tidyr::spread(unemp_ss.collapsed, group, control.rate)
+    g(unemp_ss.collapsed)
+
+    ## merge onto the treated cohort by product
+    unemp.09m1 <- merge(unemp.09m1, unemp_ss.collapsed,
+                             by = c("year", "month"))
+    rm(unemp_ss.collapsed)
+
+    ## aggregate over calendar time ------------------------------------------------
+    g(unemp.09m1)
+
+    unemp.09m1.collapsed <- unemp.09m1[, list(
+      mean.rate = weighted.mean(normalized.rate, w = base.sales),
+      Future = weighted.mean(Future, w = base.sales),
+      `No change` = weighted.mean(`No change`, w = base.sales)
+    ), by = .(year, month)]
+    g(unemp.09m1.collapsed)
+
+    setnames(unemp.09m1.collapsed, "mean.rate", "Treated")
+    unemp.09m1.collapsed <- tidyr::gather(unemp.09m1.collapsed,
+                                               key = group, value = rate,
+                                               c(Treated, Future, `No change`))
+    g(unemp.09m1.collapsed)
+    unemp.09m1.collapsed <- as.data.table(unemp.09m1.collapsed)
+    unemp.09m1.collapsed <- unemp.09m1.collapsed[!is.na(rate)]
+    unemp.09m1.collapsed[, ref_year := ref.year]
+    unemp.09m1.collapsed[, ref_month := ref.month]
+    master_res <- rbind(master_res, unemp.09m1.collapsed)
+
+    rm(unemp.09m1.collapsed, unemp.09m1)
+  }
+}
+
+fwrite(master_res, "Data/unemprate_all_cohorts_io_pooled_extended.csv")
+
+setnames(cohort_sizes, "treatment_year", "ref_year")
+## merge on cohort size
+master_res <- merge(master_res, cohort_sizes, by = c("ref_year", "ref_month"))
+fwrite(master_res, "Data/unemprate_all_cohorts_io_pooled_extended.csv")
---
+++ inst/price_indices/plots/plot_cohort_specific_all_pooled_unemp.R
@@ -0,0 +1,126 @@
+#' Author: John Bonney
+#'
+#' Plot the pooled cohort-specific trends for quantity.
+
+setwd("C:/Users/John Bonney/Desktop/Magne_projects/sales_taxes")
+
+library(tidyverse)
+library(zoo)
+library(grid)
+library(data.table)
+
+element_grob.element_custom <- function(element, ...)  {
+
+  segmentsGrob(c(1,0,0),
+               c(0,0,1),
+               c(0,0,0),
+               c(0,1,1), gp=gpar(lwd=2))
+}
+## silly wrapper to fool ggplot2
+border_custom <- function(...){
+  structure(
+    list(...), # this ... information is not used
+    class = c("element_custom","element_blank", "element") # inheritance test workaround
+  )
+
+}
+
+data_path <- "output/server/pi_data/unemprate_all_cohorts_pooled_extended.csv"
+outfile_figpath <- "reports/figs/unemp_all_cohorts_pooled_extended.png"
+
+dt <- read.csv(data_path)
+
+dt$t <- as.yearmon(paste0(dt$year, "-", dt$month))
+dt$ref_t <- as.yearmon(paste0(dt$ref_year, "-", dt$ref_month))
+dt$tt_ev <- round((dt$t - dt$ref_t) * 12)
+
+dt <- as.data.table(dt)
+dt.agg <- dt[between(tt_ev, -36, 12)]
+dt.agg <- dt.agg[, rate := rate - rate[tt_ev == -2], by = .(group, ref_t)]
+dt.agg <- dt.agg[, list(rate.agg = weighted.mean(rate, cohort_size)),
+                 by = .(group, tt_ev)]
+
+dt.test <- dt[between(tt_ev, -36, 12) & ref_t <= 2013.5]
+dt.test <- dt.test[, rate := rate - rate[tt_ev == -2], by = .(group, ref_t)]
+dt.test <- dt.test[, list(rate.agg = weighted.mean(rate, cohort_size)),
+                   by = .(group, tt_ev)]
+
+ggplot(dt.test, mapping = aes(x = tt_ev, y = rate.agg, color = group)) +
+  geom_line(size = .7) +
+  geom_point(size = .8) +
+  geom_vline(xintercept = 0, color = "red", linetype = "22", alpha = .5) +
+  theme_bw() +
+  scale_x_continuous(expand = c(.01, -.05)) +
+  labs(x = "Month", y = "Unemployment Rate", color = "Cohort") +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(size = 0.1, colour = 'grey'),
+    panel.grid.minor = element_blank(),
+    strip.background = element_rect(colour="white", fill="white"),
+    panel.spacing = unit(2, "lines"),
+    panel.border = border_custom(),
+    legend.position = c(0.2, 0.8),
+    axis.ticks.length=unit(-0.15, "cm"),
+    legend.margin = margin(t=-.2, r=0, b=-.2, l=0, unit="cm"),
+    axis.text.x = element_text(margin=unit(rep(0.3, 4), "cm")),
+    axis.text.y = element_text(margin=unit(rep(0.3, 4), "cm"))
+  )
+
+ggsave(outfile_figpath, height = 120, width = 180, units = "mm")
+
+for (pp in sort(unique(dt$ref_t))) {
+  plot.dt <- dt %>%
+    filter(ref_t == pp, between(tt_ev, -36, 12)) %>%
+    group_by(group) %>%
+    mutate(quantityi = quantityi - quantityi[tt_ev == -2])
+
+  myplot <- ggplot(plot.dt, mapping = aes(x = t, y = quantityi, color = group)) +
+    geom_line(size = .7) +
+    geom_point(size = .8) +
+    geom_vline(xintercept = pp, color = "red", linetype = 1, alpha = .5) +
+    theme_bw() +
+    scale_x_yearqtr(format = "%Y Q%q", expand = c(.01, -.05)) +
+    labs(x = "Quarter", y = "Normalized Price Index", color = "Cohort") +
+    theme(
+      panel.grid.major.x = element_blank(),
+      panel.grid.major.y = element_line(size = 0.1, colour = 'grey'),
+      panel.grid.minor = element_blank(),
+      strip.background = element_rect(colour="white", fill="white"),
+      panel.spacing = unit(2, "lines"),
+      panel.border = border_custom(),
+      legend.position = c(0.2, 0.8),
+      axis.ticks.length=unit(-0.15, "cm"),
+      legend.margin = margin(t=-.2, r=0, b=-.2, l=0, unit="cm"),
+      axis.text.x = element_text(margin=unit(rep(0.3, 4), "cm")),
+      axis.text.y = element_text(margin=unit(rep(0.3, 4), "cm"))
+    )
+  print(myplot)
+  print(pp)
+  readline(prompt="Press [enter] to continue")
+}
+# 2009 Q4
+# 2012 Q2
+# 2012 Q3
+# 2012 Q4
+
+ggplot(dt %>% filter(ref_t == 2010), mapping = aes(x = t, y = cpricei, color = group)) +
+  geom_line(size = .7) +
+  geom_point(size = .8) +
+  geom_vline(xintercept = 2010, color = "red", linetype = 1, alpha = .5) +
+  theme_bw() +
+  scale_x_yearqtr(format = "%Y Q%q", limits = c(2008, 2014), expand = c(.01, -.05)) +
+  labs(x = "Quarter", y = "Normalized Price Index", color = "Cohort") +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(size = 0.1, colour = 'grey'),
+    panel.grid.minor = element_blank(),
+    strip.background = element_rect(colour="white", fill="white"),
+    panel.spacing = unit(2, "lines"),
+    panel.border = border_custom(),
+    legend.position = c(0.68, 0.27),
+    axis.ticks.length=unit(-0.15, "cm"),
+    legend.margin = margin(t=-.2, r=0, b=-.2, l=0, unit="cm"),
+    axis.text.x = element_text(margin=unit(rep(0.3, 4), "cm")),
+    axis.text.y = element_text(margin=unit(rep(0.3, 4), "cm"))
+  )
+
