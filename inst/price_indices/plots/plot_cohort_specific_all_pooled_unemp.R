246c9abc202a9cd3479650bcb0a30ce7f83409e9
John Bonney <31965225+johnnybonney@users.noreply.github.com>
2019-03-25 19:46:39 -0500

    Initial commit of new unemp plot

---
+++ inst/price_indices/plots/plot_cohort_specific_all_pooled_unemp.R
@@ -0,0 +1,82 @@
+#' Author: John Bonney
+#'
+#' Plot the pooled cohort-specific trends for quantity.
+
+setwd("C:/Users/John Bonney/Desktop/Magne_projects/sales_taxes")
+
+library(tidyverse)
+library(zoo)
+library(grid)
+library(data.table)
+
+element_grob.element_custom <- function(element, ...)  {
+
+  segmentsGrob(c(1,0,0),
+               c(0,0,1),
+               c(0,0,0),
+               c(0,1,1), gp=gpar(lwd=2))
+}
+## silly wrapper to fool ggplot2
+border_custom <- function(...){
+  structure(
+    list(...), # this ... information is not used
+    class = c("element_custom","element_blank", "element") # inheritance test workaround
+  )
+
+}
+
+data_path <- "output/server/pi_data/ever_increase/unemprate_all_cohorts_ei_pooled.csv"
+outfile_figpath <- "reports/figs/unemp_all_cohorts_ei_pooled_extended.png"
+outfile_figpath2 <- "reports/figs/unemp_all_cohorts_ei_pooled_extended_normalized.png"
+
+dt <- read.csv(data_path)
+
+ggplot(dt, mapping = aes(x = tt_event, y = mean_rate, color = tr_group)) +
+  geom_line(size = .7) +
+  geom_point(size = .8) +
+  geom_vline(xintercept = 0, color = "red", linetype = "22", alpha = .5) +
+  theme_bw() +
+  scale_x_continuous(expand = c(.01, -.05)) +
+  labs(x = "Month", y = "Unemployment Rate", color = NULL) +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(size = 0.1, colour = 'grey'),
+    panel.grid.minor = element_blank(),
+    strip.background = element_rect(colour="white", fill="white"),
+    panel.spacing = unit(2, "lines"),
+    panel.border = border_custom(),
+    legend.position = c(0.23, 0.8),
+    axis.ticks.length=unit(-0.15, "cm"),
+    legend.margin = margin(t=-.2, r=0, b=-.2, l=0, unit="cm"),
+    axis.text.x = element_text(margin=unit(rep(0.3, 4), "cm")),
+    axis.text.y = element_text(margin=unit(rep(0.3, 4), "cm"))
+  )
+
+ggsave(outfile_figpath, height = 120, width = 180, units = "mm")
+
+dt.norm <- dt %>%
+  group_by(tr_group) %>%
+  mutate(mean_rate = mean_rate - mean_rate[tt_event == -6])
+
+ggplot(dt.norm, mapping = aes(x = tt_event, y = mean_rate, color = tr_group)) +
+  geom_line(size = .7) +
+  geom_point(size = .8) +
+  geom_vline(xintercept = 0, color = "red", linetype = "22", alpha = .5) +
+  theme_bw() +
+  scale_x_continuous(expand = c(.01, -.05)) +
+  labs(x = "Month", y = "Unemployment Rate", color = NULL) +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(size = 0.1, colour = 'grey'),
+    panel.grid.minor = element_blank(),
+    strip.background = element_rect(colour="white", fill="white"),
+    panel.spacing = unit(2, "lines"),
+    panel.border = border_custom(),
+    legend.position = c(0.23, 0.8),
+    axis.ticks.length=unit(-0.15, "cm"),
+    legend.margin = margin(t=-.2, r=0, b=-.2, l=0, unit="cm"),
+    axis.text.x = element_text(margin=unit(rep(0.3, 4), "cm")),
+    axis.text.y = element_text(margin=unit(rep(0.3, 4), "cm"))
+  )
+ggsave(outfile_figpath2, height = 120, width = 180, units = "mm")
+
