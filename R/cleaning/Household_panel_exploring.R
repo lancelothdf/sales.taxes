e5f85e88086ee8983008db22ad278e1991abf3e6
Santiago Lacouture <slacouture@uchicago.edu>
2019-07-22 12:06:04 -0500

    Add new vars and exploration

---
+++ R/cleaning/Household_panel_exploring.R
@@ -0,0 +1,100 @@
+## Sales taxes Project. Household Panel
+# Exploring Data Set
+# This code runs basic descriptive statistics to main outcomes and interest variables
+# Author: Santiago Lacouture
+
+
+library(data.table)
+library(lfe)
+library(futile.logger)
+library(AER)
+library(multcomp)
+library(psych)
+library(ggplot2)
+
+setwd("/project2/igaarder/Data/Nielsen/Household_panel")
+
+## Filepaths
+# Exporting results to my folder in the server
+
+purchases.full <- fread("cleaning/consumer_panel_2006-2016_ids.csv")
+
+## Basic Descriptive Statistics -------
+# by module 
+descriptives <- describe(purchases.full[, .(ln_sales_tax, ln_cpricei, ln_quantity, ln_share_expend,
+                                           total_expenditures, share_expend, ln_share_expend_100)])
+desc.est  <- data.table(descriptives, keep.rownames=T)
+des.est.out <- desc.est
+
+# by store
+purchases.store <- purchases.full[, list(sum_total_exp_store = sum(total_expenditures), modules_by_store = .N),
+                                    by = .(household_code, year, quarter, store_code_uc)]
+descriptives <- describe(purchases.store[, .(sum_total_exp_store, modules_by_store)])
+desc.est  <- data.table(descriptives, keep.rownames=T)
+des.est.out <- rbind(des.est.out, desc.est)
+
+# by quarter
+purchases.quarter <- purchases.store[, list(sum_total_exp_quarter = sum(sum_total_exp_store), stores = .N),
+                                  by = .(household_code, year, quarter)]
+descriptives <- describe(purchases.quarter[, .(sum_total_exp_quarter, stores)])
+desc.est  <- data.table(descriptives, keep.rownames=T)
+des.est.out <- rbind(des.est.out, desc.est)
+
+
+fwrite(des.est.out, "../../../../../home/slacouture/HMS/Basic_Descriptives.csv")
+
+## Graphing Descriptives: Oneway ---------------
+
+# Histograms
+ggplot(purchases.full, aes(x=total_expenditures), na.rm = T) +
+  geom_histogram()
+ggsave("../../../../../home/slacouture/HMS/histogram_expenditures.png")
+
+ggplot(purchases.full, aes(x=ln_cpricei), na.rm = T) +
+  geom_histogram()
+ggsave("../../../../../home/slacouture/HMS/histogram_ln_cpricei.png")
+
+ggplot(purchases.full, aes(x=ln_sales_tax), na.rm = T) +
+  geom_histogram()
+ggsave("../../../../../home/slacouture/HMS/histogram_ln_sales_tax.png")
+
+ggplot(purchases.full, aes(x=ln_quantity), na.rm = T) +
+  geom_histogram()
+ggsave("../../../../../home/slacouture/HMS/histogram_ln_quantity.png")
+
+ggplot(purchases.full, aes(x=share_expend), na.rm = T) +
+  geom_histogram()
+ggsave("../../../../../home/slacouture/HMS/histogram_share_expend.png")
+
+ggplot(purchases.full, aes(x=ln_share_expend), na.rm = T) +
+  geom_histogram()
+ggsave("../../../../../home/slacouture/HMS/histogram_ln_share_expend.png")
+
+ggplot(purchases.store, aes(x=sum_total_exp_store), na.rm = T) +
+  geom_histogram()
+ggsave("../../../../../home/slacouture/HMS/histogram_sum_total_exp_store.png")
+
+ggplot(purchases.store, aes(x=modules_by_store), na.rm = T) +
+  geom_histogram()
+ggsave("../../../../../home/slacouture/HMS/histogram_modules_by_store.png")
+
+ggplot(purchases.quarter, aes(x=sum_total_exp_quarter), na.rm = T) +
+  geom_histogram()
+ggsave("../../../../../home/slacouture/HMS/histogram_sum_total_exp_quarter.png")
+
+ggplot(purchases.quarter, aes(x=stores), na.rm = T) +
+  geom_histogram()
+ggsave("../../../../../home/slacouture/HMS/histogram_stores.png")
+
+
+
+## Constraining Data set for estimations ------------ 
+# Drop "magnet" purchases: 
+purchases.full[, sum(is.na(projection_factor))]
+# 2016154 obs
+purchases.nomagnet <- purchases.full[!is.na(projection_factor)]
+
+# Drop purchases without sales tax data
+purchases.full[, sum(is.na(sales_tax))]
+# More than 70%: 181247984 obs
+purchases.retail <- purchases.nomagnet[!is.na(sales_tax)]
---
+++ R/cleaning/clean_household_panel_newvars.R
@@ -0,0 +1,67 @@
+## Sales taxes Project. Household Panel
+# Further cleaning panel data
+# This code further cleans panel: creates and cleans interest variables and ids
+
+library(data.table)
+library(futile.logger)
+library(AER)
+library(multcomp)
+library(psych)
+
+
+setwd("/project2/igaarder/Data/Nielsen/Household_panel")
+
+## Filepaths
+
+## Load clean panel
+purchases.full <- fread("cleaning/consumer_panel_2006-2016.csv")
+
+
+## New Variables ----------
+
+# Convert variables in factor to create indexes
+purchases.full$household_code <- factor(purchases.full$household_code)
+purchases.full$store_code_uc <- factor(purchases.full$store_code_uc)
+purchases.full$product_module_code <- factor(purchases.full$product_module_code)
+purchases.full$quarter <- factor(purchases.full$quarter)
+purchases.full$year <- factor(purchases.full$year)
+
+
+## Create Indexes
+purchases.full$module_by_time <- factor(with(purchases.full, 
+                                             interaction(year, quarter, product_module_code )))
+
+purchases.full <- within(purchases.full,{household_by_store_by_module<-as.numeric(
+  factor(paste0(product_module_code,
+                store_code_uc,
+                household_code)))}) 
+
+## Create outcome and interest variables
+# -Inf values are converted to NAs
+
+# Total expenditure per quarter 
+purchases.full <- purchases.full[, sum_total_exp_quarter := sum(total_expenditures),
+               by = .(household_code, year, quarter)]
+
+# Log sales tax
+purchases.full <- purchases.full[, ln_sales_tax := log(sales_tax) ]
+purchases.full$ln_sales_tax[is.infinite(purchases.full$ln_sales_tax)] <- NA
+# Log price
+purchases.full <- purchases.full[, ln_cpricei := log(cpricei) ]
+purchases.full$ln_cpricei[is.infinite(purchases.full$ln_cpricei)] <- NA
+# Log expenditures
+purchases.full <- purchases.full[, ln_expenditures := log(total_expenditures) ]
+purchases.full$ln_expenditures[is.infinite(purchases.full$ln_expenditures)] <- NA
+# Log quantities
+purchases.full <- purchases.full[, ln_quantity := ln_expenditures - ln_cpricei ]
+purchases.full$ln_quantity[is.infinite(purchases.full$ln_quantity)] <- NA
+
+# Share of expenditure on a given module X store
+purchases.full <- purchases.full[, share_expend := total_expenditures/sum_total_exp_quarter ]
+purchases.full <- purchases.full[, ln_share_expend := log(share_expend) ]
+purchases.full$ln_share_expend[is.infinite(purchases.full$ln_share_expend)] <- NA
+
+purchases.full <- purchases.full[, ln_share_expend_100 := log(100*share_expend)]
+purchases.full$ln_share_expend_100[is.infinite(purchases.full$ln_share_expend_100)] <- NA
+
+fwrite(purchases.full, "cleaning/consumer_panel_2006-2016_ids.csv")
