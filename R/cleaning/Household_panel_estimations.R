501b7504571d8281483094c881ea2d14d3885e72
Santiago Lacouture <slacouture@uchicago.edu>
2019-07-22 10:12:58 -0500

    Modify cleaning (calendar year) and basic estimations of HH panel

--- R/cleaning/Household_panel_estimations.R
+++ R/cleaning/Household_panel_estimations.R
@@ -1,3 +1,8 @@
+## Sales taxes Project. Household Panel
+# Running first Basic Estimations
+# This code further cleans panel: creates and cleans interest variables and ids
+# Author: Santiago Lacouture
+
 
 library(data.table)
 library(lfe)
@@ -10,69 +15,21 @@ library(ggplot2)
 setwd("/project2/igaarder/Data/Nielsen/Household_panel")
 
 ## Filepaths
-# Exporting results to my folder
+# Exporting results to my folder in the server
 out.path <- "/home/slacouture/HMS"
-
-## Load clean panel
-purchases.full <- fread("cleaning/consumer_panel_2006-2016.csv")
-
-
-## New Variables ----------
-
-# Convert variables in factor to create indexes
-purchases.full$household_code <- factor(purchases.full$household_code)
-purchases.full$store_code_uc <- factor(purchases.full$store_code_uc)
-purchases.full$product_module_code <- factor(purchases.full$product_module_code)
-purchases.full$quarter <- factor(purchases.full$quarter)
-purchases.full$year <- factor(purchases.full$year)
-
-
-## Create Indexes
-purchases.full$module_by_time <- factor(with(purchases.full, 
-                                             interaction(year, quarter, product_module_code )))
-
-purchases.full <- within(purchases.full,{household_by_store_by_module<-as.numeric(
-  factor(paste0(product_module_code,
-                store_code_uc,
-                household_code)))}) 
-
-## Create outcome and interest variables
-# -Inf values are converted to NAs
-
-# Log sales tax
-purchases.full <- purchases.full[, ln_sales_tax := log(sales_tax) ]
-purchases.full$ln_sales_tax[is.infinite(purchases.full$ln_sales_tax)] <- NA
-# Log price
-purchases.full <- purchases.full[, ln_cpricei := log(cpricei) ]
-purchases.full$ln_cpricei[is.infinite(purchases.full$ln_cpricei)] <- NA
-# Log expenditures
-purchases.full <- purchases.full[, ln_expenditures := log(total_expenditures) ]
-purchases.full$ln_expenditures[is.infinite(purchases.full$ln_expenditures)] <- NA
-# Log quantities
-purchases.full <- purchases.full[, ln_quantity := ln_expenditures - ln_cpricei ]
-purchases.full$ln_quantity[is.infinite(purchases.full$ln_quantity)] <- NA
-
-# Share of expenditure on a given module X store
-purchases.full <- purchases.full[, share_expend := total_expenditures/sum_total_exp ]
-purchases.full <- purchases.full[, ln_share_expend := log(share_expend) ]
-purchases.full$ln_share_expend[is.infinite(purchases.full$ln_share_expend)] <- NA
-
-purchases.full <- purchases.full[, ln_share_expend_100 := log(100*share_expend)]
-purchases.full$ln_share_expend_100[is.infinite(purchases.full$ln_share_expend_100)] <- NA
-
-fwrite(purchases.full, "cleaning/consumer_panel_2006-2016_ids.csv")
 
 purchases.full <- fread("cleaning/consumer_panel_2006-2016_ids.csv")
 
 ## Basic Descriptive Statistics -------
-desc.est  <- data.table(describe(purchases.full[, .(ln_sales_tax, ln_cpricei, ln_quantity, ln_share_expend,
-                                         total_expenditures, share_expend, ln_share_expend_100)]), keep.rownames=T)
+descriptives <- describe(purchases.full[, .(ln_sales_tax, ln_cpricei, ln_quantity, ln_share_expend,
+                                           total_expenditures, share_expend, ln_share_expend_100)])
+desc.est  <- data.table(descriptives, keep.rownames=T)
 
 fwrite(desc.est, "../../../../../home/slacouture/HMS/Basic_Descriptives.csv")
 
 
 
-## Constraining Data set ------------ 
+## Constraining Data set for estimations ------------ 
 # Drop "magnet" purchases: 
 purchases.full[, sum(is.na(projection_factor))]
 # 2016154 obs
--- R/cleaning/clean_household_panel.R
+++ R/cleaning/clean_household_panel.R
@@ -22,16 +22,22 @@ for (yr in 2006:2016) {
   trips_file <- paste0(folderpath, "trips_", yr, ".tsv")
   store_file <- paste0("/project2/igaarder/Data/Nielsen/stores_", yr, ".dta")
   store_file2 <- paste0("/project2/igaarder/Data/Nielsen/stores_", yr - 1, ".dta")
-
+  
   ## start with purchases data
   flog.info("Loading in purchases data for %s", yr)
   purchases <- fread(purchases_file)
   purchases <- purchases[, .(trip_code_uc, upc, upc_ver_uc, total_price_paid)]
-
+  
   ## merge on the products data to get product module codes (and product group codes)
   flog.info("Merging products data to purchases for %s", yr)
   purchases <- merge(purchases, products, by = c("upc", "upc_ver_uc"))
-
+  
+  ## sum expenditures over UPCs to the product module level
+  flog.info("Summing expenditures over UPCs for %s", yr)
+  purchases <- purchases[, list(
+    total_expenditures = sum(total_price_paid)
+  ), by = .(trip_code_uc, product_module_code, product_group_code)]
+  
   ## merge on the trip data
   flog.info("Loading in trips data for %s", yr)
   trips <- fread(trips_file)
@@ -40,34 +46,42 @@ for (yr in 2006:2016) {
   trips[, year := year(purchase_date)]
   trips <- trips[, .(trip_code_uc, household_code, store_code_uc,
                      year, month, panel_year)]
-
+  
   flog.info("Merging trips data to purchases for %s", yr)
   purchases <- merge(purchases, trips, by = "trip_code_uc")
-
+  
+  ## Collapse by quarter and year (since is total there wont be problem)
+  purchases[, quarter := ceiling(month / 3)]
+  purchases <- purchases[, list(
+    total_expenditures = sum(total_expenditures) , panel_year = max(panel_year)
+  ), by = .(household_code, product_module_code, product_group_code,
+            store_code_uc, quarter, year)]
+  
   ## merge on the retailer info to get channel type and county FIPS codes
   flog.info("Loading in store data for %s", yr)
   stores <- read.dta13(store_file)
   setDT(stores)
   stores[, year := yr]
-
+  
   if (yr > 2006) {
     stores2 <- read.dta13(store_file2)
     setDT(stores2)
-    stores2[, year := yr - 1]
-
+    stores2[, year := (yr - 1) ] # TODO: end of prior year or beginning of next year?
+    
+    
     stores <- rbind(stores, stores2)
   }
-
+  
   stores <- stores[, .(store_code_uc, channel_code, fips_state_code,
                        fips_county_code, year)]
   setnames(stores,
            old = c("fips_state_code", "fips_county_code"),
            new = c("fips_state", "fips_county"))
-
+  
   flog.info("Merging store data to purchases for %s", yr)
   purchases <- merge(purchases, stores, by = c("store_code_uc", "year"), all.x = T)
-
-
+  
+  
   ## merge on some individual information?
   flog.info("Loading in panelists data for %s", yr)
   panelists <- fread(panelists_file)
@@ -76,15 +90,16 @@ for (yr in 2006:2016) {
   setnames(panelists,
            old = c("Household_Cd", "Panel_Year", "Projection_Factor",
                    "Projection_Factor_Magnet", "Household_Income"),
-           new = c("household_code", "panel_year", "projection_factor",
+           new = c("household_code", "year", "projection_factor",
                    "projection_factor_magnet", "household_income"))
   flog.info("Merging panelists data to purchases for %s", yr)
-  purchases <- merge(purchases, panelists, by = c("household_code", "panel_year"))
-
+  purchases <- merge(purchases, panelists, by = c("household_code", "year"), all.x = T)
+  
   ## save the final dataset
   flog.info("Saving cleaned dataset for panel year %s", yr)
   output.path <- paste0("cleaning/purchases_", yr, ".csv")
   fwrite(purchases, output.path)
+
 }
 
 ## link all these annual files
@@ -93,46 +108,52 @@ for (yr in 2006:2016) {
   flog.info("Appending %s data to master file", yr)
   annual.path <- paste0("cleaning/purchases_", yr, ".csv")
   annual.file <- fread(annual.path)
-
+  
   ## attach
   purchases.all <- rbind(purchases.all, annual.file)
 }
 
-## sum expenditures over UPCs to the product module level
-flog.info("Summing expenditures over UPCs for %s", yr)
-purchases.all <- purchases.all[, list(
-  total_expenditures = sum(total_price_paid)
-), by = .(trip_code_uc, product_module_code, product_group_code, year)]
+
 
 ## collapse expenditures to the quarterly level
-flog.info("Collapsing expenditures to the quarterly level for all years")
-purchases.all[, quarter := ceiling(month / 3)]
-purchases.all <- purchases.all[, list(
-  total_expenditures = sum(total_expenditures)
+purchases.full <- data.table(NULL)
+for (yr in 2006:2016) {
+  
+  purchase.yr <- purchases.all[year == yr]
+  flog.info("Collapsing expenditures to the quarterly level for year %s", yr)
+  purchase.yr <- purchase.yr[, list(
+    total_expenditures = sum(total_expenditures), 
+    projection_factor = mean(projection_factor, na.rm = T),
+    projection_factor_magnet = mean(projection_factor_magnet, na.rm = T),
+    household_income = mean(household_income, na.rm = T)
   ), by = .(household_code, product_module_code, product_group_code,
-            store_code_uc, quarter, year)]
-
+            store_code_uc, channel_code, fips_county, fips_state,
+            quarter, year)  ]
+  ## attach
+  purchases.full <- rbind(purchases.full, purchase.yr)
+}
+rm(purchases.all)
 ## Calculate total expenditure per consumer in each quarter (across modules)
-purchases.all[, sum_total_exp := sum(total_expenditures),
-             by = .(household_code, year, quarter, store_code_uc)]
+purchases.full[, sum_total_exp := sum(total_expenditures),
+               by = .(household_code, year, quarter, store_code_uc)]
 
 ## Subset to just the best-selling modules
 best_selling_modules <- fread("/project2/igaarder/Data/best_selling_modules.csv")
 keep_modules <- unique(best_selling_modules[, .(Module)][[1]])
 
-purchases.all <- purchases.all[product_module_code %in% keep_modules]
-fwrite(purchases.all, "cleaning/consumer_panel_2006-2016.csv")
+purchases.full <- purchases.full[product_module_code %in% keep_modules]
+#fwrite(purchases.full, "cleaning/consumer_panel_2006-2016.csv")
 
-purchases.all <- fread("cleaning/consumer_panel_2006-2016.csv")
+purchases.full <- fread("cleaning/consumer_panel_2006-2016.csv")
 ## merge on price indices and tax rates
 all_goods_pi_path <- "../../all_nielsen_data_2006_2016_quarterly.csv"
 all_pi <- fread(all_goods_pi_path)
 all_pi <- all_pi[, .(store_code_uc, product_module_code,
                      year, quarter, pricei, cpricei, sales_tax)]
 
-purchases.all <- merge(
-  purchases.all, all_pi,
+purchases.full <- merge(
+  purchases.full, all_pi,
   by = c("store_code_uc", "product_module_code", "year", "quarter"),
   all.x = T
-  )
-fwrite(purchases.all, "cleaning/consumer_panel_2006-2016.csv")
+)
+fwrite(purchases.full, "cleaning/consumer_panel_2006-2016.csv")
