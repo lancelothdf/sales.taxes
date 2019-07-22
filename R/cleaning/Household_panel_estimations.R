a9d29409b7e307602ea0299c0c481697e87d9bee
Santiago Lacouture <slacouture@uchicago.edu>
2019-07-22 09:01:38 -0500

    Add clean and basic estimation hh

---
+++ R/cleaning/Household_panel_estimations.R
@@ -0,0 +1,173 @@
+
+library(data.table)
+library(lfe)
+library(futile.logger)
+library(AER)
+library(multcomp)
+library(psych)
+library(ggplot2)
+
+setwd("/project2/igaarder/Data/Nielsen/Household_panel")
+
+## Filepaths
+# Exporting results to my folder
+out.path <- "/home/slacouture/HMS"
+
+## Load clean panel
+purchases.full <- fread("cleaning/consumer_panel_2006-2016.csv")
+
+
+## New Variables ----------
+
+# Convert variables in factor to create indexes
+purchases.full$household_code <- factor(purchases.full$household_code)
+purchases.full$store_code_uc <- factor(purchases.full$store_code_uc)
+purchases.full$product_module_code <- factor(purchases.full$product_module_code)
+purchases.full$quarter <- factor(purchases.full$quarter)
+purchases.full$year <- factor(purchases.full$year)
+
+
+## Create Indexes
+purchases.full$module_by_time <- factor(with(purchases.full, 
+                                             interaction(year, quarter, product_module_code )))
+
+purchases.full <- within(purchases.full,{household_by_store_by_module<-as.numeric(
+  factor(paste0(product_module_code,
+                store_code_uc,
+                household_code)))}) 
+
+## Create outcome and interest variables
+# -Inf values are converted to NAs
+
+# Log sales tax
+purchases.full <- purchases.full[, ln_sales_tax := log(sales_tax) ]
+purchases.full$ln_sales_tax[is.infinite(purchases.full$ln_sales_tax)] <- NA
+# Log price
+purchases.full <- purchases.full[, ln_cpricei := log(cpricei) ]
+purchases.full$ln_cpricei[is.infinite(purchases.full$ln_cpricei)] <- NA
+# Log expenditures
+purchases.full <- purchases.full[, ln_expenditures := log(total_expenditures) ]
+purchases.full$ln_expenditures[is.infinite(purchases.full$ln_expenditures)] <- NA
+# Log quantities
+purchases.full <- purchases.full[, ln_quantity := ln_expenditures - ln_cpricei ]
+purchases.full$ln_quantity[is.infinite(purchases.full$ln_quantity)] <- NA
+
+# Share of expenditure on a given module X store
+purchases.full <- purchases.full[, share_expend := total_expenditures/sum_total_exp ]
+purchases.full <- purchases.full[, ln_share_expend := log(share_expend) ]
+purchases.full$ln_share_expend[is.infinite(purchases.full$ln_share_expend)] <- NA
+
+purchases.full <- purchases.full[, ln_share_expend_100 := log(100*share_expend)]
+purchases.full$ln_share_expend_100[is.infinite(purchases.full$ln_share_expend_100)] <- NA
+
+fwrite(purchases.full, "cleaning/consumer_panel_2006-2016_ids.csv")
+
+purchases.full <- fread("cleaning/consumer_panel_2006-2016_ids.csv")
+
+## Basic Descriptive Statistics -------
+desc.est  <- data.table(describe(purchases.full[, .(ln_sales_tax, ln_cpricei, ln_quantity, ln_share_expend,
+                                         total_expenditures, share_expend, ln_share_expend_100)]), keep.rownames=T)
+
+fwrite(desc.est, "../../../../../home/slacouture/HMS/Basic_Descriptives.csv")
+
+
+
+## Constraining Data set ------------ 
+# Drop "magnet" purchases: 
+purchases.full[, sum(is.na(projection_factor))]
+# 2016154 obs
+purchases.nomagnet <- purchases.full[!is.na(projection_factor)]
+
+# Drop purchases without sales tax data
+purchases.full[, sum(is.na(sales_tax))]
+# More than 70%: 181247984 obs
+purchases.retail <- purchases.nomagnet[!is.na(sales_tax)]
+
+
+
+
+
+## Basic Specifications ----------
+
+# Log Share of Expenditure
+formula0 <- as.formula(paste0(
+  "ln_share_expend ~ ln_sales_tax | module_by_time + household_by_store_by_module"
+))
+
+flog.info("Estimating Log Share")
+res0 <- felm(data = purchases.retail,
+             formula = formula0,
+             weights = purchases.retail$projection_factor,
+             na.omit)
+
+flog.info("Writing results...")
+res0.dt <- data.table(coef(summary(res0)), keep.rownames=T)
+res0.dt[, outcome := "ln_share_expend"]
+res0.dt[, Rsq := summary(res0)$r.squared]
+res0.dt[, adj.Rsq := summary(res0)$adj.r.squared]
+res0.dt[, specification := "Basic"]
+LRdiff_res <- res0.dt ### Create table LRdiff_res in which we store all results (we start with the results we had just stored in res1.dt)
+N_obs <- sum((!is.na(purchases.retail$ln_share_expend)))
+
+# Share of Expenditure
+formula1 <- as.formula(paste0(
+  "share_expend ~ ln_sales_tax | module_by_time + household_by_store_by_module"
+))
+
+flog.info("Estimating Share")
+res1 <- felm(data = purchases.retail,
+             formula = formula1,
+             weights = purchases.retail$projection_factor,
+             na.omit)
+flog.info("Writing results...")
+res0.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+res0.dt[, outcome := "share_expend"]
+res0.dt[, Rsq := summary(res1)$r.squared]
+res0.dt[, adj.Rsq := summary(res1)$adj.r.squared]
+res0.dt[, specification := "Basic"]
+LRdiff_res <- rbind(LRdiff_res,res0.dt) ### Append 
+N_obs[2] <- sum((!is.na(purchases.retail$share_expend)))
+
+# Log Price
+formula2 <- as.formula(paste0(
+  "ln_cpricei ~ ln_sales_tax | module_by_time + household_by_store_by_module"
+))
+flog.info("Estimating Log Price")
+res2 <- felm(data = purchases.retail,
+             formula = formula2,
+             weights = purchases.retail$projection_factor,
+             na.omit)
+
+flog.info("Writing results...")
+res0.dt <- data.table(coef(summary(res2)), keep.rownames=T)
+res0.dt[, outcome := "ln_cpricei"]
+res0.dt[, Rsq := summary(res2)$r.squared]
+res0.dt[, adj.Rsq := summary(res2)$adj.r.squared]
+res0.dt[, specification := "Basic"]
+LRdiff_res <- rbind(LRdiff_res,res0.dt) ### Append 
+N_obs[3] <- sum((!is.na(purchases.retail$ln_cpricei)))
+
+# Log Quantity
+formula2 <- as.formula(paste0(
+  "ln_quantity ~ ln_sales_tax | module_by_time + household_by_store_by_module"
+))
+flog.info("Estimating Log Price")
+res2 <- felm(data = purchases.retail,
+             formula = formula2,
+             weights = purchases.retail$projection_factor,
+             na.omit)
+
+flog.info("Writing results...")
+res0.dt <- data.table(coef(summary(res2)), keep.rownames=T)
+res0.dt[, outcome := "ln_quantity"]
+res0.dt[, Rsq := summary(res2)$r.squared]
+res0.dt[, adj.Rsq := summary(res2)$adj.r.squared]
+res0.dt[, specification := "Basic"]
+LRdiff_res <- rbind(LRdiff_res,res0.dt) ### Append 
+N_obs[4] <- sum((!is.na(purchases.retail$ln_quantity)))
+
+# Add Ns
+LRdiff_res$N <- N_obs
+
+
+fwrite(LRdiff_res, "../../../../../home/slacouture/HMS/Basic_Results.csv")
