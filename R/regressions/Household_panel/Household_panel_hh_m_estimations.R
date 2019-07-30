ff9ea74943268b787a72808d19f36204cde1797d
Santiago Lacouture <slacouture@uchicago.edu>
2019-07-29 22:33:05 -0500

    Bug fix estimations panel

--- R/regressions/Household_panel/Household_panel_hh_m_estimations.R
+++ R/regressions/Household_panel/Household_panel_hh_m_estimations.R
@@ -58,12 +58,12 @@ for (Y in outcomes) {
 
   # Now the log
   log.Y <- paste0("ln_", Y)
-  purchases.sample[, (log.Y) := log(Y)]
+  purchases.sample[, log.Y := log( Y )]
   # make sure 0s are now missings
   purchases.full$log.Y[is.infinite(purchases.full$log.Y)] <- NA
 
   formula1 <- as.formula(paste0(
-    (log.Y), "~ ln_sales_tax | time + household_code "
+    log.Y, "~ ln_sales_tax | time + household_code "
   ))
   flog.info("Estimating with log %s as outcome", Y)
   res1 <- felm(formula = formula1, data = purchases.sample,
