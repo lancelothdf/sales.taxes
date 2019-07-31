ad4177f44f94d992714f84acbfcfe8c4e77555d4
Santiago Lacouture <slacouture@uchicago.edu>
2019-07-31 17:41:45 -0500

    Bug fix estimations winsorizing outcomes by quarterXyear at 95 perc

--- R/regressions/Household_panel/Household_panel_module_hh_q_estimations_leadlags.R
+++ R/regressions/Household_panel/Household_panel_module_hh_q_estimations_leadlags.R
@@ -297,10 +297,9 @@ for (Y in c(outcomes, outcomes_t)) {
   
   ### Winsorize Outcomes: Above percentile 95 is replaced by 95th percentile
   flog.info("Winsorizing %s ", Y)
-  purchases.sample[, indPct:=round(frank(Y, na.last="keep")/sum(!is.na(Y)), digits = 2)]
-  val.95 <- purchases.sample[indPct == 0.95, mean(Y) ]
+  val.95 <- quantile(purchases.sample$Y, na.rm = T)
   flog.info("Percentile 95 for %s is %s ", Y, val.95)
-  purchases.sample$Y[purchases.sample$indPct > 0.95 & !is.na(purchases.sample$Y)] <- val.95
+  purchases.sample$Y[purchases.sample$Y > val.95 & !is.na(purchases.sample$Y)] <- val.95
   
   formula1 <- as.formula(paste0(
     Y, "~", formula_RHS, "| module_by_time"
