1962f6726c1add454c08b340e1125df9ae29db2c
Santiago Lacouture <slacouture@uchicago.edu>
2019-08-01 10:07:28 -0500

    Bug fix estimations winsorizing outcomes by quarterXyear

--- R/regressions/Household_panel/Household_panel_hh_q_estimations_leadlags_winsor.R
+++ R/regressions/Household_panel/Household_panel_hh_q_estimations_leadlags_winsor.R
@@ -238,9 +238,8 @@ lead.lp.restr <- paste(lead.vars, "= 0")
 lag.lp.restr <- paste(lag.vars, "+ D.ln_sales_tax = 0")
 total.lp.restr <- paste(lag.vars, "+", lead.vars, "+ D.ln_sales_tax = 0")
 
-## Winsorize outcomes (upper 1%) -------
-
-purchases.sample <- purchases.sample[ , lapply(.SD, Winsorize, probs = c(0.01,0.99), na.rm = T), .SDcols = c(outcomes, outcomes_t)]
+## Winsorize outcomes (1%) -------
+purchases.sample <- purchases.sample[ , c(outcomes, outcomes_t) := (lapply(.SD, Winsorize, probs = c(0.01,0.99), na.rm = T)), .SDcols = c(outcomes, outcomes_t)]
 
 ## Run basic descriptives  ------
 
