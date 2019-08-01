1135c2da3a1e6e5db5d2c1eb6e054ebdaa07d723
Santiago Lacouture <slacouture@uchicago.edu>
2019-08-01 10:20:01 -0500

    change estimations winsorizing outcomes by quarterXyear

--- R/regressions/Household_panel/Household_panel_hh_q_estimations_leadlags_winsor.R
+++ R/regressions/Household_panel/Household_panel_hh_q_estimations_leadlags_winsor.R
@@ -238,8 +238,8 @@ lead.lp.restr <- paste(lead.vars, "= 0")
 lag.lp.restr <- paste(lag.vars, "+ D.ln_sales_tax = 0")
 total.lp.restr <- paste(lag.vars, "+", lead.vars, "+ D.ln_sales_tax = 0")
 
-## Winsorize outcomes (2%) -------
-purchases.sample <- purchases.sample[ , c(outcomes, outcomes_t) := (lapply(.SD, Winsorize, probs = c(0.02,0.98), na.rm = T)), .SDcols = c(outcomes, outcomes_t)]
+## Winsorize outcomes (5%) -------
+purchases.sample <- purchases.sample[ , c(outcomes, outcomes_t) := (lapply(.SD, Winsorize, probs = c(0.05,0.95), na.rm = T)), .SDcols = c(outcomes, outcomes_t)]
 
 ## Run basic descriptives  ------
 
