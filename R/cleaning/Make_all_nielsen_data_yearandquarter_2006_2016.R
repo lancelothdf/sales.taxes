dc9630873824492eba1f5a37435788e37435eff1
Lancelot Henry De Frahan <lancelot@midway2-login2.rcc.local>
2019-08-19 17:51:03 -0500

    fixed errors in cleaning + regressions R files

--- R/cleaning/Make_all_nielsen_data_yearandquarter_2006_2016.R
+++ R/cleaning/Make_all_nielsen_data_yearandquarter_2006_2016.R
@@ -61,8 +61,17 @@ all_pi <- merge(all_pi, all.tax, by = c("store_code_uc", "product_module_code",
                 all.x = T)
 rm(all.tax)
 
+## Impute sales_tax for 2006-2007 and 2015-2016 (+ define cpricei for these years)
+all_pi[, base.tax := sales_tax_wtd[year == 2008 & quarter == 1],
+            by = .(store_code_uc, product_module_code)]
+all_pi[year <= 2007,]$sales_tax_wtd <- all_pi[year <= 2007,]$base.tax
+
+all_pi[, base.tax := sales_tax_wtd[year == 2014 & quarter == 4], by = .(store_code_uc, product_module_code)]
+all_pi[year >= 2015,]$sales_tax_wtd <- all_pi[year >= 2015,]$base.tax
+
+
 ### Create consumer price index 2
-# We could include 
+# We could include
 all_pi <- all_pi[, ln_cpricei2 := ln_pricei2 + log(sales_tax_wtd)]
 fwrite(all_pi, all_goods_pi_path)
 rm(all_pi)
@@ -159,26 +168,26 @@ rm(all_pi)
  ###
  fwrite(yearly_data, output_yearly)
 
- 
+
  ##############
  ############## Add some more variables, measures,... and difference the data to get a "master file"
  ### Prepare the data
  all_pi <- fread(all_goods_pi_path)
  all_pi <- all_pi[year %in% 2006:2016 & !is.na(cpricei)]
- 
- 
+
+
  ### Choose the sales weighted tax rate as main measure of taxes
  all_pi[, sales_tax2 := sales_tax]
  all_pi[, sales_tax := sales_tax_wtd]
- 
- 
+
+
  ###
  # Merge to additional measures (FE quantity and quality indices)
  quant_pi <- fread(quantity_index_path)
  all_pi <- merge(all_pi, quant_pi, by = c("store_code_uc", "product_module_code", "year", "quarter"))
  rm(quant_pi)
- 
- 
+
+
  ## prep Census region/division data ------------------------------
  geo_dt <- structure(list(
    fips_state = c(1L, 2L, 4L, 5L, 6L, 8L, 9L, 10L, 12L, 13L, 15L, 16L, 17L, 18L,
@@ -193,18 +202,18 @@ rm(all_pi)
                 5L, 4L, 3L,  7L, 9L, 2L, 1L, 5L, 4L, 6L, 7L, 8L, 1L, 5L, 9L, 5L, 3L, 8L)),
    class = "data.frame", row.names = c(NA, -50L))
  setDT(geo_dt)
- 
+
 
  ####
  ## merge on the census region/division info
  all_pi <- merge(all_pi, geo_dt, by = "fips_state")
- 
+
  # impute tax rates prior to 2008 and after 2014
  all_pi[, sales_tax := ifelse(year < 2008, sales_tax[year == 2008 & quarter == 1], sales_tax),
         by = .(store_code_uc, product_module_code)]
  all_pi[, sales_tax := ifelse(year > 2014, sales_tax[year == 2014 & quarter == 4], sales_tax),
         by = .(store_code_uc, product_module_code)]
- 
+
  # create necessary variables
  all_pi[, ln_cpricei := log(cpricei)]
  all_pi[, ln_sales_tax := log(sales_tax)]
@@ -218,87 +227,86 @@ rm(all_pi)
  all_pi[, module_by_state := .GRP, by = .(product_module_code, fips_state)]
  all_pi[, region_by_module_by_time := .GRP, by = .(region, product_module_code, cal_time)]
  all_pi[, division_by_module_by_time := .GRP, by = .(division, product_module_code, cal_time)]
- 
+
  ## get sales weights
  all_pi[, base.sales := sales[year == 2008 & quarter == 1],
         by = .(store_code_uc, product_module_code)]
- 
+
  all_pi <- all_pi[!is.na(base.sales) & !is.na(sales) & !is.na(ln_cpricei) &
-                    !is.na(ln_sales_tax) & !is.na(ln_quantity) & !is.na(ln_quantity2) & is.na(ln_cpricei2)
+                    !is.na(ln_sales_tax) & !is.na(ln_quantity) & !is.na(ln_quantity2) & !is.na(ln_cpricei2)
                   & !is.na(ln_quantity3)]
- 
+
  ## balance on store-module level
  keep_store_modules <- all_pi[, list(n = .N),
                               by = .(store_code_uc, product_module_code)]
  keep_store_modules <- keep_store_modules[n == (2016 - 2005) * 4]
- 
+
  setkey(all_pi, store_code_uc, product_module_code)
  setkey(keep_store_modules, store_code_uc, product_module_code)
- 
+
  all_pi <- all_pi[keep_store_modules]
  setkey(all_pi, store_code_uc, product_module_code, year, quarter)
- 
- 
+
+
  #############################################################
  ## Delete some variables to save memory
  all_pi <- all_pi[, c("fips_state", "fips_county", "year", "quarter", "store_code_uc", "product_module_code", "ln_cpricei", "ln_cpricei2", "ln_sales","ln_quantity", "ln_quantity2", "ln_quantity3", "ln_UPC", "ln_raw_quant", "ln_sales_tax", "taxability", "base.sales", "sales", "store_by_module", "cal_time", "module_by_time", "module_by_state", "region_by_module_by_time", "division_by_module_by_time")]
  ## Here we do not carry over ln_sales_tax2 because do not want to save all leads and lags - But we should try and see if results are similar using ln_sales_tax2
- 
+
  #######################################################
  ## take first differences of outcomes and treatment
  all_pi <- all_pi[order(store_code_uc, product_module_code, cal_time),] ##Sort on store by year-quarter (in ascending order)
- 
- 
+
+
  all_pi[, D.ln_cpricei := ln_cpricei - shift(ln_cpricei, n=1, type="lag"),
         by = .(store_code_uc, product_module_code)]
- 
+
  all_pi[, D.ln_cpricei2 := ln_cpricei2 - shift(ln_cpricei2, n=1, type="lag"),
         by = .(store_code_uc, product_module_code)]
- 
+
  all_pi[, D.ln_quantity := ln_quantity - shift(ln_quantity, n=1, type="lag"),
         by = .(store_code_uc, product_module_code)]
- 
+
  all_pi[, D.ln_quantity2 := ln_quantity2 - shift(ln_quantity2, n=1, type="lag"),
         by = .(store_code_uc, product_module_code)]
- 
+
  all_pi[, D.ln_quantity3 := ln_quantity3 - shift(ln_quantity3, n=1, type="lag"),
         by = .(store_code_uc, product_module_code)]
- 
+
  all_pi[, D.ln_sales := ln_sales - shift(ln_sales, n=1, type="lag"),
         by = .(store_code_uc, product_module_code)]
- 
+
  all_pi[, D.ln_sales_tax := ln_sales_tax - shift(ln_sales_tax, n=1, type="lag"),
         by = .(store_code_uc, product_module_code)]
- 
+
  all_pi[, D.ln_UPC := ln_UPC - shift(ln_UPC, n=1, type="lag"),
         by = .(store_code_uc, product_module_code)]
- 
+
  all_pi[, D.ln_raw_quant := ln_raw_quant - shift(ln_raw_quant, n=1, type="lag"),
         by = .(store_code_uc, product_module_code)]
- 
- 
- 
+
+
+
  ## generate lags and leads of ln_sales_tax
  for (lag.val in 1:8) {
    lag.X <- paste0("L", lag.val, ".D.ln_sales_tax")
    all_pi[, (lag.X) := shift(D.ln_sales_tax, n=lag.val, type="lag"),
           by = .(store_code_uc, product_module_code)]
-   
+
    lead.X <- paste0("F", lag.val, ".D.ln_sales_tax")
    all_pi[, (lead.X) := shift(D.ln_sales_tax, n=lag.val, type="lead"),
           by = .(store_code_uc, product_module_code)]
  }
- 
- 
- 
+
+
+
 ### Keep only relevant years ---------------------------------------------------
  all_pi <- all_pi[between(year, 2008, 2014)]
  all_pi <- all_pi[ year >= 2009 | (year == 2008 & quarter >= 2)] ## First quarter of 2008, the difference was imputed not real data - so we drop it
- 
 
-### Finally keep only the variables that we may need (mostly the differenced variables) 
+
+### Finally keep only the variables that we may need (mostly the differenced variables)
  all_pi <- all_pi[, c("fips_state", "fips_county", "year", "quarter", "store_code_uc", "product_module_code", "D.ln_cpricei", "ln_cpricei2", "D.ln_cpricei2", "D.ln_sales","D.ln_quantity", "D.ln_quantity2", "D.ln_quantity3", "D.ln_UPC", "D.ln_raw_quant", "D.ln_sales_tax", "taxability", "base.sales", "sales", "store_by_module", "cal_time", "module_by_time", "module_by_state", "region_by_module_by_time", "division_by_module_by_time", "F8.D.ln_sales_tax", "F7.D.ln_sales_tax", "F6.D.ln_sales_tax", "F5.D.ln_sales_tax", "F4.D.ln_sales_tax", "F3.D.ln_sales_tax", "F2.D.ln_sales_tax", "F1.D.ln_sales_tax", "L1.D.ln_sales_tax", "L2.D.ln_sales_tax", "L3.D.ln_sales_tax", "L4.D.ln_sales_tax", "L5.D.ln_sales_tax", "L6.D.ln_sales_tax", "L7.D.ln_sales_tax", "L8.D.ln_sales_tax")]
- 
+
  fwrite(all_pi, master_quarterly_path)
- 
- 
+
