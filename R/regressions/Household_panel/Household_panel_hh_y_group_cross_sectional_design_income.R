7e0a3791c765a1ac02e383c7e6ee40d851d5cb01
Santiago Lacouture <slacouture@uchicago.edu>
2019-08-20 09:35:12 -0500

    Add income FE control for cross sectional design

---
+++ R/regressions/Household_panel/Household_panel_hh_y_group_cross_sectional_design_income.R
@@ -0,0 +1,70 @@
+## Sales taxes Project. Household Panel
+# Running Basic Cross-Sectional design on the household x group x year on best selling modules (identified tax rates)
+# Add income bin x module FE
+# Author: Santiago Lacouture
+
+
+library(data.table)
+library(lfe)
+library(futile.logger)
+library(AER)
+library(multcomp)
+library(psych)
+library(ggplot2)
+
+setwd("/project2/igaarder/Data/Nielsen/Household_panel")
+
+## Open Data
+purchases.full <- fread("cleaning/consumer_panel_y_hh_group_2006-2016.csv")
+
+## Constraining Data set for estimations ------------ 
+# Drop "magnet" households: 
+purchases.full[, sum(is.na(projection_factor))]
+purchases.nomagnet <- purchases.full[!is.na(projection_factor)]
+
+# Drop households without sales tax data
+purchases.full[, sum(is.na(ln_sales_tax))]
+purchases.sample <- purchases.nomagnet[!is.na(ln_sales_tax)]
+
+# FE
+purchases.sample[, income_group_by_time := .GRP, by = .(household_income, product_group_code, year)]
+purchases.sample[, household_by_time := .GRP, by = .(year, household_code)]
+
+
+## Estimations: Expenditure on type of module --------
+
+output.results.file <- "../../../../../home/slacouture/HMS/HH_group_year_cross_sectonal_design.csv"
+
+outcomes <- c("ln_expenditures", "ln_expenditures_taxable", "ln_expenditures_non_taxable", 
+              "ln_share", "ln_share_taxable",  "ln_share_non_taxable")
+
+LRdiff_res <- data.table(NULL)
+for (Y in outcomes) {
+  formula1 <- as.formula(paste0(
+    Y, "~ ln_sales_tax | household_by_time + group_by_time | 0 | household_code"
+  ))
+  flog.info("Estimating with %s as outcome", Y)
+  res1 <- felm(formula = formula1, data = purchases.sample,
+               weights = purchases.sample$projection_factor)
+  flog.info("Finished estimating with %s as outcome", Y)
+  
+  ## attach results
+  flog.info("Writing results...")
+  res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+  res1.dt[, outcome := Y]
+  res1.dt[, Rsq := summary(res1)$r.squared]
+  res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
+  res1.dt[, N.obs := nrow(purchases.sample[!is.na(get(Y))])]
+  res1.dt[, N_hholds := uniqueN(purchases.sample[!is.na(get(Y))], by = c("household_code"))]
+  res1.dt[, N_groups := uniqueN(purchases.sample[!is.na(get(Y))], by = c("product_group_code"))]
+  LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
+  fwrite(LRdiff_res, output.results.file)
+
+}
+
+
+
+## summary values --------------------------------------------------------------
+LRdiff_res$N_years <- uniqueN(purchases.sample, by = c("year"))
+
+fwrite(LRdiff_res, output.results.file)
