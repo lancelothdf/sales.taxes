dfd3fab25999e3baa83f4b08641c74f7760adfe2
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2019-09-24 08:36:26 -0500

    Add interaction to initial level of tax rate

---
+++ R/regressions/DID_interinitial_semester_year.R
@@ -0,0 +1,225 @@
+#' Run diff-n-diff on updated semesterly data. Try different FE definitions
+#' Simple DID design to interact with initial level of tax rate in the future
+#' 
+
+library(data.table)
+library(futile.logger)
+library(lfe)
+library(multcomp)
+
+
+setwd("/project2/igaarder")
+
+
+## input filepaths -----------------------------------------------
+#' This data is the same as all_goods_pi_path, except it has 2015-2016 data as well.
+data.semester <- "Data/Nielsen/semester_nielsen_data.csv"
+data.year <- "Data/Nielsen/yearly_nielsen_data.csv"
+
+
+## output filepaths ----------------------------------------------
+output.results.file <- "Data/DID_semesterly_and_yearly.csv"
+
+### Set up Semester Data ---------------------------------
+all_pi <- fread(data.semester)
+
+# Create initial level of tax rate
+all_pi[, L.ln_sales_tax := ln_sales_tax - D.ln_sales_tax]
+# Create interaction and interaction squared
+all_pi[, D.ln_sales_tax_L.ln_sales_tax := L.ln_sales_tax*D.ln_sales_tax]
+all_pi[, D.ln_sales_tax_L.ln_sales_tax_2 := ((L.ln_sales_tax)^2)*D.ln_sales_tax]
+
+outcomes <- c("D.ln_cpricei2", "D.ln_quantity3")
+FE_opts <- c("region_by_module_by_time", "division_by_module_by_time")
+
+### Run DID semester data --------------------------------
+LRdiff_res <- data.table(NULL)
+for (Y in c(outcomes)) {
+  for (FE in FE_opts) {
+    
+    # First only interaction
+    formula1 <- as.formula(paste0(
+      Y, "~ D.ln_sales_tax + D.ln_sales_tax_L.ln_sales_tax | ", FE, " | 0 | module_by_state"
+    ))
+    flog.info("Estimating with %s as outcome with %s FE.", Y, FE)
+    res1 <- felm(formula = formula1, data = all_pi,
+                 weights = all_pi$base.sales)
+    flog.info("Finished estimating with %s as outcome with %s FE.", Y, FE)
+    
+    
+    ## attach results
+    flog.info("Writing results...")
+    res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+    res1.dt[, outcome := Y]
+    res1.dt[, controls := FE]
+    res1.dt[, window := "semester"]
+    res1.dt[, spec := "inter1"]
+    # Add summary values
+    res1.dt[, Rsq := summary(res1)$r.squared]
+    res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
+    res1.dt[, N_obs := nrow(all_pi)]
+    res1.dt[, N_modules := length(unique(all_pi$product_module_code))]
+    res1.dt[, N_stores :=  length(unique(all_pi$store_code_uc))]
+    res1.dt[, N_counties := uniqueN(all_pi, by = c("fips_state", "fips_county"))]
+    res1.dt[, N_years := uniqueN(all_pi, by = c("year"))]
+    res1.dt[, N_county_modules := uniqueN(all_pi, by = c("fips_state", "fips_county",
+                                                         "product_module_code"))]
+    LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
+    fwrite(LRdiff_res, output.results.file)
+    
+    # Second add interaction with squared
+    formula1 <- as.formula(paste0(
+      Y, "~ D.ln_sales_tax + D.ln_sales_tax_L.ln_sales_tax + D.ln_sales_tax_L.ln_sales_tax_2 | ", FE, " | 0 | module_by_state"
+    ))
+    flog.info("Estimating with %s as outcome with %s FE.", Y, FE)
+    res1 <- felm(formula = formula1, data = all_pi,
+                 weights = all_pi$base.sales)
+    flog.info("Finished estimating with %s as outcome with %s FE.", Y, FE)
+    
+    
+    ## attach results
+    flog.info("Writing results...")
+    res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+    res1.dt[, outcome := Y]
+    res1.dt[, controls := FE]
+    res1.dt[, window := "semester"]
+    res1.dt[, spec := "inter2"]
+    # Add summary values
+    res1.dt[, Rsq := summary(res1)$r.squared]
+    res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
+    res1.dt[, N_obs := nrow(all_pi)]
+    res1.dt[, N_modules := length(unique(all_pi$product_module_code))]
+    res1.dt[, N_stores :=  length(unique(all_pi$store_code_uc))]
+    res1.dt[, N_counties := uniqueN(all_pi, by = c("fips_state", "fips_county"))]
+    res1.dt[, N_years := uniqueN(all_pi, by = c("year"))]
+    res1.dt[, N_county_modules := uniqueN(all_pi, by = c("fips_state", "fips_county",
+                                                         "product_module_code"))]
+    LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
+    fwrite(LRdiff_res, output.results.file)   
+    
+  }
+}
+
+### Set up Yearly Data ---------------------------------
+all_pi <- fread(data.year)
+
+outcomes <- c("D.ln_cpricei2", "D.ln_quantity3")
+FE_opts <- c("region_by_module_by_time", "division_by_module_by_time")
+
+## merge region a division
+geo_dt <- structure(list(
+  fips_state = c(1L, 2L, 4L, 5L, 6L, 8L, 9L, 10L, 12L, 13L, 15L, 16L, 17L, 18L,
+                 19L, 20L, 21L, 22L, 23L, 24L, 25L, 26L, 27L, 28L, 29L, 30L,
+                 31L, 32L, 33L, 34L, 35L, 36L, 37L, 38L, 39L, 40L, 41L, 42L,
+                 44L, 45L, 46L, 47L, 48L, 49L, 50L, 51L, 53L, 54L, 55L, 56L),
+  region = c(3L, 4L, 4L, 3L, 4L, 4L, 1L, 3L, 3L, 3L, 4L, 4L, 2L, 2L, 2L, 2L, 3L,
+             3L, 1L, 3L, 1L, 2L, 2L, 3L, 2L, 4L, 2L, 4L, 1L, 1L, 4L, 1L, 3L, 2L,
+             2L, 3L, 4L, 1L, 1L, 3L, 2L, 3L, 3L, 4L, 1L, 3L, 4L, 3L, 2L, 4L),
+  division = c(6L, 9L, 8L,  7L, 9L, 8L, 1L, 5L, 5L, 5L, 9L, 8L, 3L, 3L, 4L, 4L,
+               6L, 7L, 1L, 5L, 1L, 3L, 4L, 6L, 4L, 8L, 4L, 8L, 1L, 2L, 8L, 2L,
+               5L, 4L, 3L,  7L, 9L, 2L, 1L, 5L, 4L, 6L, 7L, 8L, 1L, 5L, 9L, 5L, 3L, 8L)),
+  class = "data.frame", row.names = c(NA, -50L))
+setDT(geo_dt)
+all_pi <- merge(all_pi, geo_dt, by = "fips_state")
+
+
+# take first differences of outcomes and treatment
+all_pi <- all_pi[order(store_code_uc, product_module_code, year),] ##Sort on store by year (in ascending order)
+
+
+all_pi[, D.ln_cpricei2 := ln_cpricei2 - shift(ln_cpricei2, n=1, type="lag"),
+       by = .(store_code_uc, product_module_code)]
+
+all_pi[, D.ln_quantity3 := ln_quantity3 - shift(ln_quantity3, n=1, type="lag"),
+       by = .(store_code_uc, product_module_code)]
+
+all_pi[, D.ln_sales_tax := ln_sales_tax - shift(ln_sales_tax, n=1, type="lag"),
+       by = .(store_code_uc, product_module_code)]
+
+# Make group FE
+all_pi[, store_by_module := .GRP, by = .(store_code_uc, product_module_code)]
+all_pi[, region_by_module_by_time := .GRP, by = .(region, product_module_code, year)]
+all_pi[, division_by_module_by_time := .GRP, by = .(division, product_module_code, year)]
+all_pi[, module_by_state := .GRP, by = .(product_module_code, fips_state)]
+
+
+# Create initial level of tax rate
+all_pi[, L.ln_sales_tax := ln_sales_tax - D.ln_sales_tax]
+# Create interaction and interaction squared
+all_pi[, D.ln_sales_tax_L.ln_sales_tax := L.ln_sales_tax*D.ln_sales_tax]
+all_pi[, D.ln_sales_tax_L.ln_sales_tax_2 := ((L.ln_sales_tax)^2)*D.ln_sales_tax]
+
+
+# Keep relevant years
+all_pi <- all_pi[between(year, 2008, 2014)]
+all_pi <- all_pi[ year >= 2009] ## Year 2008, the difference was imputed not real data - so we drop it
+
+### Run DID yearly data in changes only --------------------------------
+
+for (Y in c(outcomes)) {
+  for (FE in FE_opts) {
+    
+    # First only interaction
+    formula1 <- as.formula(paste0(
+      Y, "~ D.ln_sales_tax + D.ln_sales_tax_L.ln_sales_tax | ", FE, " | 0 | module_by_state"
+    ))
+    flog.info("Estimating with %s as outcome with %s FE.", Y, FE)
+    res1 <- felm(formula = formula1, data = all_pi,
+                 weights = all_pi$base.sales)
+    flog.info("Finished estimating with %s as outcome with %s FE.", Y, FE)
+    
+    
+    ## attach results
+    flog.info("Writing results...")
+    res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+    res1.dt[, outcome := Y]
+    res1.dt[, controls := FE]
+    res1.dt[, window := "year"]
+    res1.dt[, spec := "inter1"]
+    # Add summary values
+    res1.dt[, Rsq := summary(res1)$r.squared]
+    res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
+    res1.dt[, N_obs := nrow(all_pi)]
+    res1.dt[, N_modules := length(unique(all_pi$product_module_code))]
+    res1.dt[, N_stores :=  length(unique(all_pi$store_code_uc))]
+    res1.dt[, N_counties := uniqueN(all_pi, by = c("fips_state", "fips_county"))]
+    res1.dt[, N_years := uniqueN(all_pi, by = c("year"))]
+    res1.dt[, N_county_modules := uniqueN(all_pi, by = c("fips_state", "fips_county",
+                                                         "product_module_code"))]
+    LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
+    fwrite(LRdiff_res, output.results.file)
+    
+    # Second add interaction with squared
+    formula1 <- as.formula(paste0(
+      Y, "~ D.ln_sales_tax + D.ln_sales_tax_L.ln_sales_tax + D.ln_sales_tax_L.ln_sales_tax_2 | ", FE, " | 0 | module_by_state"
+    ))
+    flog.info("Estimating with %s as outcome with %s FE.", Y, FE)
+    res1 <- felm(formula = formula1, data = all_pi,
+                 weights = all_pi$base.sales)
+    flog.info("Finished estimating with %s as outcome with %s FE.", Y, FE)
+    
+    
+    ## attach results
+    flog.info("Writing results...")
+    res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+    res1.dt[, outcome := Y]
+    res1.dt[, controls := FE]
+    res1.dt[, window := "year"]
+    res1.dt[, spec := "inter2"]
+    # Add summary values
+    res1.dt[, Rsq := summary(res1)$r.squared]
+    res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
+    res1.dt[, N_obs := nrow(all_pi)]
+    res1.dt[, N_modules := length(unique(all_pi$product_module_code))]
+    res1.dt[, N_stores :=  length(unique(all_pi$store_code_uc))]
+    res1.dt[, N_counties := uniqueN(all_pi, by = c("fips_state", "fips_county"))]
+    res1.dt[, N_years := uniqueN(all_pi, by = c("year"))]
+    res1.dt[, N_county_modules := uniqueN(all_pi, by = c("fips_state", "fips_county",
+                                                         "product_module_code"))]
+    LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
+    fwrite(LRdiff_res, output.results.file)   
+    
+  }
+}
+
+
