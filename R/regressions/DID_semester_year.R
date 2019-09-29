c0ddb2d3d532db44d536c43c436144aad8e4ba85
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2019-09-29 11:30:23 -0500

    DD basic

--- R/regressions/DID_semester_year.R
+++ R/regressions/DID_semester_year.R
@@ -23,12 +23,52 @@ output.results.file <- "Data/DID_semesterly_and_yearly.csv"
 ### Set up Semester Data ---------------------------------
 all_pi <- fread(data.semester)
 
-outcomes <- c("D.ln_cpricei2", "D.ln_quantity3")
+outcomes.changes <- c("D.ln_cpricei2", "D.ln_quantity3")
+outcomes.levels <- c("ln_cpricei2", "ln_quantity3")
 FE_opts <- c("region_by_module_by_time", "division_by_module_by_time")
 
 ### Run DID semester data --------------------------------
 LRdiff_res <- data.table(NULL)
-for (Y in c(outcomes)) {
+## Run in levels
+for (Y in c(outcomes.levels)) {
+  for (FE in FE_opts) {
+    
+    formula1 <- as.formula(paste0(
+      Y, "~ ln_sales_tax | store_by_module +", FE, " | 0 | module_by_state"
+    ))
+    flog.info("Estimating with %s as outcome with %s FE.", Y, FE)
+    res1 <- felm(formula = formula1, data = all_pi,
+                 weights = all_pi$base.sales)
+    flog.info("Finished estimating with %s as outcome with %s FE.", Y, FE)
+    
+    
+    ## attach results
+    flog.info("Writing results...")
+    res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+    res1.dt[, outcome := Y]
+    res1.dt[, controls := FE]
+    res1.dt[, window := "semester"]
+    res1.dt[, spec := "levels"]
+    # Add summary values
+    res1.dt[, Rsq := summary(res1)$r.squared]
+    res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
+    res1.dt[, N_obs := nrow(all_pi)]
+    res1.dt[, N_modules := length(unique(all_pi$product_module_code))]
+    res1.dt[, N_stores :=  length(unique(all_pi$store_code_uc))]
+    res1.dt[, N_counties := uniqueN(all_pi, by = c("fips_state", "fips_county"))]
+    res1.dt[, N_years := uniqueN(all_pi, by = c("year"))]
+    res1.dt[, N_county_modules := uniqueN(all_pi, by = c("fips_state", "fips_county",
+                                                         "product_module_code"))]
+    LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
+    fwrite(LRdiff_res, output.results.file)
+    
+  }
+}
+
+
+
+## Run in Diffs
+for (Y in c(outcomes.changes)) {
   for (FE in FE_opts) {
     
     formula1 <- as.formula(paste0(
