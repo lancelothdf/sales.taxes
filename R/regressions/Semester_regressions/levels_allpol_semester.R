492c9d45bd1f6bdb1ee6d1035fe9463b8d9fa461
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2019-10-04 08:55:08 -0500

    Export Original predicted values in levels

--- R/regressions/Semester_regressions/levels_allpol_semester.R
+++ R/regressions/Semester_regressions/levels_allpol_semester.R
@@ -58,85 +58,86 @@ fwrite(report, report.out)
 
 ## For predicted values
 # Discretize taxrate. Between 5th and 95th percentile of the (weighted) distribution of ln_sales_tax in the sample with changes only.
-tax_values <-seq(quantile(all_pi$ln_sales_tax_r, probs = 0.05, na.rm = T, weight=all_pi$base.sales),
-                 quantile(all_pi$ln_sales_tax_r, probs = 0.95, na.rm = T, weight=all_pi$base.sales),
-                 length.out = 15)
+# tax_values <-seq(quantile(all_pi$ln_sales_tax_r, probs = 0.05, na.rm = T, weight=all_pi$base.sales),
+#                  quantile(all_pi$ln_sales_tax_r, probs = 0.95, na.rm = T, weight=all_pi$base.sales),
+#                  length.out = 15)
+tax_values <-seq(min(all_pi$ln_sales_tax, na.rm = T), max(all_pi$ln_sales_tax, na.rm = T), length.out = 15)
 # The value of 0 is problematic: replace it for a very small value
 if (tax_values[1] == 0) tax_values[1] <- 0.001
 
 # ### Run level twoway FE semester data --------------------------------
 LRdiff_res <- data.table(NULL)
-# for (n in 2:5) {
-#   # First create power
-#   all_pi[, paste0("ln_sales_tax_",n) := ln_sales_tax^(n)]
-#   # Add to formula
-#   RHS <- paste(RHS, paste0("ln_sales_tax_",n), sep = " + ")
-# 
-#   for (Y in c(outcomes)) {
-#     for (FE in FE_opts) {
-#       
-#       formula1 <- as.formula(paste0(
-#         Y, "~", RHS ," | ", FE, " + store_by_module | 0 | module_by_state"
-#       ))
-#       flog.info("Estimating with %s as outcome with %s FE.", Y, FE)
-#       res1 <- felm(formula = formula1, data = all_pi,
-#                    weights = all_pi$base.sales)
-#       flog.info("Finished estimating with %s as outcome with %s FE.", Y, FE)
-#       
-#       
-#       ## attach results
-#       flog.info("Writing results...")
-#       res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
-#       res1.dt[, outcome := Y]
-#       res1.dt[, controls := FE]
-#       res1.dt[, poly := "standard"]
-#       res1.dt[, degree.or := n]
-#       # Add summary values
-#       res1.dt[, Rsq := summary(res1)$r.squared]
-#       res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
-#       res1.dt[, N_obs := nrow(all_pi)]
-#       res1.dt[, N_modules := length(unique(all_pi$product_module_code))]
-#       res1.dt[, N_stores :=  length(unique(all_pi$store_code_uc))]
-#       res1.dt[, N_counties := uniqueN(all_pi, by = c("fips_state", "fips_county"))]
-#       res1.dt[, N_years := uniqueN(all_pi, by = c("year"))]
-#       res1.dt[, N_county_modules := uniqueN(all_pi, by = c("fips_state", "fips_county",
-#                                                            "product_module_code"))]
-#       LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
-#       fwrite(LRdiff_res, output.results.file)
-#       
-# 
-#       
-#       # Compute predicted values of the derivative
-#       pred_b <- rep(0,15)
-#       pred_se <- rep(0,15)
-#       for (i in 1:15) {
-#         plc.formula1 <- paste0("ln_sales_tax +", paste0(paste0(paste0(2:n,"*",paste0((tax_values[i]),"^",1:(n-1))), "*ln_sales_tax_",2:n), collapse = " + "), " = 0")
-#         # Predictred
-#         pplc.test1 <- glht(res1, linfct = c(plc.formula1))
-#         pred_b[i] <- coef(summary(pplc.test1))[[1]]
-#         pred_se[i] <- sqrt(vcov(summary(pplc.test1)))[[1]]
-#       }
-#       # Create data
-#       coef.dt <- data.table(tax_values, pred_b, pred_se)
-#       out.pred.file <- paste0(output.path,"/standard/predict",  Y, "_", n,"_", FE,".csv")
-#       fwrite(coef.dt, out.pred.file)
-#       
-#       # Output file
-#       graphout <- paste0(output.path,"/standard/", Y, "_", n,"_", FE, ".png")
-#       # Plot
-#       ggplot(data = coef.dt, mapping = aes(x = tax_values, y = pred_b)) +
-#         geom_point(size = 2, alpha = .5) +
-#         geom_line(linetype = "dashed") +
-#         geom_errorbar(aes(ymin = pred_b - 1.96 * pred_se,
-#                           ymax = pred_b + 1.96 * pred_se), width = .005) +
-#         theme_bw() +
-#         labs(x = "Sales Tax", y = paste0("Predicted response on ",Y), color = NULL) +
-#         geom_hline(yintercept = 0, color = "red", linetype = "dashed", alpha = .8)
-#       ggsave(graphout)
-#       
-#     }
-#   }
-# }
+for (n in 2:5) {
+  # First create power
+  all_pi[, paste0("ln_sales_tax_",n) := ln_sales_tax^(n)]
+  # Add to formula
+  RHS <- paste(RHS, paste0("ln_sales_tax_",n), sep = " + ")
+
+  for (Y in c(outcomes)) {
+    for (FE in FE_opts) {
+
+      formula1 <- as.formula(paste0(
+        Y, "~", RHS ," | ", FE, " + store_by_module | 0 | module_by_state"
+      ))
+      flog.info("Estimating with %s as outcome with %s FE.", Y, FE)
+      res1 <- felm(formula = formula1, data = all_pi,
+                   weights = all_pi$base.sales)
+      flog.info("Finished estimating with %s as outcome with %s FE.", Y, FE)
+
+
+      ## attach results
+      flog.info("Writing results...")
+      res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+      res1.dt[, outcome := Y]
+      res1.dt[, controls := FE]
+      res1.dt[, poly := "standard"]
+      res1.dt[, degree.or := n]
+      # Add summary values
+      res1.dt[, Rsq := summary(res1)$r.squared]
+      res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
+      res1.dt[, N_obs := nrow(all_pi)]
+      res1.dt[, N_modules := length(unique(all_pi$product_module_code))]
+      res1.dt[, N_stores :=  length(unique(all_pi$store_code_uc))]
+      res1.dt[, N_counties := uniqueN(all_pi, by = c("fips_state", "fips_county"))]
+      res1.dt[, N_years := uniqueN(all_pi, by = c("year"))]
+      res1.dt[, N_county_modules := uniqueN(all_pi, by = c("fips_state", "fips_county",
+                                                           "product_module_code"))]
+      LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
+      fwrite(LRdiff_res, output.results.file)
+
+
+
+      # Compute predicted values of the derivative
+      pred_b <- rep(0,15)
+      pred_se <- rep(0,15)
+      for (i in 1:15) {
+        plc.formula1 <- paste0("ln_sales_tax +", paste0(paste0(paste0(2:n,"*",paste0((tax_values[i]),"^",1:(n-1))), "*ln_sales_tax_",2:n), collapse = " + "), " = 0")
+        # Predictred
+        pplc.test1 <- glht(res1, linfct = c(plc.formula1))
+        pred_b[i] <- coef(summary(pplc.test1))[[1]]
+        pred_se[i] <- sqrt(vcov(summary(pplc.test1)))[[1]]
+      }
+      # Create data
+      coef.dt <- data.table(tax_values, pred_b, pred_se)
+      out.pred.file <- paste0(output.path,"/standard/predict",  Y, "_", n,"_", FE,".csv")
+      fwrite(coef.dt, out.pred.file)
+
+      # Output file
+      graphout <- paste0(output.path,"/standard/", Y, "_", n,"_", FE, ".png")
+      # Plot
+      ggplot(data = coef.dt, mapping = aes(x = tax_values, y = pred_b)) +
+        geom_point(size = 2, alpha = .5) +
+        geom_line(linetype = "dashed") +
+        geom_errorbar(aes(ymin = pred_b - 1.96 * pred_se,
+                          ymax = pred_b + 1.96 * pred_se), width = .005) +
+        theme_bw() +
+        labs(x = "Sales Tax", y = paste0("Predicted response on ",Y), color = NULL) +
+        geom_hline(yintercept = 0, color = "red", linetype = "dashed", alpha = .8)
+      ggsave(graphout)
+
+    }
+  }
+}
 
 ### Set up Hermite Polynomials ----------------------------------------------
 
