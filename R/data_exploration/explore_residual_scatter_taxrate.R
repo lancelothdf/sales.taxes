60b7d1a8dcc1081fa0153189b819c46111da5e01
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2019-10-08 10:55:08 -0500

    Plot residuals scatters

---
+++ R/data_exploration/explore_residual_scatter_taxrate.R
@@ -0,0 +1,70 @@
+#' Explore quantities and prices 
+#' Plot scatter plots to disentangle what goes on at larger values of tax rates
+#' We plot the residualized outcomes in the semester data
+#' We use division x module x time for now
+
+
+library(data.table)
+library(futile.logger)
+library(lfe)
+library(multcomp)
+library(ggplot2)
+
+
+setwd("/project2/igaarder")
+
+
+## input filepaths -----------------------------------------------
+data.semester <- "Data/Nielsen/semester_nielsen_data.csv"
+
+## output filepaths ----------------------------------------------
+output.path <- "../../home/slacouture/NLP"
+
+
+### Set up Semester Data ---------------------------------
+all_pi <- fread(data.semester)
+
+outcomes <- c("ln_cpricei2", "ln_quantity3")
+outcomesFD <- c("D.ln_cpricei2", "D.ln_quantity3")
+
+#### Run loop ----------------------
+for (Y in outcomes) {
+  
+  # Run the FE regression with weights
+  formula1 <- as.formula(paste0(
+    Y, " ~ 1 | division_by_module_by_time + store_by_module | 0 | module_by_state"
+  ))
+  res1 <- felm(formula = formula1, data = all_pi,
+               weights = all_pi$base.sales)
+  
+  # Residualize
+  all_pi$res <- res1$residuals
+  
+  # Plot the residuals
+  graphout <- paste0(output.path, "res_",Y, ".png")
+  ggplot(all_pi,aes(x = ln_sales_tax, y = res, weights = base.sales)) + 
+    stat_binhex()
+  ggsave(graphout)
+  
+}
+
+for (Y in outcomesFD) {
+  
+  # Run the FE regression with weights
+  formula1 <- as.formula(paste0(
+    Y, " ~ 1 | division_by_module_by_time | 0 | module_by_state"
+  ))
+  res1 <- felm(formula = formula1, data = all_pi,
+               weights = all_pi$base.sales)
+  
+  # Residualize
+  all_pi$res <- res1$residuals
+  
+  # Plot the residuals
+  graphout <- paste0(output.path, "res_",Y, ".png")
+  ggplot(all_pi,aes(x = ln_sales_tax, y = res, weights = base.sales)) + 
+    stat_binhex()
+  ggsave(graphout)
+  
+}
+
