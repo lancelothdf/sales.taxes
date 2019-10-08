a43077b3b711f4cac47d83d3dd5bd574f177a704
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2019-10-08 15:29:24 -0500

    Make better plots for doble residual by bin

--- R/data_exploration/explore_residual_scatter_taxrate.R
+++ R/data_exploration/explore_residual_scatter_taxrate.R
@@ -27,7 +27,83 @@ all_pi <- fread(data.semester)
 outcomes <- c("ln_cpricei2", "ln_quantity3")
 outcomesFD <- c("D.ln_cpricei2", "D.ln_quantity3")
 
-#### Run loop ----------------------
+# #### Run loop res vs. lev ----------------------
+# for (Y in outcomes) {
+#   
+#   # Run the FE regression with weights
+#   formula1 <- as.formula(paste0(
+#     Y, " ~ 1 | division_by_module_by_time + store_by_module | 0 | module_by_state"
+#   ))
+#   res1 <- felm(formula = formula1, data = all_pi,
+#                weights = all_pi$base.sales)
+#   
+#   # Residualize
+#   all_pi$res <- res1$residuals
+#   
+#   # Plot the residuals
+#   graphout <- paste0(output.path, "/res_",Y, ".png")
+#   ggplot(all_pi, aes(x = ln_sales_tax, y = res, weights = base.sales)) + 
+#     stat_binhex(aes(fill=log(..count..)), bins = 50)
+#   ggsave(graphout)
+#   
+#   # Plot the residuals comming from non-0 changes
+#   graph.data <- all_pi[D.ln_sales_tax !=0,]
+#   graphout <- paste0(output.path, "/res_",Y, "no0change.png")
+#   ggplot(graph.data, aes(x = ln_sales_tax, y = res, weights = base.sales)) + 
+#     stat_binhex(aes(fill=log(..count..)), bins = 50)
+#   ggsave(graphout)
+#   
+#   
+# }
+# 
+# for (Y in outcomesFD) {
+#   
+#   # Run the FE regression with weights
+#   formula1 <- as.formula(paste0(
+#     Y, " ~ 1 | division_by_module_by_time | 0 | module_by_state"
+#   ))
+#   res1 <- felm(formula = formula1, data = all_pi,
+#                weights = all_pi$base.sales)
+#   
+#   # Residualize
+#   all_pi$res <- res1$residuals
+#   
+#   # Plot the residuals
+#   graphout <- paste0(output.path, "/res_",Y, ".png")
+#   ggplot(all_pi, aes(x = ln_sales_tax, y = res, weights = base.sales)) + 
+#     stat_binhex(aes(fill=log(..count..)), bins = 50)
+#   ggsave(graphout)
+#   
+# 
+#   # Plot the residuals comming from non-0 changes
+#   graph.data <- all_pi[D.ln_sales_tax !=0,]
+#   graphout <- paste0(output.path, "/res_",Y, "no0change.png")
+#   ggplot(graph.data, aes(x = ln_sales_tax, y = res, weights = base.sales)) + 
+#     stat_binhex(aes(fill=log(..count..)), bins = 50)
+#   ggsave(graphout)
+#   
+# 
+# }
+
+### Res vs. res set up --------
+
+# Create "initial" tax rate bin
+all_pi[, L.ln_sales_tax := ln_sales_tax - D.ln_sales_tax]
+all_pi[, init_tax_bin := floor(L.ln_sales_tax/0.0125) ]
+
+# Residualize tax rate and D. tax rate
+taxlev <- felm(formula = as.formula("ln_sales_tax ~ 1 | division_by_module_by_time + store_by_module | 0 | module_by_state"),
+               data = all_pi,
+               weights = all_pi$base.sales)
+all_pi$ln_sales_tax.res <- taxlev$residuals
+
+taxfd <- felm(formula = as.formula("D.ln_sales_tax ~ 1 | division_by_module_by_time | 0 | module_by_state"),
+               data = all_pi,
+               weights = all_pi$base.sales)
+all_pi$D.ln_sales_tax.res <- taxfd$residuals
+
+
+#### Run loop res vs. res set up ----------------------
 for (Y in outcomes) {
   
   # Run the FE regression with weights
@@ -40,20 +116,29 @@ for (Y in outcomes) {
   # Residualize
   all_pi$res <- res1$residuals
   
-  # Plot the residuals
-  graphout <- paste0(output.path, "/res_",Y, ".png")
-  ggplot(all_pi, aes(x = ln_sales_tax, y = res, weights = base.sales)) + 
-    stat_binhex(aes(fill=log(..count..)), bins = 50)
-  ggsave(graphout)
-  
-  # Plot the residuals comming from non-0 changes
-  graph.data <- all_pi[D.ln_sales_tax !=0,]
-  graphout <- paste0(output.path, "/res_",Y, "no0change.png")
-  ggplot(graph.data, aes(x = ln_sales_tax, y = res, weights = base.sales)) + 
-    stat_binhex(aes(fill=log(..count..)), bins = 50)
-  ggsave(graphout)
-  
-  
+  # Plot the residuals by bin
+  for (bin in unique(all_pi$init_tax_bin)) {
+    
+    ## Full residuals
+    graph.data <- all_pi[init_tax_bin == bin,]
+    graphout <- paste0(output.path, "/res by bin/res_",Y, "_bin", bin,".png")
+    ggplot(all_pi, aes(x = ln_sales_tax.res, y = res, weights = base.sales)) + 
+      stat_binhex(aes(fill=log(..count..)), bins = 50) +
+      labs(x = "(residualized) Sales Tax in bin", y = paste0("(residualized)", Y), color = NULL)
+      
+    ggsave(graphout)
+    
+    # Plot the residuals comming from non-0 changes
+    graph.data <- graph.data[D.ln_sales_tax !=0,]
+    graphout <- paste0(output.path, "/res by bin/res_",Y, "_bin", bin, "no0change.png")
+    ggplot(graph.data, aes(x = ln_sales_tax.res, y = res, weights = base.sales)) + 
+      stat_binhex(aes(fill=log(..count..)), bins = 50) + 
+      labs(x = "(residualized) Sales Tax in bin", y = paste0("(residualized)", Y), color = NULL)
+    ggsave(graphout)    
+    
+    
+  }
+
 }
 
 for (Y in outcomesFD) {
@@ -68,20 +153,29 @@ for (Y in outcomesFD) {
   # Residualize
   all_pi$res <- res1$residuals
   
-  # Plot the residuals
-  graphout <- paste0(output.path, "/res_",Y, ".png")
-  ggplot(all_pi, aes(x = ln_sales_tax, y = res, weights = base.sales)) + 
-    stat_binhex(aes(fill=log(..count..)), bins = 50)
-  ggsave(graphout)
+  # Plot the residuals by bin
+  for (bin in unique(all_pi$init_tax_bin)) {
+    
+    ## Full residuals
+    graph.data <- all_pi[init_tax_bin == bin,]
+    graphout <- paste0(output.path, "/res by bin/res_",Y, "_bin", bin,".png")
+    ggplot(all_pi, aes(x = D.ln_sales_tax.res, y = res, weights = base.sales)) + 
+      stat_binhex(aes(fill=log(..count..)), bins = 50) +
+      labs(x = "(residualized) \Delta Sales Tax in bin", y = paste0("(residualized)", Y), color = NULL)
+    
+    ggsave(graphout)
+    
+    # Plot the residuals comming from non-0 changes
+    graph.data <- graph.data[D.ln_sales_tax !=0,]
+    graphout <- paste0(output.path, "/res by bin/res_",Y, "_bin", bin, "no0change.png")
+    ggplot(graph.data, aes(x = D.ln_sales_tax.res, y = res, weights = base.sales)) + 
+      stat_binhex(aes(fill=log(..count..)), bins = 50) + 
+      labs(x = "(residualized) \Delta Sales Tax in bin", y = paste0("(residualized)", Y), color = NULL)
+    ggsave(graphout)    
+    
+    
+  }
   
-
-  # Plot the residuals comming from non-0 changes
-  graph.data <- all_pi[D.ln_sales_tax !=0,]
-  graphout <- paste0(output.path, "/res_",Y, "no0change.png")
-  ggplot(graph.data, aes(x = ln_sales_tax, y = res, weights = base.sales)) + 
-    stat_binhex(aes(fill=log(..count..)), bins = 50)
-  ggsave(graphout)
   
-
 }
 
