9171b6d43ee7ddc4289a3b7a8931e1d1d085cb68
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2019-10-19 08:59:30 -0500

    Include within in DiD estimation

--- R/regressions/DID_semester_year.R
+++ R/regressions/DID_semester_year.R
@@ -22,9 +22,14 @@ output.results.file <- "Data/DID_semesterly_and_yearly.csv"
 
 ### Set up Semester Data ---------------------------------
 all_pi <- fread(data.semester)
+all_pi[, w.ln_sales_tax := ln_sales_tax - mean(ln_sales_tax), by = .(store_by_module)]
+all_pi[, w.ln_cpricei2 := ln_cpricei2 - mean(ln_cpricei2), by = .(store_by_module)]
+all_pi[, w.ln_quantity3 := ln_quantity3 - mean(ln_quantity3), by = .(store_by_module)]
+
 
 outcomes.changes <- c("D.ln_cpricei2", "D.ln_quantity3")
 outcomes.levels <- c("ln_cpricei2", "ln_quantity3")
+outcomes.within <- c("w.ln_cpricei2", "w.ln_quantity3")
 FE_opts <- c("region_by_module_by_time", "division_by_module_by_time")
 
 ### Run DID semester data --------------------------------
@@ -103,11 +108,49 @@ for (Y in c(outcomes.changes)) {
   }
 }
 
+
+## Run within
+for (Y in c(outcomes.within)) {
+  for (FE in FE_opts) {
+    
+    formula1 <- as.formula(paste0(
+      Y, "~ w.ln_sales_tax | ", FE, " | 0 | module_by_state"
+    ))
+    flog.info("Estimating with %s as outcome with %s FE.", Y, FE)
+    res1 <- felm(formula = formula1, data = all_pi,
+                 weights = all_pi$base.sales)
+    flog.info("Finished estimating with %s as outcome with %s FE.", Y, FE)
+    
+    
+    ## attach results
+    flog.info("Writing results...")
+    res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+    res1.dt[, outcome := Y]
+    res1.dt[, controls := FE]
+    res1.dt[, window := "semester"]
+    res1.dt[, spec := "within"]
+    # Add summary values
+    res1.dt[, Rsq := summary(res1)$r.squared]
+    res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
+    res1.dt[, N_obs := nrow(all_pi)]
+    res1.dt[, N_modules := length(unique(all_pi$product_module_code))]
+    res1.dt[, N_stores :=  length(unique(all_pi$store_code_uc))]
+    res1.dt[, N_counties := uniqueN(all_pi, by = c("fips_state", "fips_county"))]
+    res1.dt[, N_years := uniqueN(all_pi, by = c("year"))]
+    res1.dt[, N_county_modules := uniqueN(all_pi, by = c("fips_state", "fips_county",
+                                                         "product_module_code"))]
+    LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
+    fwrite(LRdiff_res, output.results.file)
+    
+  }
+}
+
 ### Set up Yearly Data ---------------------------------
 all_pi <- fread(data.year)
 
 outcomes.changes <- c("D.ln_cpricei2", "D.ln_quantity3")
 outcomes.levels <- c("ln_cpricei2", "ln_quantity3")
+outcomes.within <- c("w.ln_cpricei2", "w.ln_quantity3")
 FE_opts <- c("region_by_module_by_time", "division_by_module_by_time")
 
 ## merge region a division
@@ -148,6 +191,10 @@ all_pi[, module_by_state := .GRP, by = .(product_module_code, fips_state)]
 
 # Keep relevant years
 all_pi <- all_pi[between(year, 2008, 2014)]
+all_pi[, w.ln_sales_tax := ln_sales_tax - mean(ln_sales_tax), by = .(store_by_module)]
+all_pi[, w.ln_cpricei2 := ln_cpricei2 - mean(ln_cpricei2), by = .(store_by_module)]
+all_pi[, w.ln_quantity3 := ln_quantity3 - mean(ln_quantity3), by = .(store_by_module)]
+
 
 ### Run DID yearly data in changes and levels --------------------------------
 ## Run in levels
@@ -225,4 +272,40 @@ for (Y in c(outcomes.changes)) {
   }
 }
 
+
+## Run within
+for (Y in c(outcomes.within)) {
+  for (FE in FE_opts) {
+    
+    formula1 <- as.formula(paste0(
+      Y, "~ w.ln_sales_tax | ", FE, " | 0 | module_by_state"
+    ))
+    flog.info("Estimating with %s as outcome with %s FE.", Y, FE)
+    res1 <- felm(formula = formula1, data = all_pi,
+                 weights = all_pi$base.sales)
+    flog.info("Finished estimating with %s as outcome with %s FE.", Y, FE)
+    
+    
+    ## attach results
+    flog.info("Writing results...")
+    res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+    res1.dt[, outcome := Y]
+    res1.dt[, controls := FE]
+    res1.dt[, window := "year"]
+    res1.dt[, spec := "within"]
+    # Add summary values
+    res1.dt[, Rsq := summary(res1)$r.squared]
+    res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
+    res1.dt[, N_obs := nrow(all_pi)]
+    res1.dt[, N_modules := length(unique(all_pi$product_module_code))]
+    res1.dt[, N_stores :=  length(unique(all_pi$store_code_uc))]
+    res1.dt[, N_counties := uniqueN(all_pi, by = c("fips_state", "fips_county"))]
+    res1.dt[, N_years := uniqueN(all_pi, by = c("year"))]
+    res1.dt[, N_county_modules := uniqueN(all_pi, by = c("fips_state", "fips_county",
+                                                         "product_module_code"))]
+    LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
+    fwrite(LRdiff_res, output.results.file)
+    
+  }
+}
 
