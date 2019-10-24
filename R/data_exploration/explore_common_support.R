99ab972814b613d3c8dd594d203a531415608885
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2019-10-24 15:02:27 -0500

    Explore and plot common support

--- R/data_exploration/explore_common_support.R
+++ R/data_exploration/explore_common_support.R
@@ -59,8 +59,8 @@ all_pi$treatment <- as.factor(all_pi$treatment) ## HAs to be a factor for plots
 ## Full sample
 graphout <- paste0(output.path,"/lag_tax_group.png")
 hist <- ggplot(all_pi, aes(L.ln_sales_tax, fill = treatment, weight = base.sales)) + 
-  geom_histogram(alpha = 0.3, aes(y=..count../sum(..count..)), position="identity") +    
-  theme_classic(base_size = 24) +
+  geom_histogram(alpha = 0.3, aes(y=..count../sum(..count..), fill = treatment), position="identity") +    
+  theme_classic(base_size = 24, legend.position="bottom") +
   labs(x = "Sales Tax", y = "Fraction", color = NULL) +
   scale_fill_discrete(name = "Group", labels = c("Tax Change", "No Change"))
 ggsave(graphout)
@@ -68,8 +68,8 @@ ggsave(graphout)
 ## Common support
 graphout <- paste0(output.path,"/lag_tax_group_support.png")
 hist <- ggplot(data=subset(all_pi,cs_tax == 1), aes(L.ln_sales_tax, fill = treatment, weight = base.sales)) + 
-  geom_histogram(alpha = 0.3, aes(y=..count../sum(..count..)), position="identity") +    
-  theme_classic(base_size = 24) +
+  geom_histogram(alpha = 0.3, aes(y=..count../sum(..count..), fill = treatment), position="identity") +    
+  theme_classic(base_size = 24, legend.position="bottom") +
   labs(x = "Sales Tax", y = "Fraction", color = NULL) +
   scale_fill_discrete(name = "Group", labels = c("Tax Change", "No Change"))
 ggsave(graphout)
@@ -93,18 +93,18 @@ all_pi[, cs_price := ifelse(is.na(dm.L.ln_cpricei2), 0, cs_price)]
 ## Full sample
 graphout <- paste0(output.path,"/lag_price_group.png")
 hist <- ggplot(data=all_pi, aes(dm.L.ln_cpricei2, fill = treatment, weight = base.sales)) + 
-  geom_histogram(alpha = 0.3, aes(y=..count../sum(..count..)), position="identity") +    
-  theme_classic(base_size = 24) +
+  geom_histogram(alpha = 0.3, aes(y=..count../sum(..count..), fill = treatment), position="identity") +    
+  theme_classic(base_size = 24, legend.position="bottom") +
   scale_x_continuous(limits = c(-0.5,0.5), breaks = seq(-0.5, 0.5, 0.1)) +
   labs(x = "Demeaned Price", y = "Fraction", color = NULL) +
   scale_fill_discrete(name = "Group", labels = c("Tax Change", "No Change"))
 ggsave(graphout)
 
 ## Common support
-graphout <- paste0(output.path,"/lag_price_grupo_support.png")
+graphout <- paste0(output.path,"/lag_price_gruop_support.png")
 hist <- ggplot(data=subset(all_pi,cs_price == 1), aes(dm.L.ln_cpricei2, fill = treatment,  weight = base.sales)) + 
-  geom_histogram(alpha = 0.3, aes(y=..count../sum(..count..)), position="identity") +    
-  theme_classic(base_size = 24) +
+  geom_histogram(alpha = 0.3, aes(y=..count../sum(..count..), fill = treatment), position="identity") +    
+  theme_classic(base_size = 24, legend.position="bottom") +
   labs(x = "Demeaned Price", y = "Fraction", color = NULL) +
   scale_fill_discrete(name = "Group", labels = c("Tax Change", "No Change"))
 ggsave(graphout)
