698ef7f657858bd341a689f82c5a3ac00cc48a36
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2019-10-28 08:53:47 -0500

    Saturate by initial tax polynomial

--- R/regressions/Semester_regressions/DID_semester_cs_inittax_satpoly.R
+++ R/regressions/Semester_regressions/DID_semester_cs_inittax_satpoly.R
@@ -3,8 +3,7 @@
 #' The idea is to try to see whether we observe non linearities in this sense
 #' We estimate both the "short run" and the "long run" models splitting the sample
 #' by quantiles increasing the number of groups.
-#' Here initial level means previous period and we divide by groups within the "common" support
-#' In this case, we run a fully saturated model (instead of splitting the sample)
+#' Here initial level means previous period and we use polynomials and fully interact the model
 
 
 library(data.table)
@@ -23,7 +22,8 @@ data.year <- "Data/Nielsen/yearly_nielsen_data.csv"
 
 
 ## output filepaths ----------------------------------------------
-output.results.file <- "Data/DID_sat_initial_tax_semester.csv"
+output.results.file <- "Data/DID_satpoly_initial_tax_semester.csv"
+output.path <- "../../home/slacouture/NLP/satpoly/bytaxes"
 
 ### Set up Semester Data ---------------------------------
 all_pi <- fread(data.semester)
@@ -55,34 +55,40 @@ all_pi[, cs_tax := ifelse(is.na(L.ln_sales_tax), 0, cs_tax)]
 ## Keep within the common support
 all_pi <- all_pi[cs_tax == 1,]
 
+# Discretize taxrate: common support
+tax_values <-seq(min(all_pi$L.ln_sales_tax, na.rm = T), max(all_pi$L.ln_sales_tax, na.rm = T), length.out = 15)
+# The value of 0 is problematic: replace it for a very small value
+if (tax_values_full[1] == 0) tax_values_full[1] <- 0.001
+# average tax changes (for prediction)
+av.tax.ch <- 1
 
 
 outcomes.changes <- c("D.ln_cpricei2", "D.ln_quantity3")
 outcomes.within <- c("w.ln_cpricei2", "w.ln_quantity3")
-FE_opts <- c("group_region_by_module_by_time", "group_division_by_module_by_time")
+FE_opts <- c("region_by_module_by_time", "division_by_module_by_time")
 
 
 LRdiff_res <- data.table(NULL)
 ## Run in Diffs
 for (Y in c(outcomes.changes)) {
-  for (n.g in 2:5) {
+  RHS1 <- "D.ln_sales_tax"
+  for (n in 1:3) {
     
-    # Create groups of initial values of tax rate
-    # We use the full weighted distribution
-    all_pi <- all_pi[, quantile := cut(L.ln_sales_tax,
-                                        breaks = quantile(L.ln_sales_tax, probs = seq(0, 1, by = 1/n.g), na.rm = T, weight = base.sales),
-                                        labels = 1:n.g, right = FALSE)]
-    quantlab <- round(quantile(all_pi$L.ln_sales_tax, 
-                               probs = seq(0, 1, by = 1/n.g), na.rm = T, 
-                               weight = all_pi$base.sales), digits = 4)
-    # Saturate fixed effects
-    all_pi[, group_region_by_module_by_time := .GRP, by = .(region_by_module_by_time, quantile)]
-    all_pi[, group_division_by_module_by_time := .GRP, by = .(division_by_module_by_time, quantile)]
+    # Create polynomial of initial tax rate of degree "n"
+    all_pi[, paste0("D.ln_sales_tax_init_",n) := D.ln_sales_tax*(L.ln_sales_tax^(n))]
+    # Add to formula 1
+    RHS1 <- paste(RHS1, paste0("D.ln_sales_tax_init_",n), sep = " + ")
+    
+    # Create polynomial of initial rate to interact with FE
+    all_pi[, paste0("L.ln_sales_tax_",n) := (L.ln_sales_tax^(n))]
     
     for (FE in FE_opts) {
       
+      # Saturate fixed effects
+      RHS2 <- paste0(paste0(FE,":L.ln_sales_tax_",1:n), collapse = " + ")
+      
       formula1 <- as.formula(paste0(
-        Y, "~ D.ln_sales_tax:quantile | ", FE, "+ quantile | 0 | module_by_state"
+        Y, "~ ", RHS1,"| ", FE, "+", RHS2," | 0 | module_by_state"
       ))
       flog.info("Estimating with %s as outcome with %s FE.", Y, FE)
       res1 <- felm(formula = formula1, data = all_pi,
@@ -97,8 +103,7 @@ for (Y in c(outcomes.changes)) {
       res1.dt[, controls := FE]
       res1.dt[, window := "semester"]
       res1.dt[, spec := "changes"]
-      res1.dt[, n.groups := n.g]
-      res1.dt[, lev := quantlab[-1]]
+      res1.dt[, degree := n]
       # Add summary values
       res1.dt[, Rsq := summary(res1)$r.squared]
       res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
@@ -112,7 +117,20 @@ for (Y in c(outcomes.changes)) {
       LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
       fwrite(LRdiff_res, output.results.file)
       
-      
+      # Compute predicted values of the derivative. Common support
+      pred_b <- rep(0,15)
+      pred_se <- rep(0,15)
+      for (i in 1:15) {
+        plc.formula1 <- paste0(av.tax.ch, "*(D.ln_sales_tax + ", (tax_values[i]),"*D.ln_sales_tax_init +", paste0(paste0(paste0(paste0((tax_values[i]),"^",2:(n))), "*D.ln_sales_tax_init_",2:n), collapse = " + "), ") = 0")
+        # Predictred
+        pplc.test1 <- glht(res1, linfct = c(plc.formula1))
+        pred_b[i] <- coef(summary(pplc.test1))[[1]]
+        pred_se[i] <- sqrt(vcov(summary(pplc.test1)))[[1]]
+      }
+      # Create data
+      coef.dt <- data.table(tax_values, pred_b, pred_se)
+      out.pred.file <- paste0(output.path,"/predict_", Y, "_", n,"_", FE,".csv")
+      fwrite(coef.dt, out.pred.file)      
       
     }
   }
@@ -120,26 +138,26 @@ for (Y in c(outcomes.changes)) {
 
 
 
-## Run in Diffs
+## Run within
 for (Y in c(outcomes.within)) {
-  for (n.g in 2:5) {
+  RHS1 <- "w.ln_sales_tax"
+  for (n in 1:3) {
     
-    # Create groups of initial values of tax rate
-    # We use the full weighted distribution
-    all_pi <- all_pi[, quantile := cut(L.ln_sales_tax,
-                                       breaks = quantile(L.ln_sales_tax, probs = seq(0, 1, by = 1/n.g), na.rm = T, weight = base.sales),
-                                       labels = 1:n.g, right = FALSE)]
-    quantlab <- round(quantile(all_pi$L.ln_sales_tax, 
-                               probs = seq(0, 1, by = 1/n.g), na.rm = T, 
-                               weight = all_pi$base.sales), digits = 4)
-    # Saturate fixed effects
-    all_pi[, group_region_by_module_by_time := .GRP, by = .(region_by_module_by_time, quantile)]
-    all_pi[, group_division_by_module_by_time := .GRP, by = .(division_by_module_by_time, quantile)]
+    # Create polynomial of initial tax rate of degree "n"
+    all_pi[, paste0("w.ln_sales_tax_init_",n) := w.ln_sales_tax*(L.ln_sales_tax^(n))]
+    # Add to formula 1
+    RHS1 <- paste(RHS1, paste0("w.ln_sales_tax_init_",n), sep = " + ")
+    
+    # Create polynomial of initial rate to interact with FE
+    all_pi[, paste0("L.ln_sales_tax_",n) := (L.ln_sales_tax^(n))]
     
     for (FE in FE_opts) {
       
+      # Saturate fixed effects
+      RHS2 <- paste0(paste0(FE,":L.ln_sales_tax_",1:n), collapse = " + ")
+      
       formula1 <- as.formula(paste0(
-        Y, "~ w.ln_sales_tax:quantile | ", FE, "+ quantile | 0 | module_by_state"
+        Y, "~ ", RHS1,"| ", FE, "+", RHS2," | 0 | module_by_state"
       ))
       flog.info("Estimating with %s as outcome with %s FE.", Y, FE)
       res1 <- felm(formula = formula1, data = all_pi,
@@ -169,7 +187,20 @@ for (Y in c(outcomes.within)) {
       LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
       fwrite(LRdiff_res, output.results.file)
       
-      
+      # Compute predicted values of the derivative. Common support
+      pred_b <- rep(0,15)
+      pred_se <- rep(0,15)
+      for (i in 1:15) {
+        plc.formula1 <- paste0(av.tax.ch, "*(w.ln_sales_tax + ", (tax_values[i]),"*w.ln_sales_tax_init +", paste0(paste0(paste0(paste0((tax_values[i]),"^",2:(n))), "*w.ln_sales_tax_init_",2:n), collapse = " + "), ") = 0")
+        # Predictred
+        pplc.test1 <- glht(res1, linfct = c(plc.formula1))
+        pred_b[i] <- coef(summary(pplc.test1))[[1]]
+        pred_se[i] <- sqrt(vcov(summary(pplc.test1)))[[1]]*((res1$N - res1$p)/(res1$N - res1$p - length(unique(all_pi$store_by_module))))^(1/2)
+      }
+      # Create data
+      coef.dt <- data.table(tax_values, pred_b, pred_se)
+      out.pred.file <- paste0(output.path,"/predict_", Y, "_", n,"_", FE,".csv")
+      fwrite(coef.dt, out.pred.file)        
       
     }
   }
