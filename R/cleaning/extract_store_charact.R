202775bb07ae18b900ccef0bf9f74013696698a9
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2020-01-15 15:02:57 -0600

    Extract further stores characteristics for demographics analysis

--- R/cleaning/extract_store_charact.R
+++ R/cleaning/extract_store_charact.R
@@ -1,7 +1,7 @@
 #' This code retrieves variables to refine the sample of estimation as a robustness exploration.
 #' First we retrieve the channel type of the store to restrict only to groceries
 #' Second we try to identify "chains" as in DellaVigna and Gentzkow (2019) to exclude these
-#' 
+#' Third, we extract a store characteristics based on their consumers. Similarly to DellaVigna and Gentzkow (2019)
 
 library(data.table)
 library(lfe)
@@ -28,12 +28,12 @@ for (yr in 2006:2016) {
   rm(stores.yr)
 }
 
-## Keep only 3 big types of stores
+#### 1. Keep only 3 big types of stores ------
 stores.all <- stores.all[channel_code %in% c("F", "M", "D")]
 
 length(unique(stores.all$store_code_uc))
 
-#### Follow DellaVigna and Gentzkow identification of Chains
+#### 2. Follow DellaVigna and Gentzkow identification of Chains -------
 # Main difference:they focus in 2006 to 2014 period not 2006-2016
 
 ### A. Stores Exclusion
@@ -82,10 +82,97 @@ stores.dg <- stores.dg[, c("year", "store_code_uc")]
 stores.dg[, DGsample := 1]
 
 stores.all <- merge(stores.all, stores.dg, by = c("year", "store_code_uc"), all.x = T)
+rm(stores.dg)
 
 ## Simplify the file to avoid loading by amount of data
 stores.all <- stores.all[, c("year", "store_code_uc", "channel_code", "DGsample")]
 
+
+
+#### 3. Follow DellaVigna and Gentzkow store characteristics identification ----
+
+## Need to identify consumers for each store
+# Reset working directory to work with the Consumer PAnel
+setwd("/project2/igaarder/Data/Nielsen/Household_panel")
+
+full.purchases
+for (yr in 2006:2016) {
+  ## necessary filepaths
+  folderpath <- paste0("HMS/", yr, "/Annual_Files/")
+  panelists_file <- paste0(folderpath, "panelists_", yr, ".tsv")
+  purchases_file <- paste0(folderpath, "purchases_", yr, ".tsv")
+  trips_file <- paste0(folderpath, "trips_", yr, ".tsv")
+  store_file <- paste0("/project2/igaarder/Data/Nielsen/stores_", yr, ".dta")
+
+  ## start with purchases data
+  flog.info("Loading in purchases data for %s", yr)
+  purchases <- fread(purchases_file)
+  purchases <- purchases[, .(trip_code_uc, upc, upc_ver_uc, total_price_paid)]
+  
+  ## sum expenditures over UPCs to the trip level
+  flog.info("Summing expenditures over UPCs for %s", yr)
+  purchases <- purchases[, list(
+    total_expenditures = sum(total_price_paid)
+  ), by = .(trip_code_uc)]
+  
+  ## merge on the trip data
+  flog.info("Loading in trips data for %s", yr)
+  trips <- fread(trips_file)
+  trips[, purchase_date := as.Date(purchase_date, "%Y-%m-%d")]
+  trips[, month := month(purchase_date)]
+  trips[, year := year(purchase_date)]
+  trips <- trips[, .(trip_code_uc, household_code, store_code_uc,
+                     year, month)]
+  
+  flog.info("Merging trips data to purchases for %s", yr)
+  purchases <- merge(purchases, trips, by = "trip_code_uc")
+  rm(trips)
+  
+  ## Collapse by household (since is total there wont be problem)
+  purchases <- purchases[, list(
+    total_expenditures = sum(total_expenditures),
+    n_trips = .N), 
+    by = .(household_code, store_code_uc)]
+  
+  ## merge on some individual info
+  flog.info("Loading in panelists data for %s", yr)
+  panelists <- fread(panelists_file)
+  panelists <- panelists[, .(Household_Cd, Panelist_ZipCd)]
+  setnames(panelists,
+           old = c("Household_Cd", "Panelist_ZipCd"),
+           new = c("household_code", "zip_code"))
+  flog.info("Merging panelists data to purchases for %s", yr)
+  purchases <- merge(purchases, panelists, by = c("household_code"), all.x = T)
+  rm(panelists)
+  
+  ## append to final data across years
+  full.purchases <- rbind(full.purchases, purchases)
+  rm(purchases)
+}
+
+## Finally, collapse across years
+full.purchases <- full.purchases[, .(total_expenditures = sum(total_expenditures),
+                                     n_trips = sum(n_trips)),
+                                 by = .(household_code, store_code_uc, zip_code)]
+
+## Merge zip code info to the household
+zip_data <- fread("../../consumer_zip_code_data.csv") 
+full.purchases <- merge(full.purchases, zip_data, by = "zip_code")
+rm(zip_data)
+
+## Collapse at the store level
+store_costumer_ch <- full.purchases[, .(av_hh_income_sales = mean(av_hh_income, na.rm = T, weights = total_expenditures),
+                                        per_bachelor_25_sales = mean(per_bachelor_25, na.rm = T, weights = total_expenditures),
+                                        median_age_sales = mean(median_age, na.rm = T, weights = total_expenditures),
+                                        av_hh_income_trips = mean(av_hh_income, na.rm = T, weights = n_trips),
+                                        per_bachelor_25_trips = mean(per_bachelor_25, na.rm = T, weights = n_trips),
+                                        median_age_trips = mean(median_age, na.rm = T, weights = n_trips),
+                                        n_households = .N,
+                                        n_trips = sum(n_trips)
+                                        ), by = c("store_code_uc")]
+
+## Merge info to store data
+stores_all <- merge(stores_all, store_costumer_ch, by = "store_code_uc", all.x = T)
 
 ## Save this File
 fwrite(stores.all, "Data/Nielsen/stores_all.csv")
