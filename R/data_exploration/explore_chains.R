699b9316087a0df27ef65f538e6a16f3274aaedb
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2020-01-17 08:58:07 -0600

    Explore Chains

--- R/data_exploration/explore_chains.R
+++ R/data_exploration/explore_chains.R
@@ -1,5 +1,5 @@
 #' Sales Taxes Project
-#' Robustness Check: Price uniformity in US Retail as in Dellavigna and Gentzkow
+#' Robustness Check: Price uniformity in US Retail as in Dellavigna and Gentzkow (DG)
 #' In this Code we explore how price uniformity looks like in our sample as they do in Figure 1
 #' They strategically select 1 (2) chain. We want to do this for all chains
 #' Also we explore geographical distribution of stores within chains (as opposed to their plot across states)
@@ -13,6 +13,7 @@ library(rgeos)
 library(plyr)
 library(sf)
 library(stringr)
+library(ggplot2)
 
 setwd("/project2/igaarder")
 
@@ -25,7 +26,7 @@ data.stores <- "Data/Nielsen/stores_all.csv"
 county.shp.path <- "Data/Spatial_Data_LODES/US county shape file"
 
 ## output filepaths -----------------
-folder.plots <- "/home/slacouture/Stores/Price"
+folder.price <- "/home/slacouture/Stores/Price"
 folder.maps <- "/home/slacouture/Stores/Maps"
 
 
@@ -41,7 +42,7 @@ stores.dg <- stores.all[DGsample == 1]
 stores.dg <- stores.dg[, .(n_stores = 1), by = .(fips_county_full, chain, store_code_uc)]
 # Collapse at the county level and chain to plot
 stores.dg <- stores.dg[, .(n_stores = .N), by = .(fips_county_full, chain)]
-breaks <- c(0,unique(quantile(stores.dg$n_stores, probs = seq(0, 1, by = 1/25), na.rm = T))[-1])
+breaks <- c(0, unique(quantile(stores.dg$n_stores, probs = seq(0, 1, by = 1/30), na.rm = T)))
 # Identify each chains to plot each chain geographically
 chains <- unique(stores.dg$chain)
 
@@ -56,31 +57,122 @@ county.skel <- data.table(counties@data)
 county.skel <- county.skel[, c("fips_county_full") ]
 
 ## Loop across Chains and Plot
-for (ch in chains) {
-  
-  ## Keep interest chain
-  stores.chain <- stores.dg[chain == ch]
-  ## Merge to skel and replace missing
-  stores.chain <- merge(stores.chain, county.skel, by = "fips_county_full", all = T)
-  stores.chain[, n_stores:= ifelse(is.na(n_stores), 0, n_stores)]
-  
-  ## Merge data to plot
-  counties.plot <- merge(counties, stores.chain, by = "fips_county_full")
-  
-  ## Arrange variable to plot uniformly across chains
-  counties.plot@data$n_stores_cut <- cut(counties.plot@data$n_stores, breaks = c(breaks, Inf), right = F)
+# for (ch in chains) {
+#   
+#   ## Keep interest chain
+#   stores.chain <- stores.dg[chain == ch]
+#   ## Merge to skel and replace missing
+#   stores.chain <- merge(stores.chain, county.skel, by = "fips_county_full", all = T)
+#   stores.chain[, n_stores:= ifelse(is.na(n_stores), 0, n_stores)]
+#   
+#   ## Merge data to plot
+#   counties.plot <- merge(counties, stores.chain, by = "fips_county_full")
+#   
+#   ## Arrange variable to plot uniformly across chains
+#   counties.plot@data$n_stores_cut <- cut(counties.plot@data$n_stores, breaks = c(breaks, Inf), right = F)
+# 
+#   ## Create Plot
+#   plot.name <- paste0(folder.maps, "/map_stores_chain_", ch, ".png")
+#   plot <- spplot(counties.plot, "n_stores_cut", main = paste("Stores Distribution Chain ", ch), col = "transparent", 
+#                  colorkey = list(height = 1, labels = list(at = seq(0.5, length(breaks) -0.5), labels = breaks)))
+#   
+#   ## Export
+#   png(plot.name)
+#   print(plot)
+#   dev.off()
+# }
 
-  ## Create Plot
-  plot.name <- paste0(folder.maps, "/map_stores_chain_", ch, ".png")
-  plot <- spplot(counties.plot, "n_stores_cut", main = paste("Stores Distribution Chain ", ch), col = "transparent", 
-                 colorkey = list(height = 1, labels = list(at = seq(0.5, length(breaks) -0.5), labels = breaks)))
-  
-  ## Export
-  png(plot.name)
-  print(plot)
-  dev.off()
+## Explore # stores distribution across chains and # counties with presence across chains
+
+## Stores
+# Full distribution
+chains.dg <- stores.dg[, .(n_stores = sum(n_stores)), by = .(chain)]
+
+graphout <- paste0(folder.maps,"/n_stores_hist_full.png")
+hist <- ggplot(data=chains.dg, aes(n_stores)) + 
+  geom_histogram(alpha = 0.5, aes(y=..density..)) +    
+  theme_classic(base_size = 24) +
+  labs(x = "No. stores per chain", y = "Fraction", color = NULL) +
+ggsave(graphout)
+
+# Less than 1500
+chains.dg <- chains.dg[n_stores < 1500]
+graphout <- paste0(folder.maps,"/n_stores_hist_zoom.png")
+hist <- ggplot(data=chains.dg, aes(n_stores)) + 
+  geom_histogram(alpha = 0.5, aes(y=..density..)) +    
+  theme_classic(base_size = 24) +
+  labs(x = "No. stores per chain", y = "Fraction", color = NULL) +
+  ggsave(graphout)
+
+## Counties
+# Full Distribution
+chains.dg <- stores.dg[, .(n_counties = .N), by = .(chain)]
+
+graphout <- paste0(folder.maps,"/n_counties_hist_full.png")
+hist <- ggplot(data=chains.dg, aes(n_counties)) + 
+  geom_histogram(alpha = 0.5, aes(y=..density..)) +    
+  theme_classic(base_size = 24) +
+  labs(x = "No. counties per chain", y = "Fraction", color = NULL) +
+  ggsave(graphout)
+
+# Less than 500
+chains.dg <- chains.dg[n_counties < 250]
+
+graphout <- paste0(folder.maps,"/n_counties_hist_zoom.png")
+hist <- ggplot(data=chains.dg, aes(n_counties)) + 
+  geom_histogram(alpha = 0.5, aes(y=..density..)) +    
+  theme_classic(base_size = 24) +
+  labs(x = "No. counties per chain", y = "Fraction", color = NULL) +
+  ggsave(graphout)
+
+### 2. Explore Price uniformity in our sample with our measures of prices ------
+
+## Open Final data
+all_pi <- fread(data.semester)
+
+## Merge with DG sample
+stores.dg <- stores.all[DGsample == 1][, -c("fips_county_full", "fips_state_code", "fips_county_code")]
+all_pi <- merge(all_pi, stores.dg, by = c("store_code_uc", "year"))
+
+## Create "average sales tax across time" for these plots
+all_pi[, av_sales_tax := mean(exp(ln_sales_tax)), by = c("store_code_uc", "product_module_code") ]
+
+## de-mean log prices by the module mean across stores
+all_pi[, price_plot := ln_cpricei2 - mean(ln_cpricei2, na.rm = T), by = product_module_code]
+
+###### Plot Prices as in DG. 
+## For now, don't fix missings in store characteristics
+## Products to plot: Orange juice (1040), chocolate (1293), cat food (1306) as they do. 
+## Use both income (their argument) and taxes (our argument)
+products <- c(1040, 1293, 1306)
+all_pi[, time := year + (semester-1)*0.5 + 0.25] #
+
+for (pr in products) {
+  for (ch in chains) {
+    
+    # Restrict data to plot
+    chain_product <- all_pi[chain == ch & product_module_code == pr]
+    
+    # Plot by income and export
+    graphout <- paste0(folder.price,"/", pr,"/price_income_chain_", ch,".png")
+    ggplot(data = chain_product, aes(x  = time, y = av_hh_income_sales)) +
+      geom_raster(aes(fill(price_plot))) +
+      labs(x=NULL, y="Stores, sorted by income", title = paste0("chain", ch)) +
+      scale_x_continuous(breaks = 2008:2014) +
+      guide_colorbar(title = NULL, ticks = F, label = F)
+    ggsave(graphout)
+    
+    # Plot by tax and export
+    graphout <- paste0(folder.price,"/", pr,"/price_tax_chain_", ch,".png")
+    ggplot(data = chain_product, aes(x  = time, y = av_sales_tax)) +
+      geom_raster(aes(fill(price_plot))) +
+      labs(x=NULL, y="Stores, sorted by income", title = paste0("chain", ch)) +
+      scale_x_continuous(breaks = 2008:2014) +
+      guide_colorbar(title = NULL, ticks = F, label = F)
+    ggsave(graphout)
+
+  }
 }
-
 
 
 
