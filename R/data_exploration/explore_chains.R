62420554a9249b6632a88bccdbb532b37e270f9f
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2020-01-21 16:09:32 -0600

    Explore chains

--- R/data_exploration/explore_chains.R
+++ R/data_exploration/explore_chains.R
@@ -36,8 +36,8 @@ folder.maps <- "/home/slacouture/Stores/Maps"
 stores.all <- fread("Data/Nielsen/stores_all.csv")
 # Create full fips county code
 stores.all[, fips_county_full := fips_state_code*1000 + fips_county_code]
-# Extract the identified sample to plot chains
-stores.dg <- stores.all[DGsample == 1]
+# Extract the identified sample (in our sample) to plot chains
+stores.dg <- stores.all[DGsample == 1 & is.na(N_modules)]
 # Collapse at the store level
 stores.dg <- stores.dg[, .(n_stores = 1), by = .(fips_county_full, chain, store_code_uc, fips_state_code)]
 # Collapse at the county level and chain to plot
@@ -57,30 +57,30 @@ county.skel <- data.table(counties@data)
 county.skel <- county.skel[, c("fips_county_full") ]
 
 ## Loop across Chains and Plot
-# for (ch in chains) {
-#   
-#   ## Keep interest chain
-#   stores.chain <- stores.dg[chain == ch]
-#   ## Merge to skel and replace missing
-#   stores.chain <- merge(stores.chain, county.skel, by = "fips_county_full", all = T)
-#   stores.chain[, n_stores:= ifelse(is.na(n_stores), 0, n_stores)]
-#   
-#   ## Merge data to plot
-#   counties.plot <- merge(counties, stores.chain, by = "fips_county_full")
-#   
-#   ## Arrange variable to plot uniformly across chains
-#   counties.plot@data$n_stores_cut <- cut(counties.plot@data$n_stores, breaks = c(breaks, Inf), right = F)
-# 
-#   ## Create Plot
-#   plot.name <- paste0(folder.maps, "/map_stores_chain_", ch, ".png")
-#   plot <- spplot(counties.plot, "n_stores_cut", main = paste("Stores Distribution Chain ", ch), col = "transparent", 
-#                  colorkey = list(height = 1, labels = list(at = seq(0.5, length(breaks) -0.5), labels = breaks)))
-#   
-#   ## Export
-#   png(plot.name)
-#   print(plot)
-#   dev.off()
-# }
+for (ch in chains) {
+
+  ## Keep interest chain
+  stores.chain <- stores.dg[chain == ch]
+  ## Merge to skel and replace missing
+  stores.chain <- merge(stores.chain, county.skel, by = "fips_county_full", all = T)
+  stores.chain[, n_stores:= ifelse(is.na(n_stores), 0, n_stores)]
+
+  ## Merge data to plot
+  counties.plot <- merge(counties, stores.chain, by = "fips_county_full")
+
+  ## Arrange variable to plot uniformly across chains
+  counties.plot@data$n_stores_cut <- cut(counties.plot@data$n_stores, breaks = c(breaks, Inf), right = F)
+
+  ## Create Plot
+  plot.name <- paste0(folder.maps, "/map_stores_chain_", ch, ".png")
+  plot <- spplot(counties.plot, "n_stores_cut", main = paste("Stores Distribution Chain ", ch), col = "transparent",
+                 colorkey = list(height = 1, labels = list(at = seq(0.5, length(breaks) -0.5), labels = breaks)))
+
+  ## Export
+  png(plot.name)
+  print(plot)
+  dev.off()
+}
 
 ## Explore # stores distribution across chains and # counties with presence across chains
 
@@ -176,8 +176,8 @@ for (pr in products) {
 
       nstores <- length(unique(plot1.data$store_code_uc))
       max <- max(plot1.data$Y, na.rm = T)
-      min.lab <- as.character(floor(min(plot1.data$av_sales_tax, na.rm = T)/5000))
-      max.lab <- as.character(floor(max(plot1.data$av_sales_tax, na.rm = T)/5000))
+      min.lab <- as.character(floor(min(plot1.data$av_sales_tax, na.rm = T)/2500))
+      max.lab <- as.character(floor(max(plot1.data$av_sales_tax, na.rm = T)/2500))
       # Plot by income and export
       graphout <- paste0(folder.price,"/", pr,"/price_income_chain_", ch,".png")
       ggplot(data = plot1.data, aes(x  = time, y = Y)) +
@@ -212,5 +212,94 @@ for (pr in products) {
   }
 }
 
+##### 3. Tax variance Decomposition ----------
+
+## Is variation of tax rates comming from between or within chain variation
+
+### a. Restart at the Final data
+all_pi <- fread(data.semester)
+
+### b. Build De-meaned tax rates
+
+# Groups
+all_pi[, module_by_store := .GRP, by = .(product_module_code, store_code_uc)]
+all_pi[, module_by_time := .GRP, by = .(product_module_code, semester, year)]
+
+# Demean
+all_pi[, dms.ln_sales_tax := ln_sales_tax - mean(ln_sales_tax, na.rm = T), by = .(store_by_module)]
+all_pi[, mdmt.ln_sales_tax := weighted.mean(ln_sales_tax, w = base.sales, na.rm = T), by = .(division_by_module_by_time)]
+all_pi[, dd.ln_sales_tax := ln_sales_tax - mean(ln_sales_tax, na.rm = T) - mdmt.ln_sales_tax, by = .(store_by_module)]
+
+### c. Restrict to sample with identified chains
+stores.dg <- stores.all[DGsample == 1][, -c("fips_county_full", "fips_state_code", "fips_county_code")]
+all_pi <- merge(all_pi, stores.dg, by = c("store_code_uc", "year"))
 
 
+### d. Compute variances
+
+# Within measures
+all_pi[, w.chain.tax.l := ln_sales_tax - weighted.mean(ln_sales_tax, w = base.sales, na.rm = T), by = .(chain, module_by_time)]
+all_pi[, w.chain.tax.d := dms.ln_sales_tax - weighted.mean(dms.ln_sales_tax, w = base.sales, na.rm = T), by = .(chain, module_by_time)]
+all_pi[, w.chain.tax.dd := dd.ln_sales_tax - weighted.mean(dd.ln_sales_tax, w = base.sales, na.rm = T), by = .(chain, module_by_time)]
+
+# Collapse at the chain level
+variances <- all_pi[, .(w.chain.tax.l = sum(base.sales*(w.chain.tax.l^2), na.rm = T),
+                        w.chain.tax.d = sum(base.sales*(w.chain.tax.d^2), na.rm = T),
+                        w.chain.tax.dd = sum(base.sales*(w.chain.tax.dd^2), na.rm = T),
+                        b.chain.tax.l = weighted.mean(ln_sales_tax, w = base.sales, na.rm = T),
+                        b.chain.tax.d = weighted.mean(dms.ln_sales_tax, w = base.sales, na.rm = T),
+                        b.chain.tax.dd = weighted.mean(dd.ln_sales_tax, w = base.sales, na.rm = T),
+                        base.sales = sum(base.sales),
+                        n_k = .N
+                        ), by = .(chain, module_by_time) ]
+
+# Compute between as deviations from the grand mean
+variances[, b.chain.tax.l := b.chain.tax.l - weighted.mean(b.chain.tax.l, w = base.sales, na.rm = T), by = .(module_by_time)]
+variances[, b.chain.tax.d := b.chain.tax.d - weighted.mean(b.chain.tax.d, w = base.sales, na.rm = T), by = .(module_by_time)]
+variances[, b.chain.tax.ds := b.chain.tax.ds - weighted.mean(b.chain.tax.ds, w = base.sales, na.rm = T), by = .(module_by_time)]
+
+# Compute final SSs
+variances <- variances[, .(ss.w.chain.tax.l = sum(w.chain.tax.l)/sum(base.sales),
+                           ss.w.chain.tax.d = sum(w.chain.tax.d)/sum(base.sales),
+                           ss.w.chain.tax.dd = sum(w.chain.tax.dd)/sum(base.sales),
+                           ss.b.chain.tax.l = sum(base.sales*n_k*(b.chain.tax.l)^2)/sum(base.sales),
+                           ss.b.chain.tax.d = sum(base.sales*n_k*(b.chain.tax.d)^2)/sum(base.sales),
+                           ss.b.chain.tax.dd = sum(base.sales*n_k*(b.chain.tax.dd)^2)/sum(base.sales),
+                           base.sales = sum(base.sales)
+), by = .(module_by_time)]
+
+### e. produce histograms of these measures
+variances.plot <- melt(variances, id.vars = c("module_by_time", "base.sales"),
+                       measure.vars = list(c("ss.w.chain.tax.l", "ss.b.chain.tax.l"),
+                                           c("ss.w.chain.tax.d", "ss.b.chain.tax.d"),
+                                           c("ss.w.chain.tax.d", "ss.b.chain.tax.dd")),
+                       variable.name = "type", variable.factor = T,
+                       value.name = c("tax.l", "tax.d", "tax.dd"),
+                       na.rm = T)
+
+graphout <- paste0(folder.price,"/within_betwwen_tax_l_module_time.png")
+hist <- ggplot(data=variances.plot, aes(tax.l, fill = type, weight = base.sales)) + 
+  geom_histogram(alpha = 0.3, aes(y=..count../sum(..count..), fill = treatment), position="identity") +    
+  theme_classic(base_size = 24) +
+  theme(legend.position="bottom") +
+  labs(x = "SS", y = "Density", color = NULL) +
+  scale_fill_discrete(name = "Type", labels = c("Within", "Between"))
+ggsave(graphout)
+
+graphout <- paste0(folder.price,"/within_betwwen_tax_d_module_time.png")
+hist <- ggplot(data=variances.plot, aes(tax.d, fill = type, weight = base.sales)) + 
+  geom_histogram(alpha = 0.3, aes(y=..count../sum(..count..), fill = treatment), position="identity") +    
+  theme_classic(base_size = 24) +
+  theme(legend.position="bottom") +
+  labs(x = "SS", y = "Density", color = NULL) +
+  scale_fill_discrete(name = "Type", labels = c("Within", "Between"))
+ggsave(graphout)
+
+graphout <- paste0(folder.price,"/within_betwwen_tax_dd_module_time.png")
+hist <- ggplot(data=variances.plot, aes(tax.d, fill = type, weight = base.sales)) + 
+  geom_histogram(alpha = 0.3, aes(y=..count../sum(..count..), fill = treatment), position="identity") +    
+  theme_classic(base_size = 24) +
+  theme(legend.position="bottom") +
+  labs(x = "SS", y = "Density", color = NULL) +
+  scale_fill_discrete(name = "Type", labels = c("Within", "Between"))
+ggsave(graphout)
