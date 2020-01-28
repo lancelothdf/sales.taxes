e60fa32bb468f35186ad097a712f63f4a1e5d9d4
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2020-01-28 12:00:42 -0600

    Demand estimates by observed competition

--- R/regressions/Semester_regressions/Demand_semester_initprice_quantiles_competition.R
+++ R/regressions/Semester_regressions/Demand_semester_initprice_quantiles_competition.R
@@ -95,6 +95,121 @@ LRdiff_res <- data.table(NULL)
 target_res <- data.table(NULL)
 
 
+##### HHI measure of competition ----------
+
+
+# 1. Price distribution
+prices.hhi <- all_pi[, quantile := cut(dm.L.ln_cpricei2, breaks = quantile(dm.L.ln_cpricei2, probs = seq(0, 1, by = 1/10), na.rm = T, weight = base.sales),
+                                       labels = 1:10, right = T), by = .(hhi_high)]
+prices.hhi <- prices.hhi[, .(price = max(dm.L.ln_cpricei2, na.rm = T)), by = .(hhi_high, quantile)]
+setnames(prices.hhi, "hhi_high", "high")
+prices.hhi[, type := "hhi"]
+
+# 2. and 3. Pass-through estimates and demand recovery 
+
+for (n.g in 1:5) {
+  
+  # Create groups of initial values of tax rate
+  # We use the full weighted distribution
+  all_pi <- all_pi[, quantile := cut(dm.L.ln_cpricei2,
+                                     breaks = quantile(dm.L.ln_cpricei2, probs = seq(0, 1, by = 1/n.g), na.rm = T, weight = base.sales),
+                                     labels = 1:n.g, right = FALSE)]
+  quantlab <- round(quantile(all_pi$dm.L.ln_cpricei2, 
+                             probs = seq(0, 1, by = 1/n.g), na.rm = T, 
+                             weight = all_pi$base.sales), digits = 4)
+  # Saturate fixed effects
+  all_pi[, group_region_by_module_by_time := .GRP, by = .(region_by_module_by_time, quantile)]
+  all_pi[, group_division_by_module_by_time := .GRP, by = .(division_by_module_by_time, quantile)]
+  
+  ##### Run fully saturated: split sample
+  
+  for (d in 0:1) {
+    
+    # Keep sample we use for estimation
+    sample <- all_pi[hhi_high == d]
+    
+    ## Estimate RF and FS
+    for (FE in FE_opts) {
+      for (Y in outcomes) {
+        formula1 <- as.formula(paste0(
+          Y, " ~ w.ln_sales_tax:quantile | ", FE, "+ quantile"
+        ))
+        if (n.g == 1) { formula1 <- as.formula(paste0(Y, " ~ w.ln_sales_tax  | ", FE)) }
+        res1 <- felm(formula = formula1, data = sample,
+                     weights = sample$base.sales)
+        
+        
+        ## attach results
+        res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+        res1.dt[, outcome := Y]
+        res1.dt[, controls := FE]
+        res1.dt[, n.groups := n.g]
+        res1.dt[, lev := quantlab[-1]]
+        res1.dt[, het.g := d]
+        res1.dt[, het.sam := "hhi"]
+        
+        ## Descriptives
+        res1.dt$N_obs <- nrow(sample)
+        res1.dt$N_stores <- uniqueN(sample, by = c("store_code_uc") )
+        res1.dt$N_modules <- length(unique(sample$product_module_code))
+        res1.dt$N_counties <- uniqueN(sample, by = c("fips_state", "fips_county"))
+        res1.dt$N_years <- uniqueN(sample, by = c("year")) # should be 6 (we lose one because we difference)
+        res1.dt$N_county_modules <- uniqueN(sample, by = c("fips_state", "fips_county",
+                                                           "product_module_code"))
+        LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
+        fwrite(LRdiff_res, results.file)
+      }
+      
+      ## Estimate IVs and retrieve in vector
+      IV <- LRdiff_res[outcome == "w.ln_quantity3" & n.groups == n.g & controls == FE &
+                         het.sam == "hhi" & het.g == d,][["Estimate"]]/LRdiff_res[outcome == "w.ln_cpricei2" &
+                                                                                    n.groups == n.g & controls == FE & het.sam == "hhi" & het.g == d,][["Estimate"]]
+      
+      ## Estimate the matrix of the implied system of equations
+      if (n.g > 1) {
+        # Get the empirical distribution of prices by quantile
+        sample[, base.sales.q := base.sales/sum(base.sales), by = .(quantile)]
+        sample[, p_group := floor((dm.ln_cpricei2 - min(dm.ln_cpricei2, na.rm = T))/((max(dm.ln_cpricei2, na.rm = T)-min(dm.ln_cpricei2, na.rm = T))/500)), by = .(quantile)]
+        sample[, p_ll := p_group*((max(dm.ln_cpricei2, na.rm = T)-min(dm.ln_cpricei2, na.rm = T))/500), by = .(quantile)]
+        sample[, p_ll := p_ll + min(dm.ln_cpricei2, na.rm = T), by = .(quantile)]
+        sample[, p_ul := p_ll + ((max(dm.ln_cpricei2, na.rm = T)-min(dm.ln_cpricei2, na.rm = T))/500), by = .(quantile)]
+        
+        ed.price.quantile <- sample[, .(w1 = (sum(base.sales.q))), by = .(p_ul, p_ll, quantile)]
+        ed.price.quantile[, p_m := (p_ul+p_ll)/2]
+        
+        
+        # Create the derivative of the polynomial of prices and multiplicate by weights
+        for (n in 1:n.g){
+          ed.price.quantile[, paste0("b",n) := (n)*w1*(p_m^(n-1))]
+        }
+        # Calculate integral
+        gamma <- ed.price.quantile[ , lapply(.SD, sum), by = .(quantile), .SDcols = paste0("b",1:n.g)]
+        gamma <- gamma[!is.na(quantile),][order(quantile)][, -c("quantile")]
+        
+        ## Retrieve target parameters
+        beta_hat <- as.vector(solve(as.matrix(gamma))%*%(as.matrix(IV)))
+        # Estimate intercept
+        mean.q <- all_pi[, mean(ln_quantity3, weights = base.sales)]
+        mean.p <- all_pi[, mean(dm.ln_cpricei2, weights = base.sales)]
+        beta_0_hat <- mean.q - sum((beta_hat)*(mean.p^(1:n.g)))
+        beta_hat <- c(beta_0_hat, beta_hat)
+        
+        ## Export estimated target parameters
+        estimated.target <- data.table(beta_hat)
+        estimated.target[, beta_n := .I-1]
+        estimated.target[, n.groups := n.g]
+        estimated.target[, controls := FE]
+        estimated.target[, het.g := d]
+        estimated.target[, het.sam := "hhi"]
+        
+        
+        target_res <- rbind(target_res, estimated.target)
+        fwrite(target_res, theta.results.file)
+      }
+    }  
+  }
+}
+
 ##### G&D measure of competition. We use 10 km trips for now ----------
 
 
@@ -210,120 +325,7 @@ for (n.g in 1:5) {
   }
 }
 
-##### HHI measure of competition ----------
 
-
-# 1. Price distribution
-prices.hhi <- all_pi[, quantile := cut(dm.L.ln_cpricei2, breaks = quantile(dm.L.ln_cpricei2, probs = seq(0, 1, by = 1/10), na.rm = T, weight = base.sales),
-                                        labels = 1:10, right = T), by = .(hhi_high)]
-prices.hhi <- prices.hhi[, .(price = max(dm.L.ln_cpricei2, na.rm = T)), by = .(hhi_high, quantile)]
-setnames(prices.hhi, "hhi_high", "high")
-prices.hhi[, type := "hhi"]
-
-# 2. and 3. Pass-through estimates and demand recovery 
-
-for (n.g in 1:5) {
-  
-  # Create groups of initial values of tax rate
-  # We use the full weighted distribution
-  all_pi <- all_pi[, quantile := cut(dm.L.ln_cpricei2,
-                                     breaks = quantile(dm.L.ln_cpricei2, probs = seq(0, 1, by = 1/n.g), na.rm = T, weight = base.sales),
-                                     labels = 1:n.g, right = FALSE)]
-  quantlab <- round(quantile(all_pi$dm.L.ln_cpricei2, 
-                             probs = seq(0, 1, by = 1/n.g), na.rm = T, 
-                             weight = all_pi$base.sales), digits = 4)
-  # Saturate fixed effects
-  all_pi[, group_region_by_module_by_time := .GRP, by = .(region_by_module_by_time, quantile)]
-  all_pi[, group_division_by_module_by_time := .GRP, by = .(division_by_module_by_time, quantile)]
-  
-  ##### Run fully saturated: split sample
-  
-  for (d in 0:1) {
-    
-    # Keep sample we use for estimation
-    sample <- all_pi[comp_high == d]
-    
-    ## Estimate RF and FS
-    for (FE in FE_opts) {
-      for (Y in outcomes) {
-        formula1 <- as.formula(paste0(
-          Y, " ~ w.ln_sales_tax:quantile | ", FE, "+ quantile"
-        ))
-        if (n.g == 1) { formula1 <- as.formula(paste0(Y, " ~ w.ln_sales_tax  | ", FE)) }
-        res1 <- felm(formula = formula1, data = sample,
-                     weights = sample$base.sales)
-        
-        
-        ## attach results
-        res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
-        res1.dt[, outcome := Y]
-        res1.dt[, controls := FE]
-        res1.dt[, n.groups := n.g]
-        res1.dt[, lev := quantlab[-1]]
-        res1.dt[, het.g := d]
-        res1.dt[, het.sam := "hhi"]
-        
-        ## Descriptives
-        res1.dt$N_obs <- nrow(sample)
-        res1.dt$N_stores <- uniqueN(sample, by = c("store_code_uc") )
-        res1.dt$N_modules <- length(unique(sample$product_module_code))
-        res1.dt$N_counties <- uniqueN(sample, by = c("fips_state", "fips_county"))
-        res1.dt$N_years <- uniqueN(sample, by = c("year")) # should be 6 (we lose one because we difference)
-        res1.dt$N_county_modules <- uniqueN(sample, by = c("fips_state", "fips_county",
-                                                           "product_module_code"))
-        LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
-        fwrite(LRdiff_res, results.file)
-      }
-      
-      ## Estimate IVs and retrieve in vector
-      IV <- LRdiff_res[outcome == "w.ln_quantity3" & n.groups == n.g & controls == FE &
-                         het.sam == "hhi" & het.g == d,][["Estimate"]]/LRdiff_res[outcome == "w.ln_cpricei2" &
-                                                                                       n.groups == n.g & controls == FE & het.sam == "hhi" & het.g == d,][["Estimate"]]
-      
-      ## Estimate the matrix of the implied system of equations
-      if (n.g > 1) {
-        # Get the empirical distribution of prices by quantile
-        sample[, base.sales.q := base.sales/sum(base.sales), by = .(quantile)]
-        sample[, p_group := floor((dm.ln_cpricei2 - min(dm.ln_cpricei2, na.rm = T))/((max(dm.ln_cpricei2, na.rm = T)-min(dm.ln_cpricei2, na.rm = T))/500)), by = .(quantile)]
-        sample[, p_ll := p_group*((max(dm.ln_cpricei2, na.rm = T)-min(dm.ln_cpricei2, na.rm = T))/500), by = .(quantile)]
-        sample[, p_ll := p_ll + min(dm.ln_cpricei2, na.rm = T), by = .(quantile)]
-        sample[, p_ul := p_ll + ((max(dm.ln_cpricei2, na.rm = T)-min(dm.ln_cpricei2, na.rm = T))/500), by = .(quantile)]
-        
-        ed.price.quantile <- sample[, .(w1 = (sum(base.sales.q))), by = .(p_ul, p_ll, quantile)]
-        ed.price.quantile[, p_m := (p_ul+p_ll)/2]
-        
-        
-        # Create the derivative of the polynomial of prices and multiplicate by weights
-        for (n in 1:n.g){
-          ed.price.quantile[, paste0("b",n) := (n)*w1*(p_m^(n-1))]
-        }
-        # Calculate integral
-        gamma <- ed.price.quantile[ , lapply(.SD, sum), by = .(quantile), .SDcols = paste0("b",1:n.g)]
-        gamma <- gamma[!is.na(quantile),][order(quantile)][, -c("quantile")]
-        
-        ## Retrieve target parameters
-        beta_hat <- as.vector(solve(as.matrix(gamma))%*%(as.matrix(IV)))
-        # Estimate intercept
-        mean.q <- all_pi[, mean(ln_quantity3, weights = base.sales)]
-        mean.p <- all_pi[, mean(dm.ln_cpricei2, weights = base.sales)]
-        beta_0_hat <- mean.q - sum((beta_hat)*(mean.p^(1:n.g)))
-        beta_hat <- c(beta_0_hat, beta_hat)
-        
-        ## Export estimated target parameters
-        estimated.target <- data.table(beta_hat)
-        estimated.target[, beta_n := .I-1]
-        estimated.target[, n.groups := n.g]
-        estimated.target[, controls := FE]
-        estimated.target[, het.g := d]
-        estimated.target[, het.sam := "hhi"]
-        
-        
-        target_res <- rbind(target_res, estimated.target)
-        fwrite(target_res, theta.results.file)
-      }
-    }  
-  }
-}
 
 ###### Export prices distribution -----
 
