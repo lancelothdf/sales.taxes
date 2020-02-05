8e7117aa8b038dc95f0f1c89c1059ab3fd611561
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2020-02-05 10:07:42 -0600

    Run spillovers DiD spec

--- R/regressions/Semester_regressions/DID_semester_cs_initprice_spillovers.R
+++ R/regressions/Semester_regressions/DID_semester_cs_initprice_spillovers.R
@@ -139,7 +139,7 @@ for (sam in samples) {
   sample <- all_pi[sample == 1]
   for (Y in outcomes.dd) {
     formula1 <- as.formula(paste0(
-      Y, " ~ dd.ln_statutory_tax| 0 | 0 | module_by_state"
+      Y, " ~ dd.ln_statutory_tax -1| 0 | 0 | module_by_state"
     ))
     res1 <- felm(formula = formula1, data = sample, 
                  weights = sample$base.sales)
