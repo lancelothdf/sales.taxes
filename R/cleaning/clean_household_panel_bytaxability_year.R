9b93be0c142e323d51864c1c1170c24717570789
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2020-04-22 15:24:49 -0500

    Clean household taxability year

--- R/cleaning/clean_household_panel_bytaxability_year.R
+++ R/cleaning/clean_household_panel_bytaxability_year.R
@@ -46,6 +46,7 @@ for (yr in 2006:2016) {
 
   ## sum expenditures over UPCs to the product module level
   flog.info("Summing expenditures over UPCs for %s", yr)
+  print(head(purchases))
   purchases <- purchases[, list(
     total_expenditures = sum(total_price_paid)
   ), by = .(trip_code_uc, product_module_code, product_group_code, department_code)]
