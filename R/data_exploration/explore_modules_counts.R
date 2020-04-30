850b391653474d8be36a9228bd837bfdda9269d5
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2020-04-30 10:53:21 -0500

    Explore sample size

--- R/data_exploration/explore_modules_counts.R
+++ R/data_exploration/explore_modules_counts.R
@@ -20,22 +20,23 @@ taxability_panel <- fread("taxability_state_panel.csv")
 taxability_panel[, taxability := ifelse(!is.na(reduced_rate), 2, taxability)]
 # We will use taxability as of December 2014
 taxability_panel <- taxability_panel[(month==12 & year==2014),][, .(product_module_code, fips_state, taxability)]
-# Read data (export before common support and estimation)
 
+# Read data (export before common support and estimation)
 all_pi <- fread(data.semester)
-all_pi <- merge(all_pi, taxability_panel, by = c("product_module_code", "fips_state"))
 
 # Collapse at module x store level
 all_pi <- all_pi[, .(sales = sum(sales),
                      N_semesters = .N),
-                 by = .(product_module_code, fips_state, taxability, store_code_uc)]
+                 by = .(product_module_code, fips_state, store_code_uc)]
 
 # Collapse at module level
 all_pi <- all_pi[, .(sales = sum(sales),
                      N_stores = .N,
                      N_stores_semester = sum(N_semesters)),
-                 by = .(product_module_code, fips_state, taxability)]
+                 by = .(product_module_code, fips_state)]
 
+# Merge taxability to capture 0s
+all_pi <- merge(all_pi, taxability_panel, by = c("product_module_code", "fips_state"), all.y = T)
 
 # Export
 fwrite(all_pi, "module_state_observed.csv")
