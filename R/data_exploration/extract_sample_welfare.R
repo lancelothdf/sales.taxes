f0b42101616ae8dd9cdf5043cb7f775d2fe379b9
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2020-05-19 16:49:05 -0500

    Extract price sample data

---
+++ R/data_exploration/extract_sample_welfare.R
@@ -0,0 +1,63 @@
+#' Sales Taxes
+#' Extrapolation of welfare effects
+#' In this code we extract a binned anonimized version of the data 
+#' We will use this data locally to try extrapolation exercises, before performing any serious estimation in the actual data
+#' 
+
+library(data.table)
+library(futile.logger)
+library(lfe)
+library(multcomp)
+
+setwd("/project2/igaarder")
+
+## inputs -----------------------------------------------
+data.semester <- "Data/Nielsen/semester_nielsen_data.csv"
+
+## Outputs ----------------------------------------------
+output.table <- "Data/extraction_state_binned_price.csv"
+
+
+all_pi <- fread(data.semester)
+
+# Need to demean
+all_pi[, module_by_time := .GRP, by = .(product_module_code, semester, year)]
+all_pi[, L.ln_cpricei2 := ln_cpricei2 - D.ln_cpricei2]
+all_pi[, dm.L.ln_cpricei2 := L.ln_cpricei2 - mean(L.ln_cpricei2, na.rm = T), by = module_by_time]
+all_pi[, dm.ln_cpricei2 := ln_cpricei2 - mean(ln_cpricei2, na.rm = T), by = module_by_time]
+
+# Defining common support
+control <- all_pi[D.ln_sales_tax == 0,]
+treated <- all_pi[D.ln_sales_tax != 0,]
+
+# Price 
+pct1.control <- quantile(control$dm.L.ln_cpricei2, probs = 0.01, na.rm = T, weight=control$base.sales)
+pct1.treated <- quantile(treated$dm.L.ln_cpricei2, probs = 0.01, na.rm = T, weight=treated$base.sales)
+
+pct99.control <- quantile(control$dm.L.ln_cpricei2, probs = 0.99, na.rm = T, weight=control$base.sales)
+pct99treated <- quantile(treated$dm.L.ln_cpricei2, probs = 0.99, na.rm = T, weight=treated$base.sales)
+
+all_pi[, cs_price := ifelse(dm.L.ln_cpricei2 > max(pct1.treated, pct1.control) & 
+                              dm.L.ln_cpricei2 < min(pct99treated, pct99.control), 1, 0)]
+# Make sure missings are 0s
+all_pi[, cs_price := ifelse(is.na(dm.L.ln_cpricei2), 0, cs_price)]
+
+## Keep within the common support
+all_pi <- all_pi[cs_price == 1,]
+
+## cut the tails (keep between 1st and 99th percentile)
+pct1 <- quantile(all_pi$dm.ln_cpricei2, probs = 0.01, na.rm = T, weight=base.sales)
+pct99 <- quantile(all_pi$dm.ln_cpricei2, probs = 0.99, na.rm = T, weight=base.sales)
+all_pi <- all_pi[(dm.ln_cpricei2 > pct1 & dm.ln_cpricei2 < pct99),]
+
+## Collapse
+# Generate rounded price
+all_pi[, p_m := round(dm.ln_cpricei2, 3)]
+
+# collapse for every price x state 
+all_pi[, .(tau = weighted.mean(ln_sales_tax, w = base.sales),
+           eta_m = sum(base.sales)), by = .(fips_state, p_m)]
+
+# Export
+fwrite(all_pi, output.table)
+
