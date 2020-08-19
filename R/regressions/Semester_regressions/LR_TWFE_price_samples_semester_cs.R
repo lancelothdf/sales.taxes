6b4df1227d5ad3800fead5b5d9c7a50bdc0eab4a
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2020-08-19 08:29:29 -0500

    passthorugh check

--- R/regressions/Semester_regressions/LR_TWFE_price_samples_semester_cs.R
+++ R/regressions/Semester_regressions/LR_TWFE_price_samples_semester_cs.R
@@ -31,15 +31,15 @@ all_pi[, w.ln_pricei2 := ln_pricei2 - mean(ln_pricei2), by = .(store_by_module)]
 all_pi[, w.ln_sales := ln_sales - mean(ln_sales), by = .(store_by_module)]
 
 
-# Need to demean
+# Need to demean (also p lags)
 all_pi[, module_by_time := .GRP, by = .(product_module_code, semester, year)]
-all_pi[, L.ln_cpricei2 := ln_cpricei2 - D.ln_cpricei2]
-all_pi[, dm.L.ln_cpricei2 := L.ln_cpricei2 - mean(L.ln_cpricei2, na.rm = T), by = module_by_time]
 all_pi[, dm.ln_cpricei2 := ln_cpricei2 - mean(ln_cpricei2, na.rm = T), by = module_by_time]
 all_pi[, dm.ln_pricei2 := ln_pricei2 - mean(ln_pricei2, na.rm = T), by = module_by_time]
-all_pi[, dm.L.ln_pricei2 := L.ln_pricei2 - mean(L.ln_pricei2, na.rm = T), by = module_by_time]
 all_pi[, dm.ln_quantity3 := ln_quantity3 - mean(ln_quantity3, na.rm = T), by = module_by_time]
-
+all_pi[, L.ln_cpricei2 := ln_cpricei2 - D.ln_cpricei2]
+all_pi[, L.ln_pricei2 := ln_pricei2 - D.ln_pricei2]
+all_pi[, dm.L.ln_cpricei2 := L.ln_cpricei2 - mean(L.ln_cpricei2, na.rm = T), by = module_by_time]
+all_pi[, dm.L.ln_pricei2 := L.ln_pricei2 - mean(L.ln_pricei2, na.rm = T), by = module_by_time]
 
 # Defining common support
 control <- all_pi[D.ln_sales_tax == 0,]
