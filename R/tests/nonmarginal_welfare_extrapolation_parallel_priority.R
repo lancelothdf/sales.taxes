8c90877add86ff9416d50646c91fa291a70e9bef
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2020-09-28 21:59:34 -0500

    Fix minor bug nonmarginal: Infinite cases

--- R/tests/nonmarginal_welfare_extrapolation_parallel_priority.R
+++ R/tests/nonmarginal_welfare_extrapolation_parallel_priority.R
@@ -20,6 +20,7 @@ source("Code/sales.taxes/R/tests/welfare_formulae_nlopt.R")
 
 ## Output files
 out.file.nonmarginal <- "Data/nonmarginal_extrapoaltion_state_priority.csv"
+out.file.nonmarginal <- "Data/nonmarginal_extrapoaltion_state_priorityB.csv"
 
 
 # 0. Parallelize options
@@ -38,15 +39,18 @@ IVs <- IVs[controls == "division_by_module_by_time"]
 
 # 3. Values to Tests
 
-sigmas.test <- c(0.25, 0.5, 0.75, 1)
+#sigmas.test <- c(0.25, 0.5, 0.75, 1)
+sigmas.test <- c(0.75, 1)
 
 thetas.list <- list()
+## All
 # thetas.list$s25 <- c(0, 0.058897778, 0.564015475)
 # thetas.list$s50 <- c(0, 0.018501935, 0.202776786)
 # thetas.list$s75 <- c(0, 0.008007016, 0.102212766)
 # thetas.list$s100 <- c(0, 0.004861793, 0.066616166)
-thetas.list$s25 <- c(0)
-thetas.list$s50 <- c(0)
+## Priority
+# thetas.list$s25 <- c(0)
+# thetas.list$s50 <- c(0)
 thetas.list$s75 <- c(0)
 thetas.list$s100 <- c(0, 0.066616166)
 
