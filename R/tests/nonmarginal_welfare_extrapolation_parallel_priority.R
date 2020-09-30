c9f373e3fc2d29d32f820e58341c5bda2beb118d
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2020-09-30 15:04:23 -0500

    solve scaling down

--- R/tests/nonmarginal_welfare_extrapolation_parallel_priority.R
+++ R/tests/nonmarginal_welfare_extrapolation_parallel_priority.R
@@ -22,8 +22,8 @@ source("Code/sales.taxes/R/tests/welfare_formulae_nlopt.R")
 #out.file.nonmarginal <- "Data/nonmarginal_extrapoaltion_state_priority.csv"
 #out.file.nonmarginal <- "Data/nonmarginal_extrapoaltion_state_priorityA.csv"
 #out.file.nonmarginal <- "Data/nonmarginal_extrapoaltion_state_priorityB.csv"
-out.file.nonmarginal <- "Data/nonmarginal_extrapoaltion_state_priorityC.csv"
-#out.file.nonmarginal <- "Data/nonmarginal_extrapoaltion_state_priorityD.csv"
+#out.file.nonmarginal <- "Data/nonmarginal_extrapoaltion_state_priorityC.csv"
+out.file.nonmarginal <- "Data/nonmarginal_extrapoaltion_state_priorityD.csv"
 
 
 # 0. Parallelize options
@@ -43,7 +43,7 @@ IVs <- IVs[controls == "division_by_module_by_time"]
 # 3. Values to Tests
 
 #sigmas.test <- c(0.25, 0.5, 0.75, 1)
-sigmas.test <- c(0.75)
+sigmas.test <- c(1)
 #sigmas.test <- c(0.75, 1)
 
 thetas.list <- list()
@@ -55,8 +55,8 @@ thetas.list <- list()
 ## Priority
 # thetas.list$s25 <- c(0)
 # thetas.list$s50 <- c(0)
- thetas.list$s75 <- c(0)
-# thetas.list$s100 <- c(0, 0.066616166)
+# thetas.list$s75 <- c(0)
+ thetas.list$s100 <- c(0, 0.066616166)
 
 states.test <- unique(data$fips_state)
 
