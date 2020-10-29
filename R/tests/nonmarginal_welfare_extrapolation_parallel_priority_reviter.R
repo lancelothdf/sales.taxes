610780933c2aa5cc8b6dd8edf8f43d35d78316a4
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2020-10-29 05:13:32 -0500

    More iterations non-convergent

--- R/tests/nonmarginal_welfare_extrapolation_parallel_priority_reviter.R
+++ R/tests/nonmarginal_welfare_extrapolation_parallel_priority_reviter.R
@@ -92,7 +92,7 @@ setnames(min.criteria, c("K", "D"), c("Degree", "L"))
 ## 7. Set up Optimization Parameters (algorithm for now)
 nlo.opts.local.df <- list(
   "algorithm"="NLOPT_LN_COBYLA",
-  "maxeval" = 1800,
+  "maxeval" = 2400,
   "xtol_rel"=1.0e-8
 )
 
@@ -223,14 +223,14 @@ for (case in c("down", "up")) {
       data.table(state, sc, sigma = sig, theta, case, K, D, obj, it, mu)
       
     }
-    res.conv <- welfare.st[it != 1800, ]
+    res.conv <- welfare.st[it != 2400, ]
     res.conv<- res.conv[, lapply(.SD, mean), by = .(state, sigma, theta, sc, case, K, D), .SDcols = c("obj", "it")]
     
     
     results.conv <- rbind(results.conv, res.conv)
     fwrite(results.conv, out.file.nonmarginal.c)
     
-    results.rev <- rbind(results.rev, welfare.st[it == 1800, ])
+    results.rev <- rbind(results.rev, welfare.st[it == 2400, ])
     fwrite(results.rev, out.file.nonmarginal.r)
     
   }
