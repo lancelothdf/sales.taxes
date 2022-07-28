a725272f912e12d8a609fe6be8d558b16ec72838
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2022-07-28 11:25:19 -0500

    Modify semester data creation to avoid needing to run yearly

--- R/cleaning/Make_all_nielsen_data_semester_2006_2016.R
+++ R/cleaning/Make_all_nielsen_data_semester_2006_2016.R
@@ -17,6 +17,7 @@ setwd("/project2/igaarder")
 ## useful filepaths ------------------------------------------------------------
 sales_data_path <- "Data/sales_quarterly_2006-2016.csv"
 quarterly_tax_path <- "Data/quarterly_tax_rates.csv"
+quarterly_tax_path_old <- "Data/quarterly_tax_rates_older.csv"
 all_goods_pi_path <- "Data/all_nielsen_data_2006_2016_quarterly.csv"
 all_goods_pi_path_old <- "Data/all_nielsen_data_2006_2016_quarterly_old.csv"
 FE_pindex_path <- "Data/Nielsen/Pindex_FE_yearly_all_years.csv"
