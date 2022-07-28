88d7c8aace468d02c7c5555d4774b95cdf49cc6a
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2022-07-28 14:02:02 -0500

    Bug fix expansion tax data

--- R/cleaning/Make_tax_rates_panel.R
+++ R/cleaning/Make_tax_rates_panel.R
@@ -68,17 +68,17 @@ head(expanded.data)
 
 all.tax <- rbind(tax.data, expanded.data, fill = T)
 head(all.tax[year < 2008])
-all.tax <- all.tax[year < 2008, 
-                   sales_tax := sales_tax[year == 2008 & month == 1] - 
-                     state_tax[year == 2008 & month == 1] + state_tax,
-                   by = c("fips_state", "fips_county")]
+all.tax[year < 2008, 
+         sales_tax := sales_tax[year == 2008 & month == 1] - 
+           state_tax[year == 2008 & month == 1] + state_tax,
+         by = c("fips_state", "fips_county")]
 head(all.tax[year < 2008])
 
 head(all.tax[year > 2014])
-all.tax <- all.tax[year > 2014, 
-                   sales_tax := sales_tax[year == 2014 & month == 12] - 
-                     state_tax[year == 2014 & month == 12] + state_tax,
-                   by = c("fips_state", "fips_county")]
+all.tax[year > 2014, 
+         sales_tax := sales_tax[year == 2014 & month == 12] - 
+           state_tax[year == 2014 & month == 12] + state_tax,
+         by = c("fips_state", "fips_county")]
 head(all.tax[year > 2014])
 
 
