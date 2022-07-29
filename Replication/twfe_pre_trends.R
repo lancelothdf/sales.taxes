bd590b2296e6340d0cab1f7b768e03b16d33b5d6
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2022-07-29 13:14:08 -0500

    Run DL model on new data: both restricted and full

--- Replication/twfe_pre_trends.R
+++ Replication/twfe_pre_trends.R
@@ -24,13 +24,7 @@ outcomes <- c("w.ln_cpricei2", "w.ln_quantity3", "w.ln_pricei2", "w.ln_sales")
 FE_opts <- c("region_by_module_by_time", "division_by_module_by_time")
 
 # Define samples
-all_pi[, all := 1]
-all_pi[(year > 2007 & year < 2015) & 
-         !(year ==2008 & semester == 1) & 
-         !(year ==2014 & semester == 2) 
-       , non_imp_tax := 1]
 samples <- c("all", "non_imp_tax")
-samples <- c("non_imp_tax")
 
 
 LRdiff_res <- data.table(NULL)
@@ -45,10 +39,10 @@ for (s in samples) {
       formula1 <- as.formula(paste0(
         Y, "~ w.ln_sales_tax | ", FE, " | 0 | module_by_state"
       ))
-      flog.info("Estimating with %s as outcome with %s FE.", Y, FE)
+      flog.info("Estimating with %s as outcome with %s FE in sample %s.", Y, FE, s)
       res1 <- felm(formula = formula1, data = data.est,
                    weights = data.est$base.sales)
-      flog.info("Finished estimating with %s as outcome with %s FE.", Y, FE)
+      flog.info("Finished estimating with %s as outcome with %s FE in sample %s.", Y, FE, s)
       
       
       ## attach results
@@ -56,7 +50,7 @@ for (s in samples) {
       res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
       res1.dt[, outcome := Y]
       res1.dt[, controls := FE]
-      res1.dt[, sample := FE]
+      res1.dt[, sample := s]
       # Add summary values
       res1.dt[, Rsq := summary(res1)$r.squared]
       res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
@@ -76,10 +70,10 @@ for (s in samples) {
         formula1 <- as.formula(paste0(
           Y, "~ F", k,".w.ln_sales_tax | ", FE, " | 0 | module_by_state"
         ))
-        flog.info("Estimating pretrend %s with %s as outcome with %s FE.", k, Y, FE)
+        flog.info("Estimating pretrend %s with %s as outcome with %s FE in sample %s.", k, Y, FE, s)
         res1 <- felm(formula = formula1, data = data.est,
                      weights = data.est$base.sales)
-        flog.info("Finished estimating pretrend %s with %s as outcome with %s FE.", k, Y, FE)
+        flog.info("Finished estimating pretrend %s with %s as outcome with %s FE in sample %s.", k, Y, FE, s)
         
         
         ## attach results
@@ -87,7 +81,7 @@ for (s in samples) {
         res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
         res1.dt[, outcome := Y]
         res1.dt[, controls := FE]
-        res1.dt[, sample := FE]
+        res1.dt[, sample := s]
         # Add summary values
         res1.dt[, Rsq := summary(res1)$r.squared]
         res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
