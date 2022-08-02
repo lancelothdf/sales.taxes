1c5476752d816b6d048f257ee913d678ebbe7153
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2022-08-02 03:17:16 -0500

    Adjust cross-sectional retailer estimation code for replication

--- Replication/cross_section_retailer.R
+++ Replication/cross_section_retailer.R
@@ -20,105 +20,85 @@ setwd("/project2/igaarder")
 rm(list = ls())
 
 ## input filepath ----------------------------------------------
-yearly_data <- fread("Data/Nielsen/yearly_nielsen_data.csv")
+all_pi <- fread("Data/Replication/all_pi.csv")
 
 ## output filepath ----------------------------------------------
 output.results.file.crossec <- "Data/Replication/LRdiff_cross_sectional_design.csv"
 
 
-### 4A. Cross-Sectional Estimates Retailer Data -----------------------
-### Price
-cohort.weights <- rep(1, 7) ##Construct weights to average across cohorts/years.  Start with equal weights
+### Clean data ----
+colnames(all_pi)
+## Collapse to yearly data
+yearly_data <- all_pi[, list(ln_cpricei2 = log(mean(exp(ln_cpricei2))), 
+                             ln_quantity2 = log(mean(exp(ln_quantity2))),
+                             ln_quantity3 = log(mean(exp(ln_quantity3))), 
+                             base.sales = sum(base.sales), 
+                             sales = sum(sales), 
+                             ln_sales_tax = log(weighted.mean(exp(ln_sales_tax), w = sales))), 
+                      by = .(store_code_uc, product_module_code, product_group_code, fips_state, 
+                             fips_county, year, state_by_module, module_by_time, store_by_time)]
+rm(all_pi)
+
+# Restrict to relevant tax data span
+yearly_data <- yearly_data[year >= 2008 & year <= 2014]
+yearly_data$year <- factor(yearly_data$year) ##Convert the indicator for year to a factor variable (needed for interaction in the regression between ln_sales_tax and dummy for year)
+
+##Construct weights to average across cohorts/years.  Equal weights
+cohort.weights <- rep(1, 7) 
 cohort.weights <- cohort.weights/sum(cohort.weights)
 
-formula0 <- as.formula(paste0(
-  "ln_cpricei2 ~ ln_sales_tax:year | module_by_time + store_by_time | 0 | state_by_module "
-))
 
-res0 <- felm(data = yearly_data,
-             formula = formula0,
-             weights = yearly_data$base.sales)
+### Cross-Sectional Estimates Retailer Data -----------------------
 
 
-## attach results
-flog.info("Writing results...")
-res1.dt <- data.table(coef(summary(res0)), keep.rownames=T)
-res1.dt[, outcome := "ln_cpricei2"]
-res1.dt[, Rsq := summary(res0)$r.squared]
-res1.dt[, adj.Rsq := summary(res0)$adj.r.squared]
-res1.dt[, specification := "LR"]
-LRdiff_res <- res1.dt ### Create table LRdiff_res in which we store all results (we start with the results we had just stored in res1.dt)
-fwrite(LRdiff_res, output.results.file.crossec)  ## Write results to a csv file 
+# outcomes of interest
+outcomes <- c("ln_cpricei2", "ln_quantity2", "ln_quantity3")
 
-
-
-### Take linear combinations of coefficients and attach results (this is the coefficient of interest)
-lc.lr0 <- paste0(cohort.weights[1], "*ln_sales_tax:year2008 + ", cohort.weights[2], "*ln_sales_tax:year2009 + ", cohort.weights[3], "*ln_sales_tax:year2010 + ", cohort.weights[4], "*ln_sales_tax:year2011 + ", cohort.weights[5], "*ln_sales_tax:year2012 + ", cohort.weights[6], "*ln_sales_tax:year2013 + ", cohort.weights[7], "*ln_sales_tax:year2014", sep = "")
-lc.formula0 <- paste0(lc.lr0, " = 0", sep = "")
-lc.test0 <- glht(res0, linfct = c(lc.formula0))
-
-# Calculate the p-value
-pval <- 2*(1 - pnorm(abs(coef(summary(lc.test0))[[1]]/sqrt(vcov(summary(lc.test0)))[[1]])))
-
-
-lp.dt <- data.table(
-  rn = "avg.ln_sales_tax",
-  Estimate = coef(summary(lc.test0))[[1]],
-  `Cluster s.e.` = sqrt(vcov(summary(lc.test0)))[[1]],
-  `Pr(>|t|)` = pval,
-  outcome = "ln_cpricei2",
-  Rsq = summary(res0)$r.squared,
-  adj.Rsq = summary(res0)$adj.r.squared,
-  specification = "LR")
-LRdiff_res <- rbind(LRdiff_res, lp.dt, fill = T) ## Merge results to LRdiff_res
-fwrite(LRdiff_res, output.results.file.crossec) ## Write resulting file to a csv file
-
-
-
-
-### Quantity
-formula1 <- as.formula(paste0(
-  "ln_quantity2 ~ ln_sales_tax:year | module_by_time + store_by_time | 0 | state_by_module "
-))
-
-res1 <- felm(data = yearly_data,
-             formula = formula1,
-             weights = yearly_data$base.sales)
-
-
-## attach results
-flog.info("Writing results...")
-res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
-res1.dt[, outcome := "ln_quantity2"]
-res1.dt[, Rsq := summary(res1)$r.squared]
-res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
-res1.dt[, specification := "LR"]
-LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T) ## Merge results to LRdiff_res
-fwrite(LRdiff_res, output.results.file.crossec)  ## Write results to a csv file 
-
-
-# Take linear combinations of coefficients
-lc.formula1 <- paste0(cohort.weights[1], "*ln_sales_tax:year2008 + ", cohort.weights[2], "*ln_sales_tax:year2009 + ", cohort.weights[3], "*ln_sales_tax:year2010 + ", cohort.weights[4], "*ln_sales_tax:year2011 + ", cohort.weights[5], "*ln_sales_tax:year2012 + ", cohort.weights[6], "*ln_sales_tax:year2013 + ", cohort.weights[7], "*ln_sales_tax:year2014 = 0", sep = "")
-lc.test1 <- glht(res1, linfct = c(lc.formula1))
-
-# Calculate the p-value
-pval <- 2*(1 - pnorm(abs(coef(summary(lc.test1))[[1]]/sqrt(vcov(summary(lc.test1)))[[1]])))
-
-
-
-lp.dt <- data.table(
-  rn = "avg.ln_sales_tax",
-  Estimate = coef(summary(lc.test1))[[1]],
-  `Cluster s.e.` = sqrt(vcov(summary(lc.test1)))[[1]],
-  `Pr(>|t|)` = pval,
-  outcome = "ln_quantity2",
-  Rsq = summary(res1)$r.squared,
-  adj.Rsq = summary(res1)$adj.r.squared,
-  specification = "LR")
-LRdiff_res <- rbind(LRdiff_res, lp.dt, fill = T) ## Merge results to LRdiff_res
-fwrite(LRdiff_res, output.results.file.crossec) ## Write resulting file to a csv file
-
-
+for (Y in outcomes) {
+  
+  # Formula
+  formula0 <- as.formula(paste0(
+    Y, " ~ ln_sales_tax:year | module_by_time + store_by_time | 0 | state_by_module "
+  ))
+  
+  # Regression
+  res0 <- felm(data = yearly_data,
+               formula = formula0,
+               weights = yearly_data$base.sales)
+  
+  
+  ## attach results
+  flog.info("Writing results...")
+  res1.dt <- data.table(coef(summary(res0)), keep.rownames=T)
+  res1.dt[, outcome := Y]
+  res1.dt[, Rsq := summary(res0)$r.squared]
+  res1.dt[, adj.Rsq := summary(res0)$adj.r.squared]
+  LRdiff_res <- res1.dt ### Create table LRdiff_res in which we store all results (we start with the results we had just stored in res1.dt)
+  fwrite(LRdiff_res, output.results.file.crossec)  ## Write results to a csv file 
+  
+  
+  ### Take linear combinations of coefficients and attach results (this is the coefficient of interest)
+  lc.lr0 <- paste0(cohort.weights[1], "*ln_sales_tax:year2008 + ", cohort.weights[2], "*ln_sales_tax:year2009 + ", cohort.weights[3], "*ln_sales_tax:year2010 + ", cohort.weights[4], "*ln_sales_tax:year2011 + ", cohort.weights[5], "*ln_sales_tax:year2012 + ", cohort.weights[6], "*ln_sales_tax:year2013 + ", cohort.weights[7], "*ln_sales_tax:year2014", sep = "")
+  lc.formula0 <- paste0(lc.lr0, " = 0", sep = "")
+  lc.test0 <- glht(res0, linfct = c(lc.formula0))
+  
+  # Calculate the p-value
+  pval <- 2*(1 - pnorm(abs(coef(summary(lc.test0))[[1]]/sqrt(vcov(summary(lc.test0)))[[1]])))
+  
+  
+  lp.dt <- data.table(
+    rn = "avg.ln_sales_tax",
+    Estimate = coef(summary(lc.test0))[[1]],
+    `Cluster s.e.` = sqrt(vcov(summary(lc.test0)))[[1]],
+    `Pr(>|t|)` = pval,
+    outcome = Y,
+    Rsq = summary(res0)$r.squared,
+    adj.Rsq = summary(res0)$adj.r.squared)
+  LRdiff_res <- rbind(LRdiff_res, lp.dt, fill = T) ## Merge results to LRdiff_res
+  fwrite(LRdiff_res, output.results.file.crossec) ## Write resulting file to a csv file
+  
+  
+}
 
 LRdiff_res$N_obs <- nrow(yearly_data)
 LRdiff_res$N_modules <- length(unique(yearly_data$product_module_code))
