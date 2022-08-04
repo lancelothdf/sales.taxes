426a25c1b13f04e88927743650c325ee478a7222
Santiago Lacouture <slacouture@UCHICAGO.EDU>
2022-08-04 07:58:30 -0500

    Explore other specification for spillover estimates

--- Replication/reduced_form_evidence_spillovers.R
+++ Replication/reduced_form_evidence_spillovers.R
@@ -44,6 +44,97 @@ lead.lp.restr <- paste(lead.vars, "= 0")
 lag.lp.restr <- paste(lag.vars, "+ D.ln_statutory_tax = 0")
 total.lp.restr <- paste(lag.vars, "+", lead.vars, "+ D.ln_statutory_tax = 0")
 
+####### TWFE specification ------
+
+## De-mean statutory tax rate
+all_pi_spill[, w.ln_sales_tax := ln_statutory_tax - mean(ln_statutory_tax, na.rm = T), by = store_by_module]
+
+
+outcomes <- c("w.ln_cpricei2", "w.ln_quantity3", "w.ln_pricei2", "w.ln_sales")
+FE_opts <- c("region_by_module_by_time", "division_by_module_by_time")
+
+# Define samples
+samples <- c("all", "non_imp_tax")
+
+
+## Run
+for (s in samples) {
+  
+  for (sam in subsamples) {
+    all_pi_spill[, sample := get(sam)]
+    all_pi_spill_econ[, sample := get(sam)]
+    sample <- all_pi_spill_econ[sample == 1 & get(s) == 1]
+    
+    for (Y in c(outcomes)) {
+      for (FE in FE_opts) {
+        
+        formula1 <- as.formula(paste0(
+          Y, "~ w.ln_statutory_tax | ", FE, " | 0 | module_by_state"
+        ))
+        flog.info("Estimating with %s as outcome with %s FE in sample %s.", Y, FE, s)
+        res1 <- felm(formula = formula1, data = data.est,
+                     weights = data.est$base.sales)
+        flog.info("Finished estimating with %s as outcome with %s FE in sample %s.", Y, FE, s)
+        
+        
+        ## attach results
+        flog.info("Writing results...")
+        res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+        res1.dt[, outcome := Y]
+        res1.dt[, controls := FE]
+        res1.dt[, sample := s]
+        res1.dt[, spec := "TWFE"]
+        # Add summary values
+        res1.dt[, Rsq := summary(res1)$r.squared]
+        res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
+        res1.dt[, N_obs := nrow(data.est)]
+        res1.dt[, N_modules := length(unique(data.est$product_module_code))]
+        res1.dt[, N_stores :=  length(unique(data.est$store_code_uc))]
+        res1.dt[, N_counties := uniqueN(data.est, by = c("fips_state", "fips_county"))]
+        res1.dt[, N_years := uniqueN(data.est, by = c("year"))]
+        res1.dt[, N_county_modules := uniqueN(data.est, by = c("fips_state", "fips_county",
+                                                               "product_module_code"))]
+        LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
+        fwrite(LRdiff_res, results.file.spillovers)
+        
+        ## Run pre-trends
+        for (k in 2:4) {
+          
+          formula1 <- as.formula(paste0(
+            Y, "~ F", k,".w.ln_statutory_tax | ", FE, " | 0 | module_by_state"
+          ))
+          flog.info("Estimating pretrend %s with %s as outcome with %s FE in sample %s.", k, Y, FE, s)
+          res1 <- felm(formula = formula1, data = data.est,
+                       weights = data.est$base.sales)
+          flog.info("Finished estimating pretrend %s with %s as outcome with %s FE in sample %s.", k, Y, FE, s)
+          
+          
+          ## attach results
+          flog.info("Writing results...")
+          res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
+          res1.dt[, outcome := Y]
+          res1.dt[, controls := FE]
+          res1.dt[, sample := s]
+          res1.dt[, spec := "TWFE"]
+          # Add summary values
+          res1.dt[, Rsq := summary(res1)$r.squared]
+          res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
+          res1.dt[, N_obs := nrow(data.est)]
+          res1.dt[, N_modules := length(unique(data.est$product_module_code))]
+          res1.dt[, N_stores :=  length(unique(data.est$store_code_uc))]
+          res1.dt[, N_counties := uniqueN(data.est, by = c("fips_state", "fips_county"))]
+          res1.dt[, N_years := uniqueN(data.est, by = c("year"))]
+          res1.dt[, N_county_modules := uniqueN(data.est, by = c("fips_state", "fips_county",
+                                                                 "product_module_code"))]
+          LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
+          fwrite(LRdiff_res, results.file.spillovers)
+        }
+      }
+    }
+  }
+}
+
+
 
 ### Distributed lag model - semester -----------------
 LRdiff_res <- data.table(NULL)
@@ -250,97 +341,6 @@ for (s in samples) {
   
 }
 
-
-
-####### TWFE specification ------
-
-## De-mean statutory tax rate
-all_pi[, w.ln_sales_tax := ln_statutory_tax - mean(ln_statutory_tax, na.rm = T), by = store_by_module]
-
-
-outcomes <- c("w.ln_cpricei2", "w.ln_quantity3", "w.ln_pricei2", "w.ln_sales")
-FE_opts <- c("region_by_module_by_time", "division_by_module_by_time")
-
-# Define samples
-samples <- c("all", "non_imp_tax")
-
-
-## Run
-for (s in samples) {
-  
-  for (sam in subsamples) {
-    all_pi_spill[, sample := get(sam)]
-    all_pi_spill_econ[, sample := get(sam)]
-    sample <- all_pi_spill_econ[sample == 1 & get(s) == 1]
-    
-    for (Y in c(outcomes)) {
-      for (FE in FE_opts) {
-        
-        formula1 <- as.formula(paste0(
-          Y, "~ w.ln_statutory_tax | ", FE, " | 0 | module_by_state"
-        ))
-        flog.info("Estimating with %s as outcome with %s FE in sample %s.", Y, FE, s)
-        res1 <- felm(formula = formula1, data = data.est,
-                     weights = data.est$base.sales)
-        flog.info("Finished estimating with %s as outcome with %s FE in sample %s.", Y, FE, s)
-        
-        
-        ## attach results
-        flog.info("Writing results...")
-        res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
-        res1.dt[, outcome := Y]
-        res1.dt[, controls := FE]
-        res1.dt[, sample := s]
-        res1.dt[, spec := "TWFE"]
-        # Add summary values
-        res1.dt[, Rsq := summary(res1)$r.squared]
-        res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
-        res1.dt[, N_obs := nrow(data.est)]
-        res1.dt[, N_modules := length(unique(data.est$product_module_code))]
-        res1.dt[, N_stores :=  length(unique(data.est$store_code_uc))]
-        res1.dt[, N_counties := uniqueN(data.est, by = c("fips_state", "fips_county"))]
-        res1.dt[, N_years := uniqueN(data.est, by = c("year"))]
-        res1.dt[, N_county_modules := uniqueN(data.est, by = c("fips_state", "fips_county",
-                                                               "product_module_code"))]
-        LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
-        fwrite(LRdiff_res, results.file.spillovers)
-        
-        ## Run pre-trends
-        for (k in 2:4) {
-          
-          formula1 <- as.formula(paste0(
-            Y, "~ F", k,".w.ln_statutory_tax | ", FE, " | 0 | module_by_state"
-          ))
-          flog.info("Estimating pretrend %s with %s as outcome with %s FE in sample %s.", k, Y, FE, s)
-          res1 <- felm(formula = formula1, data = data.est,
-                       weights = data.est$base.sales)
-          flog.info("Finished estimating pretrend %s with %s as outcome with %s FE in sample %s.", k, Y, FE, s)
-          
-          
-          ## attach results
-          flog.info("Writing results...")
-          res1.dt <- data.table(coef(summary(res1)), keep.rownames=T)
-          res1.dt[, outcome := Y]
-          res1.dt[, controls := FE]
-          res1.dt[, sample := s]
-          res1.dt[, spec := "TWFE"]
-          # Add summary values
-          res1.dt[, Rsq := summary(res1)$r.squared]
-          res1.dt[, adj.Rsq := summary(res1)$adj.r.squared]
-          res1.dt[, N_obs := nrow(data.est)]
-          res1.dt[, N_modules := length(unique(data.est$product_module_code))]
-          res1.dt[, N_stores :=  length(unique(data.est$store_code_uc))]
-          res1.dt[, N_counties := uniqueN(data.est, by = c("fips_state", "fips_county"))]
-          res1.dt[, N_years := uniqueN(data.est, by = c("year"))]
-          res1.dt[, N_county_modules := uniqueN(data.est, by = c("fips_state", "fips_county",
-                                                                 "product_module_code"))]
-          LRdiff_res <- rbind(LRdiff_res, res1.dt, fill = T)
-          fwrite(LRdiff_res, results.file.spillovers)
-        }
-      }
-    }
-  }
-}
 
 
 ### Distributed lag model - yearly -----------------
