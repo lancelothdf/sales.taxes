7ea61fc6742e24c6f98d0dae3e057de8f64f28de
Lancelot Henry De Frahan <lancelot@midway2-login2.rcc.local>
2022-11-05 15:30:12 -0500

    file to bin price data conditional on taxability and food status

--- Replication/Distributional Incidence/summary_data_for_distributional_incidence.R
+++ Replication/Distributional Incidence/summary_data_for_distributional_incidence.R
@@ -11,12 +11,29 @@ library(data.table)
 setwd("/project2/igaarder")
 rm(list = ls())
 
+
 ## Open data filepaths ----------------------------------------------
 all_pi <- fread("Data/Replication/all_pi.csv")
+taxability <- fread("Data/taxability_state_panel.csv")
 
+
+# collapse taxability to the semester
+taxability[, taxability := ifelse(taxability == 2, 3, taxability)]
+taxability[, semester := ceiling(month/6)]
+taxability <- taxability[, .(taxability = mean(taxability),
+                             reduced_rate = mean(reduced_rate, na.rm = T),
+                             Food = mean(FoodNonfood)), 
+                         by = .(product_module_code, semester, year, fips_state)]
+taxability[, taxability := ifelse(!is.nan(reduced_rate), 2, taxability)]
+
+# Drop items whose taxability status within the period (gets rid of 39 observations - their taxability happens to be between 0 and 1)
+taxability <- taxability[taxability == 0 | taxability == 1 | taxability == 2 | taxability == 3,]
+
+
+## Merge all_pi to taxability and Food status
+all_pi <- merge(all_pi, taxability, by = c("year", "semester", "fips_state", "product_module_code"), all.x = T)
 
 ### Final binned data sets for welfare extrapolations ---------
-
 
 ## Collapse to binned price for Welfare extrapolation (needed to make it computationally feasible)
 # Generate rounded price
@@ -25,7 +42,7 @@ all_pi_p <- all_pi_p[, p_m := round(dm.ln_cpricei2, 3)]
 
 # collapse for every price x state on taxable goods 
 all_pi_p<- all_pi_p[ln_sales_tax > 0, .(tau = weighted.mean(ln_sales_tax, w = base.sales),
-                                        eta_m = sum(base.sales)), by = .(fips_state, p_m)]
+                                        eta_m = sum(base.sales)), by = .(fips_state, p_m, taxability, Food)]
 ## Collapse to binned tax 
 # Generate rounded tax
 all_pi_t <- copy(all_pi)
@@ -33,9 +50,9 @@ all_pi_t[, tau := round(ln_sales_tax, 3)]
 
 # collapse for every price x state on taxable goods 
 all_pi_t<- all_pi_t[ln_sales_tax > 0, .(p_m = weighted.mean(dm.ln_cpricei2, w = base.sales),
-                                        eta_m = sum(base.sales)), by = .(fips_state, tau)]
+                                        eta_m = sum(base.sales)), by = .(fips_state, tau, taxability, Food)]
 
 
-fwrite(all_pi_p, "Data/Replication/extraction_state_binned_price.csv", showProgress = T)
-fwrite(all_pi_t, "Data/Replication/extraction_state_binned_tax.csv", showProgress = T)
+fwrite(all_pi_p, "Data/Replication/extraction_state_binned_price_distributional.csv", showProgress = T)
+fwrite(all_pi_t, "Data/Replication/extraction_state_binned_tax_distributional.csv", showProgress = T)
 
