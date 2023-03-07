9764e973bcfbef4d2f0b1a77f40aab85e022718e
Lancelot Henry De Frahan <lancelot@Lancelots-MacBook-Pro.local>
2023-03-07 13:34:36 -0600

    Updated driver_v3.R

--- Replication/Replication_v3/driver_v3.R
+++ Replication/Replication_v3/driver_v3.R
@@ -1,6 +1,6 @@
 ##### Wesley Janson and Santiago Lacouture
 #' Sales Taxes
-#' Replication File. Updated on 9/13/2022
+#' Replication File. Updated on 03/07/2023
 #' This Code is the main driver file for replicating the
 #' full code. We call individual scripts to perform specific
 #' tasks here.
@@ -9,38 +9,38 @@
 
 
 
-setwd("/project2/igaarder")
+setwd("/project/igaarder")
 
 ###Run Code
-source("cleaning_code_v2.R") #DONE - Revisiting to reduce disk use
-source("summary_data_v2.R") #DONE - Revisiting to reduce disk use
-source("cleaning_store_characteristics_v2.R") #DONE
-source("reduced_form_evidence_v2.R") #DONE
-source("reduced_form_evidence_controls_v2.R") #DONE
-source("reduced_form_evidence_spillovers_v2.R") #DONE
-source("cross_section_retailer_v2.R") #DONE
-source("cross_section_consumer_panel_v2.R") #DONE
+source("cleaning_code_v3.R") #DONE - Revisiting to reduce disk use
+source("summary_data_v3.R") #DONE - Revisiting to reduce disk use
+source("cleaning_store_characteristics_v3.R") #DONE
+source("reduced_form_evidence_v3.R") #DONE
+source("reduced_form_evidence_controls_v3.R") #DONE
+source("reduced_form_evidence_spillovers_v3.R") #DONE
+source("cross_section_retailer_v3.R") #DONE
+source("cross_section_consumer_panel_v3.R") #DONE
 #source("twfe_pre_trends_v2.R") #DONE
-source("DiD_v2.R") #DONE
+source("DiD_v3.R") #DONE
 #source("reduced_form_cohort_twfe_parallel_v2.R") #DONE ## I don't think we need to run this one anymore
-source("reduced_form_evidence_nonlinearities_v2.R") #DONE 
+source("reduced_form_evidence_nonlinearities_v3.R") #DONE 
 
-source("point_estimates_bootstrap_v2.R") #DONE
-source("point_estimates_robust_storecharact_v2.R") #DONE
-source("partial_id_matrices_bootstrap_v2.R") #DONE
-source("partial_id_solve_bootstrap_v2.R") #DONE
-source("compute_average_elasticities_v2.R") #DONE 
-source("imperfect_salience_estimates_v2.R") #DONE 
-source("imperfect_salience_matrices_v2.R") #DONE 
-source("average_conduct_parameter_salience_v2.R") #DONE 
-source("welfare_mincriteria_v2.R") #DONE 
-source("welfare_nationwide_v2.R")  #DONE 
-source("welfare_nationwide_bootrel_v2.R")  #DONE
-source("welfare_state_marginal_v2.R") #DONE
-source("welfare_state_nonmarginal_v2.R") 
-source("simulation_point_v2.R") #DONE
-source("simulation_partial_v2.R")
-source("Figures_v2.R") 
+source("point_estimates_bootstrap_v3.R") #DONE
+source("point_estimates_robust_storecharact_v3.R") #DONE
+source("partial_id_matrices_bootstrap_v3.R") #DONE
+source("partial_id_solve_bootstrap_v3.R") #DONE
+source("compute_average_elasticities_v3.R") #DONE 
+source("imperfect_salience_estimates_v3.R") #DONE 
+source("imperfect_salience_matrices_v3.R") #DONE 
+source("average_conduct_parameter_salience_v3.R") #DONE 
+source("welfare_mincriteria_v3.R") #DONE 
+source("welfare_nationwide_v3.R")  #DONE 
+source("welfare_nationwide_bootrel_v3.R")  #DONE
+source("welfare_state_marginal_v3.R") #DONE
+source("welfare_state_nonmarginal_v3.R") 
+source("simulation_point_v3.R") #DONE
+source("simulation_partial_v3.R")
+source("Figures_v3.R") 
 # Tables are produced in Stata with /Tables.do
 
 
