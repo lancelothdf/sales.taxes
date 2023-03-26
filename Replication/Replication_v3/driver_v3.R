3c891c24cc9a0148392e54daf5812b2fcc185dd6
Lancelot Henry De Frahan <lancelot@Lancelots-MacBook-Pro.local>
2023-03-26 09:36:38 -0500

    Updated driver

--- Replication/Replication_v3/driver_v3.R
+++ Replication/Replication_v3/driver_v3.R
@@ -14,48 +14,47 @@ setwd("/project/igaarder")
 ###Run Code
 source("cleaning_code_v3.R") #DONE - Revisiting to reduce disk use
 source("summary_data_v3.R") #DONE - Revisiting to reduce disk use
-source("cleaning_store_characteristics_v3.R") #DONE
-
+source("cleaning_store_characteristics_v3.R") 
 source("reduced_form_evidence_v3.R") #DONE
 
-source("DiD_v3.R") #DONE
+source("DiD_v3.R") 
 
-source("reduced_form_evidence_nonlinearities_v3.R") #DONE 
+source("reduced_form_evidence_nonlinearities_v3.R") 
 
 ##For now - I tried alternative versions of the reduced-form evidence
-source("reduced_form_evidence_nonlinearities_DLL_v3.R") #DONE
-source("reduced_form_evidence_nonlinearities_sample2_v3.R") #DONE
-source("reduced_form_evidence_nonlinearities_DLL_sample2_v3.R") #DONE
-source("reduced_form_evidence_nonlinearities_sample3_v3.R") #DONE
-source("reduced_form_evidence_nonlinearities_DLL_sample3_v3.R") #DONE
+source("reduced_form_evidence_nonlinearities_DLL_v3.R") 
+source("reduced_form_evidence_nonlinearities_sample2_v3.R")
+source("reduced_form_evidence_nonlinearities_DLL_sample2_v3.R")
+source("reduced_form_evidence_nonlinearities_sample3_v3.R")
+source("reduced_form_evidence_nonlinearities_DLL_sample3_v3.R") 
 
-source("reduced_form_evidence_controls_v3.R") #DONE
+source("reduced_form_evidence_controls_v3.R") 
 
 ##For now _ I tried an alternative version
-source("reduced_form_evidence_controls_DLL_v3.R") #DONE
+source("reduced_form_evidence_controls_DLL_v3.R") 
 
-source("reduced_form_evidence_spillovers_v3.R") #DONE
+source("reduced_form_evidence_spillovers_v3.R") 
 
 
-source("cross_section_retailer_v3.R") #DONE
-source("cross_section_consumer_panel_v3.R") #DONE
-#source("twfe_pre_trends_v2.R") #DONE
+source("cross_section_retailer_v3.R") 
+source("cross_section_consumer_panel_v3.R") 
+#source("twfe_pre_trends_v2.R") 
 #source("reduced_form_cohort_twfe_parallel_v2.R") #DONE ## I don't think we need to run this one anymore
 
-source("point_estimates_bootstrap_v3.R") #DONE
-source("point_estimates_robust_storecharact_v3.R") #DONE
-source("partial_id_matrices_bootstrap_v3.R") #DONE
-source("partial_id_solve_bootstrap_v3.R") #DONE
-source("compute_average_elasticities_v3.R") #DONE 
-source("imperfect_salience_estimates_v3.R") #DONE 
-source("imperfect_salience_matrices_v3.R") #DONE 
-source("average_conduct_parameter_salience_v3.R") #DONE 
-source("welfare_mincriteria_v3.R") #DONE 
-source("welfare_nationwide_v3.R")  #DONE 
-source("welfare_nationwide_bootrel_v3.R")  #DONE
-source("welfare_state_marginal_v3.R") #DONE
+source("point_estimates_bootstrap_v3.R") 
+source("point_estimates_robust_storecharact_v3.R")
+source("partial_id_matrices_bootstrap_v3.R") 
+source("partial_id_solve_bootstrap_v3.R")
+source("compute_average_elasticities_v3.R") 
+source("imperfect_salience_estimates_v3.R")
+source("imperfect_salience_matrices_v3.R") 
+source("average_conduct_parameter_salience_v3.R")
+source("welfare_mincriteria_v3.R") 
+source("welfare_nationwide_v3.R") 
+source("welfare_nationwide_bootrel_v3.R") 
+source("welfare_state_marginal_v3.R")
 source("welfare_state_nonmarginal_v3.R") 
-source("simulation_point_v3.R") #DONE
+source("simulation_point_v3.R") 
 source("simulation_partial_v3.R")
 source("Figures_v3.R") 
 # Tables are produced in Stata with /Tables.do
